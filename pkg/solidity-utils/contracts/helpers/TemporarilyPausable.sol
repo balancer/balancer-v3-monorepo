1b2b5974baa70a72ae631cf9941d5e69cd7683bc
Igor Yalovoy <to@ylv.io>
2023-05-04 12:26:50 -0600

    Delete files

--- pkg/solidity-utils/contracts/helpers/TemporarilyPausable.sol
+++ pkg/solidity-utils/contracts/helpers/TemporarilyPausable.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity ^0.7.0;
-
-import "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/BalancerErrors.sol";
-import "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/ITemporarilyPausable.sol";
+pragma solidity ^0.8.18;
 
 /**
  * @dev Allows for a contract to be paused during an initial period after deployment, disabling functionality. Can be
@@ -22,39 +19,6 @@ import "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/ITemporari
  * irreversible.
  */
 abstract contract TemporarilyPausable is ITemporarilyPausable {
-    // The Pause Window and Buffer Period are timestamp-based: they should not be relied upon for sub-minute accuracy.
-    // solhint-disable not-rely-on-time
-
-    uint256 private immutable _pauseWindowEndTime;
-    uint256 private immutable _bufferPeriodEndTime;
-
-    bool private _paused;
-
-    constructor(uint256 pauseWindowDuration, uint256 bufferPeriodDuration) {
-        _require(pauseWindowDuration <= PausableConstants.MAX_PAUSE_WINDOW_DURATION, Errors.MAX_PAUSE_WINDOW_DURATION);
-        _require(
-            bufferPeriodDuration <= PausableConstants.MAX_BUFFER_PERIOD_DURATION,
-            Errors.MAX_BUFFER_PERIOD_DURATION
-        );
-
-        uint256 pauseWindowEndTime = block.timestamp + pauseWindowDuration;
-
-        _pauseWindowEndTime = pauseWindowEndTime;
-        _bufferPeriodEndTime = pauseWindowEndTime + bufferPeriodDuration;
-    }
-
-    /**
-     * @dev Reverts if the contract is paused.
-     */
-    modifier whenNotPaused() {
-        _ensureNotPaused();
-        _;
-    }
-
-    /**
-     * @dev Returns the current contract pause status, as well as the end times of the Pause Window and Buffer
-     * Period.
-     */
     function getPausedState()
         external
         view
@@ -64,69 +28,5 @@ abstract contract TemporarilyPausable is ITemporarilyPausable {
             uint256 pauseWindowEndTime,
             uint256 bufferPeriodEndTime
         )
-    {
-        paused = !_isNotPaused();
-        pauseWindowEndTime = _getPauseWindowEndTime();
-        bufferPeriodEndTime = _getBufferPeriodEndTime();
-    }
-
-    /**
-     * @dev Sets the pause state to `paused`. The contract can only be paused until the end of the Pause Window, and
-     * unpaused until the end of the Buffer Period.
-     *
-     * Once the Buffer Period expires, this function reverts unconditionally.
-     */
-    function _setPaused(bool paused) internal {
-        if (paused) {
-            _require(block.timestamp < _getPauseWindowEndTime(), Errors.PAUSE_WINDOW_EXPIRED);
-        } else {
-            _require(block.timestamp < _getBufferPeriodEndTime(), Errors.BUFFER_PERIOD_EXPIRED);
-        }
-
-        _paused = paused;
-        emit PausedStateChanged(paused);
-    }
-
-    /**
-     * @dev Reverts if the contract is paused.
-     */
-    function _ensureNotPaused() internal view {
-        _require(_isNotPaused(), Errors.PAUSED);
-    }
-
-    /**
-     * @dev Reverts if the contract is not paused.
-     */
-    function _ensurePaused() internal view {
-        _require(!_isNotPaused(), Errors.NOT_PAUSED);
-    }
-
-    /**
-     * @dev Returns true if the contract is unpaused.
-     *
-     * Once the Buffer Period expires, the gas cost of calling this function is reduced dramatically, as storage is no
-     * longer accessed.
-     */
-    function _isNotPaused() internal view returns (bool) {
-        // After the Buffer Period, the (inexpensive) timestamp check short-circuits the storage access.
-        return block.timestamp > _getBufferPeriodEndTime() || !_paused;
-    }
-
-    // These getters lead to reduced bytecode size by inlining the immutable variables in a single place.
-
-    function _getPauseWindowEndTime() private view returns (uint256) {
-        return _pauseWindowEndTime;
-    }
-
-    function _getBufferPeriodEndTime() private view returns (uint256) {
-        return _bufferPeriodEndTime;
-    }
-}
-
-/**
- * @dev Keep the maximum durations in a single place.
- */
-library PausableConstants {
-    uint256 public constant MAX_PAUSE_WINDOW_DURATION = 270 days;
-    uint256 public constant MAX_BUFFER_PERIOD_DURATION = 90 days;
+    {}
 }
