da1f9bf3b3ba7cd6312bf4154232de1807cce456
Jeffrey Bennett <endymionjkb@gmail.com>
2023-05-07 22:11:06 -0400

    lint

--- pvt/helpers/src/models/pools/mockPool.ts
+++ pvt/helpers/src/models/pools/mockPool.ts
@@ -1,11 +1,9 @@
 import { ethers } from 'ethers';
 import { BigNumberish } from '../../numbers';
 
-export const encodeJoin = (joinAmounts: BigNumberish[]): string =>
-  encodeJoinExitMockPool(joinAmounts);
+export const encodeJoin = (joinAmounts: BigNumberish[]): string => encodeJoinExitMockPool(joinAmounts);
 
-export const encodeExit = (exitAmounts: BigNumberish[]): string =>
-  encodeJoinExitMockPool(exitAmounts);
+export const encodeExit = (exitAmounts: BigNumberish[]): string => encodeJoinExitMockPool(exitAmounts);
 
 function encodeJoinExitMockPool(amounts: BigNumberish[]): string {
   return ethers.utils.defaultAbiCoder.encode(['uint256[]'], [amounts]);
