8df05cfb429cb02017361b83c87c4b12eb0d5019
Igor Yalovoy <to@ylv.io>
2023-05-09 12:00:15 -0600

    Remove import

--- pkg/solidity-utils/contracts/test/TemporarilyPausableMock.sol
+++ pkg/solidity-utils/contracts/test/TemporarilyPausableMock.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.18;
 
-import "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/ITemporarilyPausable.sol";
 import "../helpers/TemporarilyPausable.sol";
 
 contract TemporarilyPausableMock is TemporarilyPausable {
