cdddf16a045b5b5544ede3e59dacc847414a97e0
Igor Yalovoy <to@ylv.io>
2023-05-11 13:17:01 -0600

    Comment

--- pkg/solidity-utils/contracts/helpers/TemporarilyPausable.sol
+++ pkg/solidity-utils/contracts/helpers/TemporarilyPausable.sol
@@ -5,22 +5,8 @@ pragma solidity ^0.8.18;
 import "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/ITemporarilyPausable.sol";
 
 /**
- * @dev Allows for a contract to be paused during an initial period after deployment, disabling functionality. Can be
- * used as an emergency switch in case a security vulnerability or threat is identified.
- *
- * The contract can only be paused during the Pause Window, a period that starts at deployment. It can also be
- * unpaused and re-paused any number of times during this period. This is intended to serve as a safety measure: it lets
- * system managers react quickly to potentially dangerous situations, knowing that this action is reversible if careful
- * analysis later determines there was a false alarm.
- *
- * If the contract is paused when the Pause Window finishes, it will remain in the paused state through an additional
- * Buffer Period, after which it will be automatically unpaused forever. This is to ensure there is always enough time
- * to react to an emergency, even if the threat is discovered shortly before the Pause Window expires.
- *
- * Note that since the contract can only be paused within the Pause Window, unpausing during the Buffer Period is
- * irreversible.
- *
- * Contract has a compatible inteface with OpenZeppelin Pauseable smart contract
+ * @title TemporarilyPausable
+ * @dev Contract has a compatible inteface with OpenZeppelin Pauseable smart contract
  * https://docs.openzeppelin.com/contracts/4.x/api/security#Pausable
  * Inheritance is not used because OZ lib is using revert strings and we are using custom errors
  */
