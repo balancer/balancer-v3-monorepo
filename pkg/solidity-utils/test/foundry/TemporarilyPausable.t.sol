cbb05c8a3d6feb2089ddd85ba6afdd5145d6757c
Igor Yalovoy <to@ylv.io>
2023-05-15 18:52:20 -0600

    Add Forge support

---
+++ pkg/solidity-utils/test/foundry/TemporarilyPausable.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+import "../../contracts/helpers/TemporarilyPausable.sol";
+
+contract TemporarilyPausableTest is Test {
+    function setUp() public {}
+
+    function testFindExists() public {}
+}
