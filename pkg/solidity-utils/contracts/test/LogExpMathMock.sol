c3e0dc8292e98ab8329de14e0ec533d085898c8f
Igor Yalovoy <to@ylv.io>
2023-05-19 16:33:14 -0600

    Add LogExpMathMock

---
+++ pkg/solidity-utils/contracts/test/LogExpMathMock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.18;
+
+import "../math/LogExpMath.sol";
+
+contract LogExpMathMock {
+    function pow(uint256 x, uint256 y) public pure returns (uint256) {
+        return LogExpMath.pow(x, y);
+    }
+}
