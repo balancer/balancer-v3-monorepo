808797be7981db97969ddc84334d728e680b8bfa
Igor Yalovoy <to@ylv.io>
2023-05-23 15:38:57 -0600

    Mock functions for LogExpMath

--- pkg/solidity-utils/contracts/test/LogExpMathMock.sol
+++ pkg/solidity-utils/contracts/test/LogExpMathMock.sol
@@ -8,4 +8,16 @@ contract LogExpMathMock {
     function pow(uint256 x, uint256 y) public pure returns (uint256) {
         return LogExpMath.pow(x, y);
     }
+
+    function exp(int256 x) public pure returns (int256) {
+        return LogExpMath.exp(x);
+    }
+
+    function log(int256 arg, int256 base) public pure returns (int256) {
+        return LogExpMath.log(arg, base);
+    }
+
+    function ln(int256 a) public pure returns (int256) {
+        return LogExpMath.ln(a);
+    }
 }
