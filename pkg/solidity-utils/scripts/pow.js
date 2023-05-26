cd89493da7f7b564af2487bed0637e4d4cecfa6d
Igor Yalovoy <to@ylv.io>
2023-05-25 18:48:37 -0600

    Add pow JS script

---
+++ pkg/solidity-utils/scripts/pow.js
@@ -0,0 +1,25 @@
+const { create, all } = require('mathjs');
+const { utils } = require('ethers');
+
+const config = {
+  number: 'BigNumber',
+  precision: 64,
+};
+
+const mathInstance = create(all, config);
+
+function calculatePower(base, exponent) {
+  const ONE_18 = mathInstance.bignumber('1e+18');
+  let baseBig = mathInstance.divide(mathInstance.bignumber(base), ONE_18);
+  let exponentBig = mathInstance.divide(mathInstance.bignumber(exponent), ONE_18);
+
+  let result = mathInstance.round(mathInstance.pow(baseBig, exponentBig));
+  return mathInstance.format(result, { notation: 'fixed' });
+}
+
+// Use process.argv to get command line arguments
+let base = process.argv[2];
+let exponent = process.argv[3];
+
+let result = calculatePower(base, exponent);
+console.log(utils.defaultAbiCoder.encode(['uint256'], [result]));
