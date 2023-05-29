19676ca66942454f83d260b5a38c8272d518f479
Igor Yalovoy <to@ylv.io>
2023-05-29 17:34:30 -0600

    Update bounds for pow function

--- pkg/solidity-utils/test/foundry/LogExpMath.t.sol
+++ pkg/solidity-utils/test/foundry/LogExpMath.t.sol
@@ -14,9 +14,9 @@ contract LogExpMathTest is Test {
     uint256 constant ONE_20 = 1e20;
     uint256 constant MILD_EXPONENT_BOUND = 2 ** 254 / uint256(ONE_20);
     uint256 constant UPPER_BASE_BOUND = 1e10 * ONE_18;
-    uint256 constant LOWER_BASE_BOUND = 1e4;
+    uint256 constant LOWER_BASE_BOUND = 1e14;
     uint256 constant UPPER_EXPONENT_BOUND = 1e3 * ONE_18;
-    uint256 constant LOWER_EXPONENT_BOUND = 1e6;
+    uint256 constant LOWER_EXPONENT_BOUND = 1e14;
     LogExpMathMock mock;
 
     function setUp() public {
