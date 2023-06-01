c07c838166ebc648a42f8b91b15ff3e389ca6e0b
Igor Yalovoy <to@ylv.io>
2023-06-01 17:49:49 -0600

    Differential tests for LogExpMath (#26)
    * Remove recursion from exp
    
    * Remove recursion from ln
    
    * Add unchecked arithmetics operations
    
    * Add LogExpMathTest
    
    * Comments about recursion
    
    * Add mathjs
    
    * Add OZ contracts
    
    * Add OZ remapping
    
    * Update yarn lock
    
    * Add pow JS script
    
    * Add pow diff test
    
    * Update forge test command
    
    * Fix precision for pow script
    
    * Add bounds for pow function
    
    * Update number of runs for fuzz tests
    
    * Log failed Forge runs
    
    * Lower EXPECTED_RELATIVE_ERROR
    
    * Add test:stress command
    
    * Update bounds for pow function
    
    * Do not tolerate fails
    
    * Adjust bounds and error delta
    
    * Adjust bounds for pow
    
    * Better tests defaults

--- pkg/solidity-utils/test/foundry/LogExpMath.t.sol
+++ pkg/solidity-utils/test/foundry/LogExpMath.t.sol
@@ -3,11 +3,54 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "../../contracts/math/LogExpMath.sol";
+import "../../contracts/test/LogExpMathMock.sol";
 
 contract LogExpMathTest is Test {
+    uint256 internal constant EXPECTED_RELATIVE_ERROR = 1e4;
+    uint256 constant ONE_18 = 1e18;
+    uint256 constant ONE_20 = 1e20;
+    uint256 constant MILD_EXPONENT_BOUND = 2 ** 254 / uint256(ONE_20);
+    uint256 constant UPPER_BASE_BOUND = 1e7 * ONE_18;
+    uint256 constant LOWER_BASE_BOUND = 1e15;
+    uint256 constant UPPER_EXPONENT_BOUND = 5 * ONE_18;
+    uint256 constant LOWER_EXPONENT_BOUND = 1e14;
+    LogExpMathMock mock;
+
+    function setUp() public {
+        mock = new LogExpMathMock();
+    }
+
     function testPow() external {
         assertApproxEqAbs(LogExpMath.pow(2e18, 2e18), 4e18, 100);
+    }
+
+    /**
+     * forge-config: default.fuzz.runs = 256
+     * forge-config: intense.fuzz.runs = 10000
+     */
+    function testPowMatchesJSFuzzed(uint256 base, uint256 exponent) external {
+        base = bound(base, LOWER_BASE_BOUND, UPPER_BASE_BOUND);
+        exponent = bound(exponent, LOWER_EXPONENT_BOUND, UPPER_EXPONENT_BOUND);
+
+        uint256 pow = mock.pow(base, exponent);
+
+        string[] memory bashInput = new string[](4);
+
+        // Build ffi command string
+        bashInput[0] = "node";
+        bashInput[1] = "./scripts/pow.js";
+        bashInput[2] = Strings.toString(base);
+        bashInput[3] = Strings.toString(exponent);
+
+        // Run command and capture output
+        bytes memory result = vm.ffi(bashInput);
+        uint256 expectedResult = abi.decode(result, (uint256));
+
+        uint256 delta = expectedResult / EXPECTED_RELATIVE_ERROR;
+        // for delta of 0 allow precision loss of 1
+        assertApproxEqAbs(pow, expectedResult, delta == 0 ? 1 : delta);
     }
 }
