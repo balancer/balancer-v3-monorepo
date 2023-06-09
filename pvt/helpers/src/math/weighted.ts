470dd4c215d9d5cf09940005cd9c6bdd6360754b
Igor Yalovoy <to@ylv.io>
2023-06-09 13:58:39 -0600

    Weighted math (#28)
    * Remove recursion from exp
    
    * Remove recursion from ln
    
    * Add unchecked arithmetics operations
    
    * Add LogExpMathTest
    
    * Comments about recursion
    
    * Add mathjs
    
    * Add OZ contracts
    
    * Add OZ remapping
    
    * Update yarn lock
    
    * Add pow JS script
    
    * Add pow diff test
    
    * Update forge test command
    
    * Fix precision for pow script
    
    * Add bounds for pow function
    
    * Update number of runs for fuzz tests
    
    * Log failed Forge runs
    
    * Lower EXPECTED_RELATIVE_ERROR
    
    * Add test:stress command
    
    * Update bounds for pow function
    
    * Do not tolerate fails
    
    * Adjust bounds and error delta
    
    * Adjust bounds for pow
    
    * Copy V2
    
    * Add custom errors
    
    * Convert to 0.8 math
    
    * Error specs
    
    * Add V2 WeightedMath tests
    
    * Remove function underscores
    
    * Add WeightedMathMock
    
    * Add JS WeightedMath
    
    * Fix WeightedMath tests
    
    * Make methods internal
    
    * Lint
    
    * Refator tests
    
    * Fix math
    
    * Add constants
    
    * Add calcBptOutGivenExactTokensIn tests
    
    * Remove console.log
    
    * Add calcBptOutGivenExactTokenIn math
    
    * Add calcBptOutGivenExactTokenIn tests
    
    * Add MaxOutRatio test
    
    * Refactor
    
    * Fix math error
    
    * Add calcTokenInGivenExactBptOut tests
    
    * Refactor
    
    * Add calcBptInGivenExactTokenOut
    
    * Add calcBptInGivenExactTokensOut and  calcBptInGivenExactTokenOut tests
    
    * Fix calcTokenOutGivenExactBptIn
    
    * Add calcTokenOutGivenExactBptIn test
    
    * Add calcBptOutAddToken func
    
    * Add calcBptOutAddToken test
    
    * More coverage
    
    * Refactor
    
    * More tests
    
    * SWAP_FEE
    
    * Refactor
    
    * revert foundry settings
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/test/WeightedMath.test.ts
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Update pkg/solidity-utils/contracts/math/WeightedMath.sol
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>
    
    * Lint
    
    * Refactor
    
    ---------
    
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

---
+++ pvt/helpers/src/math/weighted.ts
@@ -0,0 +1,334 @@
+import { Decimal } from 'decimal.js';
+import { BigNumber } from 'ethers';
+
+import { BigNumberish, bn, decimal, fp, fromFp, toFp, fpMul, fpDiv, FP_ONE, FP_100_PCT } from '../numbers';
+
+export function calculateInvariant(fpRawBalances: BigNumberish[], fpRawWeights: BigNumberish[]): BigNumber {
+  const normalizedWeights = fpRawWeights.map(fromFp);
+  const balances = fpRawBalances.map(decimal);
+  const invariant = balances.reduce((inv, balance, i) => inv.mul(balance.pow(normalizedWeights[i])), decimal(1));
+  return bn(invariant);
+}
+
+export function calcOutGivenIn(
+  fpBalanceIn: BigNumberish,
+  fpWeightIn: BigNumberish,
+  fpBalanceOut: BigNumberish,
+  fpWeightOut: BigNumberish,
+  fpAmountIn: BigNumberish
+): BigNumber {
+  const newBalance = fromFp(fpBalanceIn).add(fromFp(fpAmountIn));
+  const base = fromFp(fpBalanceIn).div(newBalance);
+  const exponent = fromFp(fpWeightIn).div(fromFp(fpWeightOut));
+  const ratio = decimal(1).sub(base.pow(exponent));
+  return bn(toFp(fromFp(fpBalanceOut).mul(ratio)));
+}
+
+export function calcInGivenOut(
+  fpBalanceIn: BigNumberish,
+  fpWeightIn: BigNumberish,
+  fpBalanceOut: BigNumberish,
+  fpWeightOut: BigNumberish,
+  fpAmountOut: BigNumberish
+): BigNumber {
+  const newBalance = fromFp(fpBalanceOut).sub(fromFp(fpAmountOut));
+  const base = fromFp(fpBalanceOut).div(newBalance);
+  const exponent = fromFp(fpWeightOut).div(fromFp(fpWeightIn));
+  const ratio = base.pow(exponent).sub(1);
+  return bn(toFp(fromFp(fpBalanceIn).mul(ratio)));
+}
+
+export function calcBptOutGivenExactTokensIn(
+  fpBalances: BigNumberish[],
+  fpWeights: BigNumberish[],
+  fpAmountsIn: BigNumberish[],
+  fpBptTotalSupply: BigNumberish,
+  fpSwapFeePercentage: BigNumberish
+): BigNumberish {
+  const weights = fpWeights.map(fromFp);
+  const balances = fpBalances.map(fromFp);
+  const amountsIn = fpAmountsIn.map(fromFp);
+  const bptTotalSupply = fromFp(fpBptTotalSupply);
+
+  const balanceRatiosWithFee = [];
+  let invariantRatioWithFees = decimal(0);
+  for (let i = 0; i < balances.length; i++) {
+    balanceRatiosWithFee[i] = balances[i].add(amountsIn[i]).div(balances[i]);
+    invariantRatioWithFees = invariantRatioWithFees.add(balanceRatiosWithFee[i].mul(weights[i]));
+  }
+
+  let invariantRatio = decimal(1);
+  for (let i = 0; i < balances.length; i++) {
+    let amountInWithoutFee;
+
+    if (balanceRatiosWithFee[i].gt(invariantRatioWithFees)) {
+      const nonTaxableAmount = balances[i].mul(invariantRatioWithFees.sub(1));
+      const taxableAmount = amountsIn[i].sub(nonTaxableAmount);
+      amountInWithoutFee = nonTaxableAmount.add(taxableAmount.mul(decimal(1).sub(fromFp(fpSwapFeePercentage))));
+    } else {
+      amountInWithoutFee = amountsIn[i];
+    }
+
+    const tokenBalanceRatio = balances[i].add(amountInWithoutFee).div(balances[i]);
+
+    invariantRatio = invariantRatio.mul(tokenBalanceRatio.pow(weights[i]));
+  }
+
+  const bptOut = bptTotalSupply.mul(invariantRatio.sub(1));
+  return fp(bptOut);
+}
+
+export function calcBptOutGivenExactTokenIn(
+  fpBalance: BigNumberish,
+  fpWeight: BigNumberish,
+  fpAmountIn: BigNumberish,
+  fpBptTotalSupply: BigNumberish,
+  fpSwapFeePercentage: BigNumberish
+): BigNumberish {
+  const balance = fromFp(fpBalance);
+  const normalizedWeight = fromFp(fpWeight);
+  const amountIn = fromFp(fpAmountIn);
+  const bptTotalSupply = fromFp(fpBptTotalSupply);
+
+  let amountInWithoutFee;
+  const balanceRatioWithFee = balance.add(amountIn).div(balance);
+
+  const invariantRatioWithFees = balanceRatioWithFee.mul(normalizedWeight).add(complement(normalizedWeight));
+
+  if (balanceRatioWithFee.gt(invariantRatioWithFees)) {
+    const nonTaxableAmount = invariantRatioWithFees.gt(1) ? balance.mul(invariantRatioWithFees.sub(1)) : decimal(0);
+    const taxableAmount = amountIn.sub(nonTaxableAmount);
+    const swapFee = taxableAmount.mul(fromFp(fpSwapFeePercentage));
+
+    amountInWithoutFee = nonTaxableAmount.add(taxableAmount).sub(swapFee);
+  } else {
+    amountInWithoutFee = amountIn;
+    // If a token's amount in is not being charged a swap fee then it might be zero.
+    // In this case, it's clear that the sender should receive no BPT.
+    if (amountInWithoutFee.floor().toNumber() == 0) {
+      return 0;
+    }
+  }
+
+  const balanceRatio = balance.add(amountInWithoutFee).div(balance);
+
+  const invariantRatio = balanceRatio.pow(normalizedWeight);
+
+  const bptOut = invariantRatio.gt(1) ? bptTotalSupply.mul(invariantRatio.sub(1)) : 0;
+  return fp(bptOut);
+}
+
+export function calcTokenInGivenExactBptOut(
+  fpBalance: BigNumberish,
+  fpWeight: BigNumberish,
+  fpBptAmountOut: BigNumberish,
+  fpBptTotalSupply: BigNumberish,
+  fpSwapFeePercentage: BigNumberish
+): BigNumberish {
+  const bptAmountOut = fromFp(fpBptAmountOut);
+  const bptTotalSupply = fromFp(fpBptTotalSupply);
+  const weight = fromFp(fpWeight);
+  const balance = fromFp(fpBalance);
+  const swapFeePercentage = fromFp(fpSwapFeePercentage);
+
+  const invariantRatio = bptTotalSupply.add(bptAmountOut).div(bptTotalSupply);
+  const tokenBalanceRatio = invariantRatio.pow(decimal(1).div(weight));
+  const tokenBalancePercentageExcess = decimal(1).sub(weight);
+  const amountInAfterFee = balance.mul(tokenBalanceRatio.sub(decimal(1)));
+
+  const amountIn = amountInAfterFee.div(decimal(1).sub(tokenBalancePercentageExcess.mul(swapFeePercentage)));
+  return fp(amountIn);
+}
+
+export function calcBptInGivenExactTokensOut(
+  fpBalances: BigNumber[],
+  fpNormalizedWeights: BigNumber[],
+  fpAmountsOut: BigNumber[],
+  fpBptTotalSupply: BigNumber,
+  fpSwapFeePercentage: BigNumber
+): BigNumber {
+  const swapFeePercentage = fromFp(fpSwapFeePercentage);
+  const normalizedWeights = fpNormalizedWeights.map(fromFp);
+  const balances = fpBalances.map(fromFp);
+  const amountsOut = fpAmountsOut.map(fromFp);
+  const bptTotalSupply = fromFp(fpBptTotalSupply);
+
+  const balanceRatiosWithoutFee = [];
+  let invariantRatioWithoutFees = decimal(0);
+  for (let i = 0; i < balances.length; i++) {
+    const balanceRatioWithoutFee = balances[i].sub(amountsOut[i]).div(balances[i]);
+    balanceRatiosWithoutFee.push(balanceRatioWithoutFee);
+    invariantRatioWithoutFees = invariantRatioWithoutFees.add(balanceRatioWithoutFee.mul(normalizedWeights[i]));
+  }
+
+  let invariantRatio = decimal(1);
+  for (let i = 0; i < balances.length; i++) {
+    let amountOutWithFee;
+    if (invariantRatioWithoutFees > balanceRatiosWithoutFee[i]) {
+      const nonTaxableAmount = balances[i].mul(complement(invariantRatioWithoutFees));
+      const taxableAmount = amountsOut[i].sub(nonTaxableAmount);
+      const taxableAmountPlusFees = taxableAmount.div(complement(swapFeePercentage));
+
+      amountOutWithFee = nonTaxableAmount.add(taxableAmountPlusFees);
+    } else {
+      amountOutWithFee = amountsOut[i];
+      if (amountOutWithFee.floor().toNumber() == 0) {
+        continue;
+      }
+    }
+
+    const balanceRatio = balances[i].sub(amountOutWithFee).div(balances[i]);
+    invariantRatio = invariantRatio.mul(balanceRatio.pow(normalizedWeights[i]));
+  }
+
+  const bptIn = bptTotalSupply.mul(complement(invariantRatio));
+  return fp(bptIn);
+}
+
+export function calcBptInGivenExactTokenOut(
+  fpBalance: BigNumberish,
+  fpWeight: BigNumberish,
+  fpAmountOut: BigNumberish,
+  fpBptTotalSupply: BigNumberish,
+  fpSwapFeePercentage: BigNumberish
+): BigNumberish {
+  const balance = fromFp(fpBalance);
+  const normalizedWeight = fromFp(fpWeight);
+  const amountOut = fromFp(fpAmountOut);
+  const bptTotalSupply = fromFp(fpBptTotalSupply);
+  const swapFeePercentage = fromFp(fpSwapFeePercentage);
+
+  let amountOutWithFee;
+  const balanceRatioWithoutFee = balance.sub(amountOut).div(balance);
+
+  const invariantRatioWithoutFees = balanceRatioWithoutFee.mul(normalizedWeight).add(complement(normalizedWeight));
+
+  if (invariantRatioWithoutFees.gt(balanceRatioWithoutFee)) {
+    const nonTaxableAmount = balance.mul(complement(invariantRatioWithoutFees));
+    const taxableAmount = amountOut.sub(nonTaxableAmount);
+    const taxableAmountPlusFees = taxableAmount.div(complement(swapFeePercentage));
+
+    amountOutWithFee = nonTaxableAmount.add(taxableAmountPlusFees);
+  } else {
+    amountOutWithFee = amountOut;
+    // If a token's amount in is not being charged a swap fee then it might be zero.
+    // In this case, it's clear that the sender should receive no BPT.
+    if (amountOutWithFee.floor().toNumber() == 0) {
+      return 0;
+    }
+  }
+
+  const balanceRatio = balance.sub(amountOutWithFee).div(balance);
+
+  const invariantRatio = balanceRatio.pow(normalizedWeight);
+
+  const bptOut = bptTotalSupply.mul(complement(invariantRatio));
+  return fp(bptOut);
+}
+
+export function calcTokenOutGivenExactBptIn(
+  fpBalance: BigNumberish,
+  fpWeight: BigNumberish,
+  fpBptAmountIn: BigNumberish,
+  fpBptTotalSupply: BigNumberish,
+  fpSwapFeePercentage: BigNumberish
+): BigNumberish {
+  const bptAmountIn = fromFp(fpBptAmountIn);
+  const bptTotalSupply = fromFp(fpBptTotalSupply);
+  const swapFeePercentage = fromFp(fpSwapFeePercentage);
+  const weight = fromFp(fpWeight);
+  const balance = fromFp(fpBalance);
+
+  const invariantRatio = bptTotalSupply.sub(bptAmountIn).div(bptTotalSupply);
+  const tokenBalanceRatio = invariantRatio.pow(decimal(1).div(weight));
+  const tokenBalancePercentageExcess = decimal(1).sub(weight);
+  const amountOutBeforeFee = balance.mul(decimal(1).sub(tokenBalanceRatio));
+
+  const amountOut = amountOutBeforeFee.mul(decimal(1).sub(tokenBalancePercentageExcess.mul(swapFeePercentage)));
+  return fp(amountOut);
+}
+
+export function calcTokensOutGivenExactBptIn(
+  fpBalances: BigNumberish[],
+  fpBptAmountIn: BigNumberish,
+  fpBptTotalSupply: BigNumberish
+): BigNumber[] {
+  const balances = fpBalances.map(fromFp);
+  const bptRatio = fromFp(fpBptAmountIn).div(fromFp(fpBptTotalSupply));
+  const amountsOut = balances.map((balance) => balance.mul(bptRatio));
+  return amountsOut.map(fp);
+}
+
+export function calculateOneTokenSwapFeeAmount(
+  fpBalances: BigNumberish[],
+  fpWeights: BigNumberish[],
+  lastInvariant: BigNumberish,
+  tokenIndex: number
+): Decimal {
+  const balance = fpBalances.map(fromFp)[tokenIndex];
+  const weight = fromFp(fpWeights[tokenIndex]);
+  const exponent = decimal(1).div(weight);
+  const currentInvariant = calculateInvariant(fpBalances, fpWeights);
+  const invariantRatio = decimal(lastInvariant).div(decimal(currentInvariant));
+  const accruedFees = balance.mul(decimal(1).sub(invariantRatio.pow(exponent)));
+
+  return toFp(accruedFees);
+}
+
+export function calculateBPTSwapFeeAmount(
+  fpInvariantGrowthRatio: BigNumberish,
+  preSupply: BigNumberish,
+  postSupply: BigNumberish,
+  fpProtocolSwapFeePercentage: BigNumberish
+): BigNumber {
+  const supplyGrowthRatio = fpDiv(postSupply, preSupply);
+
+  if (bn(fpInvariantGrowthRatio).lte(supplyGrowthRatio)) {
+    return bn(0);
+  }
+  const swapFeePercentage = FP_100_PCT.sub(fpDiv(supplyGrowthRatio, fpInvariantGrowthRatio));
+  const k = fpMul(swapFeePercentage, fpProtocolSwapFeePercentage);
+
+  const numerator = bn(postSupply).mul(k);
+  const denominator = FP_ONE.sub(k);
+
+  return numerator.div(denominator);
+}
+
+export function calculateMaxOneTokenSwapFeeAmount(
+  fpBalances: BigNumberish[],
+  fpWeights: BigNumberish[],
+  fpMinInvariantRatio: BigNumberish,
+  tokenIndex: number
+): Decimal {
+  const balance = fpBalances.map(fromFp)[tokenIndex];
+  const weight = fromFp(fpWeights[tokenIndex]);
+
+  const exponent = decimal(1).div(weight);
+  const maxAccruedFees = balance.mul(decimal(1).sub(fromFp(fpMinInvariantRatio).pow(exponent)));
+
+  return toFp(maxAccruedFees);
+}
+
+export function calculateSpotPrice(fpBalances: BigNumberish[], fpWeights: BigNumberish[]): BigNumber {
+  const numerator = fromFp(fpBalances[0]).div(fromFp(fpWeights[0]));
+  const denominator = fromFp(fpBalances[1]).div(fromFp(fpWeights[1]));
+  return bn(toFp(numerator.div(denominator)).toFixed(0));
+}
+
+export function calculateBPTPrice(
+  fpBalance: BigNumberish,
+  fpWeight: BigNumberish,
+  totalSupply: BigNumberish
+): BigNumber {
+  return bn(toFp(fromFp(fpBalance).div(fromFp(fpWeight)).div(fromFp(totalSupply))).toFixed(0));
+}
+
+export function calcBptOutAddToken(totalSupply: BigNumberish, fpWeight: BigNumberish): BigNumber {
+  const weightSumRatio = decimal(1).div(decimal(1).sub(fromFp(fpWeight)));
+  return bn(toFp(fromFp(totalSupply).mul(weightSumRatio.sub(1))));
+}
+
+function complement(val: Decimal) {
+  return val.lt(1) ? decimal(1).sub(val) : 0;
+}
