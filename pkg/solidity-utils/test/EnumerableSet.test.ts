2e701c8601e462ee24fd2921f3f69e64cbfd0936
Jeffrey Bennett <endymionjkb@gmail.com>
2023-06-15 16:26:46 -0400

    refactor: actually, keep 2nd level for expansion

--- pkg/solidity-utils/test/EnumerableSet.test.ts
+++ pkg/solidity-utils/test/EnumerableSet.test.ts
@@ -5,17 +5,19 @@ import { shouldBehaveLikeSet } from './EnumerableSet.behavior';
 import { sharedBeforeEach } from '@balancer-labs/v3-common/sharedBeforeEach';
 
 describe('EnumerableSet', () => {
-  const members = [
-    '0x8B40ECf815AC8d53aB4AD2a00248DE77296344Db',
-    '0x638141Eb8905D9A55D81610f45bC2B47120059e7',
-    '0x7571A57e94F046725612f786Aa9bf44ce6b56894',
-  ];
+  describe('EnumerableAddressSet', () => {
+    const members = [
+      '0x8B40ECf815AC8d53aB4AD2a00248DE77296344Db',
+      '0x638141Eb8905D9A55D81610f45bC2B47120059e7',
+      '0x7571A57e94F046725612f786Aa9bf44ce6b56894',
+    ];
 
-  const store: { set?: Contract } = {};
+    const store: { set?: Contract } = {};
 
-  sharedBeforeEach(async () => {
-    store.set = await deploy('EnumerableAddressSetMock');
+    sharedBeforeEach(async () => {
+      store.set = await deploy('EnumerableAddressSetMock');
+    });
+
+    shouldBehaveLikeSet(store as { set: Contract }, members);
   });
-
-  shouldBehaveLikeSet(store as { set: Contract }, members);
 });
