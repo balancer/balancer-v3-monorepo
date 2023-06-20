cbc786e69eb96a48c6603fdc4f85a51d4942949a
Jeffrey Bennett <endymionjkb@gmail.com>
2023-06-20 09:25:32 -0400

    pragma consistency; add newline; explicit not reverted checks

--- pkg/solidity-utils/contracts/test/ReentrancyAttack.sol
+++ pkg/solidity-utils/contracts/test/ReentrancyAttack.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.4;
 
 contract ReentrancyAttack {
     function _msgSender() internal view virtual returns (address) {
