5c1d3374e85d27045791ccb43f1954046026db8f
Igor Yalovoy <to@ylv.io>
2023-07-03 17:07:31 -0600

    Add ERC721 methods

--- pkg/vault/contracts/test/VaultMock.sol
+++ pkg/vault/contracts/test/VaultMock.sol
@@ -11,7 +11,7 @@ contract VaultMock is Vault {
         uint256 pauseWindowDuration,
         uint256 bufferPeriodDuration
     ) Vault(weth, pauseWindowDuration, bufferPeriodDuration) {
-        // solhint-disable-previous-line no-empty-blocks      
+        // solhint-disable-previous-line no-empty-blocks
     }
 
     function mint(address poolToken, address to, uint256 amount) external {
@@ -20,6 +20,22 @@ contract VaultMock is Vault {
 
     function burn(address poolToken, address from, uint256 amount) external {
         _burn(poolToken, from, amount);
+    }
+
+    function mintERC721(address poolToken, address to, uint256 tokenId) external {
+        _mintERC721(poolToken, to, tokenId);
+    }
+
+    function burnERC721(address poolToken, uint256 tokenId) external {
+        _burnERC721(poolToken, tokenId);
+    }
+
+    function safeMintERC721(address poolToken, address to, uint256 tokenId) external {
+        _safeMintERC721(poolToken, msg.sender, to, tokenId);
+    }
+
+    function safeMintERC721(address poolToken, address to, uint256 tokenId, bytes memory data) external {
+        _safeMintERC721(poolToken, msg.sender, to, tokenId, data);
     }
 
     function isETH(IAsset asset) external pure returns (bool) {
@@ -38,7 +54,15 @@ contract VaultMock is Vault {
         _pause();
     }
 
+    // Used for testing the ReentrancyGuard
     function reentrantRegisterPool(address factory, IERC20[] memory tokens) external nonReentrant {
         this.registerPool(factory, tokens);
+    }
+
+    // Used for testing pool registration, which is ordinarily done in the constructor of the pool.
+    // The Mock pool has an argument for whether or not to register on deployment. To call register pool
+    // separately, deploy it with the registration flag false, then call this function.
+    function manualRegisterPool(address factory, IERC20[] memory tokens) external whenNotPaused {
+        _registerPool(factory, tokens);
     }
 }
