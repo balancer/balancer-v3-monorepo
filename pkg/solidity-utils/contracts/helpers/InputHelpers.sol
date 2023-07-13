6716b1fcbfc9739c5b89540713441323f49f6efa
Igor Yalovoy <to@ylv.io>
2023-07-13 12:04:40 -0600

    Add helpers

---
+++ pkg/solidity-utils/contracts/helpers/InputHelpers.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.4;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+library InputHelpers {
+    /**
+     * @dev 
+     */
+    error InputLengthMismatch();
+
+    /**
+     * @dev 
+     */
+    error UnsortedArray();
+
+    function ensureInputLengthMatch(address[] a, address[] b) internal pure {
+        if (a.length != b.length) {
+            revert InputLengthMismatch();
+        }
+    }
+
+    function ensureInputLengthMatch(uint256 a, uint256 b) internal pure {
+        if (a != b) {
+            revert InputLengthMismatch();
+        }
+    }
+
+    function ensureInputLengthMatch(
+        uint256 a,
+        uint256 b,
+        uint256 c
+    ) internal pure {
+        if (a != b || b != c) {
+            revert InputLengthMismatch();
+        }
+    }
+
+    function ensureArrayIsSorted(IERC20[] memory array) internal pure {
+        address[] memory addressArray;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            addressArray := array
+        }
+        ensureArrayIsSorted(addressArray);
+    }
+
+    function ensureArrayIsSorted(address[] memory array) internal pure {
+        if (array.length < 2) {
+            return;
+        }
+
+        address previous = array[0];
+        for (uint256 i = 1; i < array.length; ++i) {
+            address current = array[i];
+            if (previous > current) {
+                revert UnsortedArray();
+            }
+            previous = current;
+        }
+    }
+}
