edf50e663b51bbcd308dfa1e20f7c004c9b60eb3
Igor Yalovoy <to@ylv.io>
2023-08-08 13:29:25 -0600

    Update IBasePool.sol interface

--- pkg/interfaces/contracts/vault/IBasePool.sol
+++ pkg/interfaces/contracts/vault/IBasePool.sol
@@ -4,11 +4,6 @@ pragma solidity ^0.8.4;
 
 import "./IVault.sol";
 
-/**
- * @dev Interface for adding and removing liquidity that all Pool contracts should implement. Note that this is not
- * the complete Pool contract interface, as it is missing the swap hooks. Pool contracts should also inherit from
- * either IGeneralPool or IMinimalSwapInfoPool
- */
 interface IBasePool {
     event OnLiquidityAdded(
         address sender,
@@ -20,27 +15,6 @@ interface IBasePool {
 
     event OnLiquidityRemoved(address sender, uint256[] currentBalances, uint256 bptAmountIn, bytes userData);
 
-    /**
-     * @dev Called by the Vault when a user calls `IVault.joinPool` to add liquidity to this Pool. Returns how many of
-     * each registered token the user should provide, as well as the amount of protocol fees the Pool owes to the Vault.
-     * The Vault will then take tokens from `sender` and add them to the Pool's balances, as well as collect
-     * the reported amount in protocol fees, which the pool should calculate based on `protocolSwapFeePercentage`.
-     *
-     * Protocol fees are reported and charged on join events so that the Pool is free of debt whenever new users join.
-     *
-     * `sender` is the account performing the join (from which tokens will be withdrawn), and `recipient` is the account
-     * designated to receive any benefits (typically pool shares). `balances` contains the total balances
-     * for each token the Pool registered in the Vault, in the same order that `IVault.getPoolTokens` would return.
-     *
-     * `lastChangeBlock` is the last block in which *any* of the Pool's registered tokens last changed its total
-     * balance.
-     *
-     * `userData` contains any pool-specific instructions needed to perform the calculations, such as the type of
-     * join (e.g., proportional given an amount of pool shares, single-asset, multi-asset, etc.)
-     *
-     * Contracts implementing this function should check that the caller is indeed the Vault before performing any
-     * state-changing operations, such as minting pool shares.
-     */
     function onAddLiquidity(
         address sender,
         uint256[] memory currentBalances,
@@ -48,28 +22,6 @@ interface IBasePool {
         bytes memory userData
     ) external returns (uint256[] memory amountsIn, uint256 bptAmountOut);
 
-    /**
-     * @dev Called by the Vault when a user calls `IVault.exitPool` to remove liquidity from this Pool. Returns how many
-     * tokens the Vault should deduct from the Pool's balances, as well as the amount of protocol fees the Pool owes
-     * to the Vault. The Vault will then take tokens from the Pool's balances and send them to `recipient`,
-     * as well as collect the reported amount in protocol fees, which the Pool should calculate based on
-     * `protocolSwapFeePercentage`.
-     *
-     * Protocol fees are charged on exit events to guarantee that users exiting the Pool have paid their share.
-     *
-     * `sender` is the account performing the exit (typically the pool shareholder), and `recipient` is the account
-     * to which the Vault will send the proceeds. `balances` contains the total token balances for each token
-     * the Pool registered in the Vault, in the same order that `IVault.getPoolTokens` would return.
-     *
-     * `lastChangeBlock` is the last block in which *any* of the Pool's registered tokens last changed its total
-     * balance.
-     *
-     * `userData` contains any pool-specific instructions needed to perform the calculations, such as the type of
-     * exit (e.g., proportional given an amount of pool shares, single-asset, multi-asset, etc.)
-     *
-     * Contracts implementing this function should check that the caller is indeed the Vault before performing any
-     * state-changing operations, such as burning pool shares.
-     */
     function onRemoveLiquidity(
         address sender,
         uint256[] memory currentBalances,
@@ -78,38 +30,8 @@ interface IBasePool {
         bytes memory userData
     ) external returns (uint256[] memory amountsOut);
 
-    /**
-     * @dev This is called by the Vault when a user calls `IVault.swap` or `IVault.batchSwap` to swap with this Pool.
-     * Returns the number of tokens the Pool will grant to the user in a 'given in' swap, or that the user will
-     * grant to the pool in a 'given out' swap.
-     *
-     * This can often be implemented by a `view` function, since many pricing algorithms don't need to track state
-     * changes in swaps. However, contracts implementing this in non-view functions should check that the caller is
-     * indeed the Vault.
-     */
     function onSwap(SwapParams calldata params) external returns (uint256 amountCalculated);
 
-    // This data structure represents a request for a token swap, where `kind` indicates the swap type ('given in' or
-    // 'given out') which indicates whether or not the amount sent by the pool is known.
-    //
-    // The pool receives `tokenIn` and sends `tokenOut`. `amount` is the number of `tokenIn` tokens the pool will take
-    // in, or the number of `tokenOut` tokens the Pool will send out, depending on the given swap `kind`.
-    //
-    // All other fields are not strictly necessary for most swaps, but are provided to support advanced scenarios in
-    // some Pools.
-    //
-    // `poolId` is the ID of the Pool involved in the swap - this is useful for Pool contracts that implement more than
-    // one Pool.
-    //
-    // The meaning of `lastChangeBlock` depends on the Pool specialization:
-    //  - Two Token or Minimal Swap Info: the last block in which either `tokenIn` or `tokenOut` changed its total
-    //    balance.
-    //  - General: the last block in which *any* of the Pool's registered tokens changed its total balance.
-    //
-    // `from` is the origin address for the funds the Pool receives, and `to` is the destination address
-    // where the Pool sends the outgoing tokens.
-    //
-    // `userData` is extra data provided by the caller - typically a signature from a trusted party.
     struct SwapParams {
         IVault.SwapKind kind;
         IERC20 tokenIn;
