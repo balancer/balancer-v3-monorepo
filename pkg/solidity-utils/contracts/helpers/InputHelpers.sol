b8b5c6779eda84ecf037cd0ced67da3234ae4642
Igor Yalovoy <to@ylv.io>
2023-08-08 14:42:17 -0600

    Remove unused functions and error messages

--- pkg/solidity-utils/contracts/helpers/InputHelpers.sol
+++ pkg/solidity-utils/contracts/helpers/InputHelpers.sol
@@ -10,17 +10,6 @@ library InputHelpers {
      */
     error InputLengthMismatch();
 
-    /**
-     * @dev
-     */
-    error UnsortedArray();
-
-    function ensureInputLengthMatch(address[] memory a, address[] memory b) internal pure {
-        if (a.length != b.length) {
-            revert InputLengthMismatch();
-        }
-    }
-
     function ensureInputLengthMatch(uint256 a, uint256 b) internal pure {
         if (a != b) {
             revert InputLengthMismatch();
@@ -30,30 +19,6 @@ library InputHelpers {
     function ensureInputLengthMatch(uint256 a, uint256 b, uint256 c) internal pure {
         if (a != b || b != c) {
             revert InputLengthMismatch();
-        }
-    }
-
-    function ensureArrayIsSorted(IERC20[] memory array) internal pure {
-        address[] memory addressArray;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            addressArray := array
-        }
-        ensureArrayIsSorted(addressArray);
-    }
-
-    function ensureArrayIsSorted(address[] memory array) internal pure {
-        if (array.length < 2) {
-            return;
-        }
-
-        address previous = array[0];
-        for (uint256 i = 1; i < array.length; ++i) {
-            address current = array[i];
-            if (previous > current) {
-                revert UnsortedArray();
-            }
-            previous = current;
         }
     }
 }
