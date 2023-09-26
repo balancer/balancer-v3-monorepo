ded7cb8c9335fddbe863ed47254da0c02693c3e8
Jeffrey Bennett <endymionjkb@gmail.com>
2023-09-26 18:37:09 -0400

    feat: add authentication infrastructure

---
+++ pkg/interfaces/contracts/solidity-utils/helpers/IAuthentication.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.4;
+
+interface IAuthentication {
+    /**
+     * @dev Error indicating that the sender does not have permission to call a function.
+     */
+    error SenderNotAllowed();
+
+    /**
+     * @dev Returns the action identifier associated with the external function described by `selector`.
+     */
+    function getActionId(bytes4 selector) external view returns (bytes32);
+}
