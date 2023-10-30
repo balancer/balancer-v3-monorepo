06d8352bf49dd3ae864e4d9f02b1d7b13b80f3eb
Juan Ignacio Ubeira <juani@balancer.finance>
2023-10-30 14:47:13 -0300

    Fix build.

--- pkg/interfaces/contracts/vault/IVault.sol
+++ pkg/interfaces/contracts/vault/IVault.sol
@@ -122,7 +122,7 @@ interface IVault {
      * @dev This function assumes the default proportional liquidity methods are supported.
      * @param factory The factory address associated with the pool being registered
      * @param tokens An array of token addresses the pool will manage
-     * @param callbacks Flags indicating which callbacks the pool supports
+     * @param config Flags indicating which callbacks the pool supports
      * @param liquidityManagement Liquidity management flags with implemented methods
      */
     function registerPool(
