ad1d48d560795a91f31943e8d117e5450510ba45
Jeff Bennett <endymionjkb@gmail.com>
2023-11-18 13:29:43 -0500

    feat: 80/20 factory

---
+++ pkg/pool-weighted/contracts/WeightedPool8020Factory.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.4;
+
+import "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+
+import "@balancer-labs/v3-vault/contracts/factories/BasePoolFactory.sol";
+
+import "./WeightedPool.sol";
+
+/**
+ * @notice Weighted Pool factory for 80/20 pools.
+ */
+contract WeightedPool8020Factory is BasePoolFactory {
+    uint256 private constant _EIGHTY = 8e17; // 80%
+    uint256 private constant _TWENTY = 2e17; // 20%
+
+    constructor(IVault vault, uint256 pauseWindowDuration) BasePoolFactory(vault, pauseWindowDuration) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /**
+     * @dev Deploys a new `WeightedPool`.
+     */
+    function create(
+        string memory name,
+        string memory symbol,
+        IERC20 highWeightToken,
+        IERC20 lowWeightToken,
+        bytes32 salt
+    ) external returns (address pool) {
+        IERC20[] memory tokens = new IERC20[](2);
+        tokens[0] = highWeightToken;
+        tokens[1] = lowWeightToken;
+
+        uint256[] memory weights = new uint256[](2);
+        weights[0] = _EIGHTY;
+        weights[1] = _TWENTY;
+
+        // Passing the salt argument causes the contract to be deployed with create2.
+        pool = address(
+            new WeightedPool{ salt: salt }(
+                WeightedPool.NewPoolParams({ name: name, symbol: symbol, tokens: tokens, normalizedWeights: weights }),
+                getVault()
+            )
+        );
+
+        getVault().registerPool(
+            pool,
+            tokens,
+            getNewPoolPauseWindowEndTime(),
+            address(0), // no pause manager
+            PoolCallbacks({
+                shouldCallBeforeAddLiquidity: false,
+                shouldCallAfterAddLiquidity: false,
+                shouldCallBeforeRemoveLiquidity: false,
+                shouldCallAfterRemoveLiquidity: false,
+                shouldCallAfterSwap: false
+            }),
+            LiquidityManagement({
+                supportsAddLiquidityProportional: true,
+                supportsAddLiquiditySingleTokenExactOut: true,
+                supportsAddLiquidityUnbalanced: true,
+                supportsAddLiquidityCustom: false,
+                supportsRemoveLiquidityProportional: true,
+                supportsRemoveLiquiditySingleTokenExactIn: true,
+                supportsRemoveLiquiditySingleTokenExactOut: true,
+                supportsRemoveLiquidityCustom: false
+            })
+        );
+
+        _registerPoolWithFactory(pool);
+    }
+}
