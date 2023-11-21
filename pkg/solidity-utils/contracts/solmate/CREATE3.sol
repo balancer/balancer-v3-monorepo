46bbad5be3bc83c3df147d0ff7d3b651a64d6025
Jeff Bennett <endymionjkb@gmail.com>
2023-11-21 15:36:41 -0500

    lint

--- pkg/solidity-utils/contracts/solmate/CREATE3.sol
+++ pkg/solidity-utils/contracts/solmate/CREATE3.sol
@@ -9,6 +9,7 @@ import { Bytes32AddressLib } from "./Bytes32AddressLib.sol";
 /// @author Modified from 0xSequence (https://github.com/0xSequence/create3/blob/master/contracts/Create3.sol)
 library CREATE3 {
     using Bytes32AddressLib for bytes32;
+    // solhint-disable no-inline-assembly
 
     //--------------------------------------------------------------------------------//
     // Opcode     | Opcode + Arguments    | Description      | Stack View             //
@@ -31,12 +32,12 @@ library CREATE3 {
     // 0x60       |  0x6018               | PUSH1 18         | 24 8                   //
     // 0xf3       |  0xf3                 | RETURN           |                        //
     //--------------------------------------------------------------------------------//
-    bytes internal constant PROXY_BYTECODE = hex"67_36_3d_3d_37_36_3d_34_f0_3d_52_60_08_60_18_f3";
+    bytes internal constant _PROXY_BYTECODE = hex"67_36_3d_3d_37_36_3d_34_f0_3d_52_60_08_60_18_f3";
 
-    bytes32 internal constant PROXY_BYTECODE_HASH = keccak256(PROXY_BYTECODE);
+    bytes32 internal constant _PROXY_BYTECODE_HASH = keccak256(_PROXY_BYTECODE);
 
     function deploy(bytes32 salt, bytes memory creationCode, uint256 value) internal returns (address deployed) {
-        bytes memory proxyChildBytecode = PROXY_BYTECODE;
+        bytes memory proxyChildBytecode = _PROXY_BYTECODE;
 
         address proxy;
         /// @solidity memory-safe-assembly
@@ -66,7 +67,7 @@ library CREATE3 {
                 // Salt:
                 salt,
                 // Bytecode hash:
-                PROXY_BYTECODE_HASH
+                _PROXY_BYTECODE_HASH
             )
         ).fromLast20Bytes();
 
