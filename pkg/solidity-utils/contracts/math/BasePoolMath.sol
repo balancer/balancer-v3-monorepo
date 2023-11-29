01a15bc6d3d22be4104c62f8c35bac4091a36357
Igor Yalovoy <to@ylv.io>
2023-11-29 14:31:21 -0600

    Add computeAddLiquidityUnbalanced function to BasePoolMath for unbalanced liquidity addition with fee adjustment.

--- pkg/solidity-utils/contracts/math/BasePoolMath.sol
+++ pkg/solidity-utils/contracts/math/BasePoolMath.sol
@@ -56,4 +56,37 @@ library BasePoolMath {
             amountsOut[i] = balances[i].mulDown(bptRatio);
         }
     }
+
+    function computeAddLiquidityUnbalanced(
+        uint256[] memory oldBalances,
+        uint256[] memory exactAmounts,
+        uint256 totalSupply,
+        uint256 swapFeePercentage,
+        function(uint256[] memory) external view returns (uint256) calcInvariant
+    ) external view returns (uint256 bptAmountOut) {
+        uint256 numTokens = oldBalances.length;
+        uint256[] memory newBalances = new uint256[](numTokens);
+        for (uint256 index = 0; index < oldBalances.length; index++) {
+            newBalances[index] = oldBalances[index] + exactAmounts[index];
+        }
+        uint256 oldInvariant = calcInvariant(oldBalances);
+        console2.log("oldInvariant:", oldInvariant);
+        uint256 invariantRatio = calcInvariant(newBalances).divDown(oldInvariant);
+        console2.log("newInvraint:", calcInvariant(newBalances));
+        console2.log("invariantRatio:", invariantRatio);
+        uint256[] memory newBalancesWithFees = new uint256[](numTokens);
+        for (uint256 index = 0; index < oldBalances.length; index++) {
+            // mulUp because higher tax is more secure
+            if (invariantRatio.mulUp(oldBalances[index]) > newBalances[index]) {
+                uint256 taxableAmount = invariantRatio.mulUp(oldBalances[index]) - newBalances[index];
+                console2.log("taxableAmount:", taxableAmount);
+                newBalancesWithFees[index] = newBalances[index] - taxableAmount.divUp(swapFeePercentage);
+            } else {
+                newBalancesWithFees[index] = newBalances[index];
+            }
+        }
+        uint256 newInvariantWithFees = calcInvariant(newBalancesWithFees);
+        // mulDown/divDown because we are giving away pool tokens
+        return totalSupply.mulDown((newInvariantWithFees - oldInvariant).divDown(oldInvariant));
+    }
 }
