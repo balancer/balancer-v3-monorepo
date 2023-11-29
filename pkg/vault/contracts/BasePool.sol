64010b7baf2b9552c8ee8ae3122bfde65983cb74
Igor Yalovoy <to@ylv.io>
2023-11-29 17:21:46 -0600

    Remove onRemoveLiquiditySingleTokenExactOut callback from BasePool contract.

--- pkg/vault/contracts/BasePool.sol
+++ pkg/vault/contracts/BasePool.sol
@@ -118,16 +118,6 @@ abstract contract BasePool is IBasePool, ERC20PoolToken {
     }
 
     /// @inheritdoc IBasePool
-    function onRemoveLiquiditySingleTokenExactOut(
-        address,
-        uint256,
-        uint256,
-        uint256[] memory
-    ) external view virtual returns (uint256) {
-        revert CallbackNotImplemented();
-    }
-
-    /// @inheritdoc IBasePool
     function onRemoveLiquidityCustom(
         address,
         uint256,
