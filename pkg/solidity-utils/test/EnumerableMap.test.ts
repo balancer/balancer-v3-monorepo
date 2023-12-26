a38030f85330ed31bfa6146c3d010003707f4785
Jeff Bennett <endymionjkb@gmail.com>
2023-12-26 10:23:10 -0500

    feat: Add IERC20ToBytes32Map

--- pkg/solidity-utils/test/EnumerableMap.test.ts
+++ pkg/solidity-utils/test/EnumerableMap.test.ts
@@ -220,7 +220,7 @@ describe('EnumerableMap', () => {
       it('does not revert when accessing indexes outside of the map', async () => {
         const length = await map.length();
 
-        await expect(await map.unchecked_valueAt(length)).not.to.be.reverted;
+        expect(await map.unchecked_valueAt(length)).not.to.be.reverted;
       });
     });
 
@@ -294,15 +294,33 @@ describe('EnumerableMap', () => {
     });
   }
 
-  describe('EnumerableIERC20ToUint256Map', () => {
-    const keys = [
-      '0x8B40ECf815AC8d53aB4AD2a00248DE77296344Db',
-      '0x638141Eb8905D9A55D81610f45bC2B47120059e7',
-      '0x7571A57e94F046725612f786Aa9bf44ce6b56894',
-    ];
+  describe('EnumerableMap types', () => {
+    describe('EnumerableIERC20ToUint256Map', () => {
+      const keys = [
+        '0x8B40ECf815AC8d53aB4AD2a00248DE77296344Db',
+        '0x638141Eb8905D9A55D81610f45bC2B47120059e7',
+        '0x7571A57e94F046725612f786Aa9bf44ce6b56894',
+      ];
+  
+      const values = [42n, 1337n, 9999n];
+  
+      shouldBehaveLikeMap('IERC20ToUint256', keys, values);
+    });
 
-    const values = [42n, 1337n, 9999n];
+    describe('EnumerableIERC20ToBytes32Map', () => {
+      const keys = [
+        '0x8B40ECf815AC8d53aB4AD2a00248DE77296344Db',
+        '0x638141Eb8905D9A55D81610f45bC2B47120059e7',
+        '0x7571A57e94F046725612f786Aa9bf44ce6b56894',
+      ];
+  
+      const values = [
+        '0x41b1a0649752af1b28b3dc29a1556eee781e4a4c3a1f7f53f90fa834de098c4d',
+        '0x435cd288e3694b535549c3af56ad805c149f92961bf84a1c647f7d86fc2431b4',
+        '0xf2d05ec5c5729fb559780c70a93ca7b4ee2ca37f64e62fa31046b324f60d9447',
+      ];
 
-    shouldBehaveLikeMap('IERC20ToUint256', keys, values);
+      shouldBehaveLikeMap('IERC20ToBytes32', keys, values);
+    });
   });
 });
