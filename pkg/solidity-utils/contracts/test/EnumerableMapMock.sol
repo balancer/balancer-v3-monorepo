17b0a70f1cd2523a662274006c54e02aa56a8d82
EndymionJkb <EndymionJkb@gmail.com>
2024-01-02 17:10:57 -0500

    Token balance packing infrastructure (#209)

--- pkg/solidity-utils/contracts/test/EnumerableMapMock.sol
+++ pkg/solidity-utils/contracts/test/EnumerableMapMock.sol
@@ -57,3 +57,55 @@ contract EnumerableIERC20ToUint256MapMock {
         return _map.get(key);
     }
 }
+
+contract EnumerableIERC20ToBytes32MapMock {
+    using EnumerableMap for EnumerableMap.IERC20ToBytes32Map;
+
+    // solhint-disable func-name-mixedcase
+
+    EnumerableMap.IERC20ToBytes32Map private _map;
+
+    function contains(IERC20 key) public view returns (bool) {
+        return _map.contains(key);
+    }
+
+    function set(IERC20 key, bytes32 value) public returns (bool) {
+        return _map.set(key, value);
+    }
+
+    function unchecked_indexOf(IERC20 key) public view returns (uint256) {
+        return _map.unchecked_indexOf(key);
+    }
+
+    function indexOf(IERC20 key) public view returns (uint256) {
+        return _map.indexOf(key);
+    }
+
+    function unchecked_setAt(uint256 index, bytes32 value) public {
+        _map.unchecked_setAt(index, value);
+    }
+
+    function remove(IERC20 key) public returns (bool) {
+        return _map.remove(key);
+    }
+
+    function length() public view returns (uint256) {
+        return _map.length();
+    }
+
+    function at(uint256 index) public view returns (IERC20 key, bytes32 value) {
+        return _map.at(index);
+    }
+
+    function unchecked_at(uint256 index) public view returns (IERC20 key, bytes32 value) {
+        return _map.unchecked_at(index);
+    }
+
+    function unchecked_valueAt(uint256 index) public view returns (bytes32 value) {
+        return _map.unchecked_valueAt(index);
+    }
+
+    function get(IERC20 key) public view returns (bytes32) {
+        return _map.get(key);
+    }
+}
