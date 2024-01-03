44370c1dfbde7bf0ae8b855c61d75f5d3611124d
Igor Yalovoy <to@ylv.io>
2024-01-03 15:25:38 -0600

    Update pkg/interfaces/contracts/vault/IRouter.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/interfaces/contracts/vault/IRouter.sol
+++ pkg/interfaces/contracts/vault/IRouter.sol
@@ -42,7 +42,7 @@ interface IRouter {
      * @param exactAmountsIn Exact amounts of tokens to be added, sorted in token registration order
      * @param minBptAmountOut Minimum amount of pool tokens to be received
      * @param wethIsEth If true, incoming ETH will be wrapped to WETH; otherwise the Vault will pull WETH tokens
-     * @param userData Additional (optional) data required for adding liquidity
+     * @param userData Additional (optional) data required for adding initial liquidity
      * @return bptAmountOut Actual amount of pool tokens minted in exchange for initial liquidity
      */
     function initialize(
