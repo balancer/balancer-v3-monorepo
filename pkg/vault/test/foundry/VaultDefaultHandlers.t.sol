c0373af9f17c0af1fe404cfe424ae7929f291ce2
Juan Ignacio Ubeira <juani@balancer.finance>
2024-01-11 10:35:28 -0300

    Add unimplemented function test.

--- pkg/vault/test/foundry/VaultDefaultHandlers.t.sol
+++ pkg/vault/test/foundry/VaultDefaultHandlers.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
 
+import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/vault/IRateProvider.sol";
 import { CannotReceiveEth } from "@balancer-labs/v3-interfaces/contracts/vault/VaultErrors.sol";
 
 import { Vault } from "../../contracts/Vault.sol";
@@ -31,5 +32,10 @@ contract VaultDefaultHandlers is BaseVaultTest {
     function testDefaultHandler() public {
         bytes32 result = VaultExtensionMock(address(vault)).mockExtensionHash(bytes("V3"));
         assertEq(result, keccak256(bytes("V3")));
+    }
+
+    function testDefaultHandlerNonExistingFunction() public {
+        vm.expectRevert();
+        IRateProvider(address(vault)).getRate();
     }
 }
