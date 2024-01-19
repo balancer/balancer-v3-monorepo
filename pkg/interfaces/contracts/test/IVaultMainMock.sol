f825620948637a2cacb0a47f3f09c9b80ce98f49
Jeff Bennett <endymionjkb@gmail.com>
2024-01-19 00:35:28 -0500

    lint

--- pkg/interfaces/contracts/test/IVaultMainMock.sol
+++ pkg/interfaces/contracts/test/IVaultMainMock.sol
@@ -37,7 +37,10 @@ interface IVaultMainMock {
 
     function recoveryModeExit(address pool) external view;
 
-    function updateLiveBalancesAndComputePoolData(address pool, Rounding roundingDirection) external returns (PoolData memory);
+    function updateLiveBalancesAndComputePoolData(
+        address pool,
+        Rounding roundingDirection
+    ) external returns (PoolData memory);
 
     function getRawBalances(address pool) external view returns (uint256[] memory balancesRaw);
 }
