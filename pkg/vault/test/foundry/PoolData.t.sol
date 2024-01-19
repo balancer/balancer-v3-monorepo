5cd00183bf97d6008bb3ee5bd9351d67db90fe1a
Jeff Bennett <endymionjkb@gmail.com>
2024-01-19 17:25:36 -0500

    test: update tests

--- pkg/vault/test/foundry/PoolData.t.sol
+++ pkg/vault/test/foundry/PoolData.t.sol
@@ -57,9 +57,9 @@ contract PoolDataTest is BaseVaultTest {
         daiRateProvider.mockRate(daiRate);
         wstETHRateProvider.mockRate(wstETHRate);
 
-        // `updateLiveBalancesAndComputePoolData` and `getRawBalances` are functions in VaultMock.
+        // `computePoolDataUpdatingBalancesAndFees` and `getRawBalances` are functions in VaultMock.
 
-        PoolData memory data = vault.updateLiveBalancesAndComputePoolData(address(pool), roundUp ? Rounding.ROUND_UP : Rounding.ROUND_DOWN);
+        PoolData memory data = vault.computePoolDataUpdatingBalancesAndFees(address(pool), roundUp ? Rounding.ROUND_UP : Rounding.ROUND_DOWN);
 
         // Compute decimal scaling factors from the tokens, in the mock.
         uint256[] memory expectedScalingFactors = PoolMock(pool).getDecimalScalingFactors();
