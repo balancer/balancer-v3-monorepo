608b64ff01e26c12e834f0341f9bc314353a4cca
Juan Ignacio Ubeira <juani@balancer.finance>
2024-01-22 13:05:09 -0300

    Config overides.

--- pvt/common/hardhat-base-config.ts
+++ pvt/common/hardhat-base-config.ts
@@ -22,6 +22,25 @@ export const compilers: [SolcConfig] = [
   },
 ];
 
+type ContractSettings = Record<
+  string,
+  {
+    version: string;
+    runs: number;
+  }
+>;
+
+const contractSettings: ContractSettings = {
+  '@balancer-labs/v3-vault/contracts/Vault.sol': {
+    version: '0.8.21',
+    runs: 500,
+  },
+  '@balancer-labs/v3-vault/contracts/VaultExtension.sol': {
+    version: '0.8.21',
+    runs: 500,
+  },
+};
+
 export const warnings = {
   // Ignore code-size in test files: mocks may make contracts not deployable on real networks, but we don't care about
   // that.
@@ -35,4 +54,22 @@ export const warnings = {
     'shadowing-opcode': 'off',
     default: 'error',
   },
+};
+
+export const overrides = (packageName: string): Record<string, SolcConfig> => {
+  const overrides: Record<string, SolcConfig> = {};
+
+  for (const contract of Object.keys(contractSettings)) {
+    overrides[contract.replace(`${packageName}/`, '')] = {
+      version: contractSettings[contract].version,
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: contractSettings[contract].runs,
+        },
+      },
+    };
+  }
+
+  return overrides;
 };
