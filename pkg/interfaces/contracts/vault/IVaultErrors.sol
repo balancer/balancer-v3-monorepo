db06a9942a05e4c3c09dcaf6554b7f5c8c6941e4
Jeff Bennett <endymionjkb@gmail.com>
2024-01-25 16:54:56 -0500

    chore: switch order of errors

--- pkg/interfaces/contracts/vault/IVaultErrors.sol
+++ pkg/interfaces/contracts/vault/IVaultErrors.sol
@@ -136,12 +136,12 @@ interface IVaultErrors {
                                      Fees
     *******************************************************************************/
 
-    /// @dev Error raised when the protocol yield fee percentage exceeds the maximum allowed value.
-    error ProtocolYieldFeePercentageTooHigh();
-    
     /// @dev Error raised when the protocol swap fee percentage exceeds the maximum allowed value.
     error ProtocolSwapFeePercentageTooHigh();
 
+    /// @dev Error raised when the protocol yield fee percentage exceeds the maximum allowed value.
+    error ProtocolYieldFeePercentageTooHigh();
+    
     /// @dev Error raised when the swap fee percentage exceeds the maximum allowed value.
     error SwapFeePercentageTooHigh();
 
