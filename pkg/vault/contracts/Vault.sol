dc18af4435b3a7e8a301ea011b9aefa3c4be53db
Juan Ignacio Ubeira <juani@balancer.finance>
2024-01-30 13:40:02 -0300

    Add withHandler to remove liquidity.

--- pkg/vault/contracts/Vault.sol
+++ pkg/vault/contracts/Vault.sol
@@ -640,6 +640,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
         RemoveLiquidityParams memory params
     )
         external
+        withHandler
         withInitializedPool(params.pool)
         whenPoolNotPaused(params.pool)
         returns (uint256 bptAmountIn, uint256[] memory amountsOut, bytes memory returnData)
@@ -711,6 +712,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
         uint256 exactBptAmountIn
     )
         external
+        withHandler
         nonReentrant
         withInitializedPool(pool)
         onlyInRecoveryMode(pool)
