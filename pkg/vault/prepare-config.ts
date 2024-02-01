f2b584a993914020ff3e4ea0c752a5678643e1ef
Jeff Bennett <endymionjkb@gmail.com>
2024-02-01 16:18:32 -0500

    flail: add rpc setup in .toml; include export in .env

--- pkg/vault/prepare-config.ts
+++ pkg/vault/prepare-config.ts
@@ -5,7 +5,7 @@ import { mkdirSync, writeFileSync } from 'fs';
 const HH_CONFIG_FILENAME = `${homedir()}/.env`;
 
 if (process.env.CI) {
-  const content = `MAINNET_RPC_URL="${process.env.MAINNET_RPC_URL}"`;
+  const content = `export MAINNET_RPC_URL="${process.env.MAINNET_RPC_URL}"`;
 
   mkdirSync(path.dirname(HH_CONFIG_FILENAME), { recursive: true });
   writeFileSync(HH_CONFIG_FILENAME, content);
