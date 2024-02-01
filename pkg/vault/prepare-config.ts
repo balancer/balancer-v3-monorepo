b33b7ce72ecf4a30d8e4ec16036dd9bf2c67b61c
Jeff Bennett <endymionjkb@gmail.com>
2024-02-01 16:23:57 -0500

    refactor: rename HH to ENV

--- pkg/vault/prepare-config.ts
+++ pkg/vault/prepare-config.ts
@@ -2,11 +2,11 @@ import path from 'path';
 import { homedir } from 'os';
 import { mkdirSync, writeFileSync } from 'fs';
 
-const HH_CONFIG_FILENAME = `${homedir()}/.env`;
+const ENV_CONFIG_FILENAME = `${homedir()}/.env`;
 
 if (process.env.CI) {
   const content = `export MAINNET_RPC_URL="${process.env.MAINNET_RPC_URL}"`;
 
-  mkdirSync(path.dirname(HH_CONFIG_FILENAME), { recursive: true });
-  writeFileSync(HH_CONFIG_FILENAME, content);
+  mkdirSync(path.dirname(ENV_CONFIG_FILENAME), { recursive: true });
+  writeFileSync(ENV_CONFIG_FILENAME, content);
 }
