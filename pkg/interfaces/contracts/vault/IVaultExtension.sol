478072090ecd1c6fac813f8a6bd1e67f2ac93661
daniel <91405705+danielmkm@users.noreply.github.com>
2024-02-05 13:39:59 +0100

    Fix the interface signature for registerPool (#275)

--- pkg/interfaces/contracts/vault/IVaultExtension.sol
+++ pkg/interfaces/contracts/vault/IVaultExtension.sol
@@ -104,7 +104,7 @@ interface IVaultExtension {
      * If the zero address is provided for the `pauseManager`, permissions for pausing the pool will default to the
      * authorizer.
      *
-     * @param factory The factory address associated with the pool being registered
+     * @param pool The address of the pool being registered
      * @param tokenConfig An array of descriptors for the tokens the pool will manage
      * @param pauseWindowEndTime The timestamp after which it is no longer possible to pause the pool
      * @param pauseManager Optional contract the Vault will allow to pause the pool
@@ -112,7 +112,7 @@ interface IVaultExtension {
      * @param liquidityManagement Liquidity management flags with implemented methods
      */
     function registerPool(
-        address factory,
+        address pool,
         TokenConfig[] memory tokenConfig,
         uint256 pauseWindowEndTime,
         address pauseManager,
