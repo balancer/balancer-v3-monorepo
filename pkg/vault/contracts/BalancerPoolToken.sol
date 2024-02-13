7b5a1df777c9ffbcad1f4ecb4b8c6223deb71000
Igor Yalovoy <to@ylv.io>
2024-02-12 20:39:24 -0600

    Add ERC-20 Permit extension to BalancerPoolToken for signature-based approvals and update tests accordingly

--- pkg/vault/contracts/BalancerPoolToken.sol
+++ pkg/vault/contracts/BalancerPoolToken.sol
@@ -3,6 +3,10 @@
 pragma solidity ^0.8.4;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import { Nonces } from "@openzeppelin/contracts/utils/Nonces.sol";
+import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
@@ -11,8 +15,24 @@ import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaul
 /**
  * @notice A fully ERC20-compatible token to be used as the base contract for Balancer Pools,
  * with all the data and implementation delegated to the ERC20Multitoken contract.
+
+ * @dev Implementation of the ERC-20 Permit extension allowing approvals to be made via signatures, as defined in
+ * https://eips.ethereum.org/EIPS/eip-2612[ERC-2612].
  */
-contract BalancerPoolToken is IERC20, IERC20Metadata {
+contract BalancerPoolToken is IERC20, IERC20Metadata, IERC20Permit, EIP712, Nonces {
+    bytes32 private constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    /**
+     * @dev Permit deadline has expired.
+     */
+    error ERC2612ExpiredSignature(uint256 deadline);
+
+    /**
+     * @dev Mismatched signature.
+     */
+    error ERC2612InvalidSigner(address signer, address owner);
+
     IVault private immutable _vault;
 
     string private _name;
@@ -25,7 +45,7 @@ contract BalancerPoolToken is IERC20, IERC20Metadata {
         _;
     }
 
-    constructor(IVault vault_, string memory name_, string memory symbol_) {
+    constructor(IVault vault_, string memory name_, string memory symbol_) EIP712(name_, "1") {
         _vault = vault_;
         _name = name_;
         _symbol = symbol_;
@@ -102,5 +122,42 @@ contract BalancerPoolToken is IERC20, IERC20Metadata {
     /// @dev Emit the Approval event. This function can only be called by the MultiToken.
     function emitApproval(address owner, address spender, uint256 amount) external onlyVault {
         emit Approval(owner, spender, amount);
+    }
+
+    // @inheritdoc IERC20Permit
+    function permit(
+        address owner,
+        address spender,
+        uint256 amount,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public virtual {
+        if (block.timestamp > deadline) {
+            revert ERC2612ExpiredSignature(deadline);
+        }
+
+        bytes32 structHash = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, amount, _useNonce(owner), deadline));
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        address signer = ECDSA.recover(hash, v, r, s);
+        if (signer != owner) {
+            revert ERC2612InvalidSigner(signer, owner);
+        }
+
+        _vault.approve(owner, spender, amount);
+    }
+
+    // @inheritdoc IERC20Permit
+    function nonces(address owner) public view virtual override(IERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+
+    // @inheritdoc IERC20Permit
+    // solhint-disable-next-line func-name-mixedcase
+    function DOMAIN_SEPARATOR() external view virtual returns (bytes32) {
+        return _domainSeparatorV4();
     }
 }
--- pkg/vault/test/foundry/BalancerPoolTokenTest.t.sol
+++ pkg/vault/test/foundry/BalancerPoolTokenTest.t.sol
@@ -15,11 +15,17 @@ import { BaseVaultTest } from "./utils/BaseVaultTest.sol";
 
 contract BalancerPoolTokenTest is BaseVaultTest {
     using ArrayHelpers for *;
-    PoolMock internal poolMock;
+    PoolMock internal token;
+
+    bytes32 private constant PERMIT_TYPEHASH =
+        keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+
+    uint256 internal privateKey = 0xBEEF;
+    address user = vm.addr(privateKey);
 
     function setUp() public virtual override {
         BaseVaultTest.setUp();
-        poolMock = PoolMock(pool);
+        token = PoolMock(pool);
     }
 
     function initPool() internal override {
@@ -27,15 +33,15 @@ contract BalancerPoolTokenTest is BaseVaultTest {
     }
 
     function testMetadata() public {
-        assertEq(poolMock.name(), "ERC20 Pool");
-        assertEq(poolMock.symbol(), "ERC20POOL");
-        assertEq(poolMock.decimals(), 18);
+        assertEq(token.name(), "ERC20 Pool");
+        assertEq(token.symbol(), "ERC20POOL");
+        assertEq(token.decimals(), 18);
     }
 
     function testMint() public {
-        vault.mintERC20(address(poolMock), address(0xBEEF), defaultAmount);
+        vault.mintERC20(address(token), address(0xBEEF), defaultAmount);
 
-        assertEq(poolMock.balanceOf(address(0xBEEF)), defaultAmount);
+        assertEq(token.balanceOf(address(0xBEEF)), defaultAmount);
     }
 
     function testMintMinimum() public {
@@ -47,7 +53,7 @@ contract BalancerPoolTokenTest is BaseVaultTest {
         vault.mintERC20(address(pool), address(0xBEEF), defaultAmount);
         vault.burnERC20(address(pool), address(0xBEEF), defaultAmount - 1e6);
 
-        assertEq(poolMock.balanceOf(address(0xBEEF)), 1e6);
+        assertEq(token.balanceOf(address(0xBEEF)), 1e6);
     }
 
     function testBurnMinimum() public {
@@ -60,19 +66,19 @@ contract BalancerPoolTokenTest is BaseVaultTest {
     function testApprove() public {
         vault.mintERC20(address(pool), address(this), defaultAmount);
 
-        poolMock.approve(address(0xBEEF), defaultAmount);
+        token.approve(address(0xBEEF), defaultAmount);
 
-        assertEq(poolMock.allowance(address(this), address(0xBEEF)), defaultAmount);
+        assertEq(token.allowance(address(this), address(0xBEEF)), defaultAmount);
     }
 
     function testTransfer() public {
         vault.mintERC20(address(pool), address(this), 1e18);
 
-        assertTrue(poolMock.transfer(address(0xBEEF), 1e18));
-        assertEq(poolMock.totalSupply(), 1e18);
+        assertTrue(token.transfer(address(0xBEEF), 1e18));
+        assertEq(token.totalSupply(), 1e18);
 
-        assertEq(poolMock.balanceOf(address(this)), 0);
-        assertEq(poolMock.balanceOf(address(0xBEEF)), 1e18);
+        assertEq(token.balanceOf(address(this)), 0);
+        assertEq(token.balanceOf(address(0xBEEF)), 1e18);
     }
 
     function testTransferFrom() public {
@@ -81,13 +87,13 @@ contract BalancerPoolTokenTest is BaseVaultTest {
         vault.mintERC20(address(pool), address(from), defaultAmount);
 
         vm.prank(from);
-        poolMock.approve(address(this), defaultAmount);
+        token.approve(address(this), defaultAmount);
 
-        poolMock.transferFrom(from, address(0xBEEF), defaultAmount);
+        token.transferFrom(from, address(0xBEEF), defaultAmount);
 
-        assertEq(poolMock.allowance(from, address(0xBEEF)), 0);
-        assertEq(poolMock.balanceOf(address(0xBEEF)), defaultAmount);
-        assertEq(poolMock.balanceOf(from), 0);
+        assertEq(token.allowance(from, address(0xBEEF)), 0);
+        assertEq(token.balanceOf(address(0xBEEF)), defaultAmount);
+        assertEq(token.balanceOf(from), 0);
     }
 
     function testMintToZero() public {
@@ -101,6 +107,199 @@ contract BalancerPoolTokenTest is BaseVaultTest {
         vm.expectRevert(
             abi.encodeWithSelector(IERC20Errors.ERC20InsufficientAllowance.selector, address(this), 0, defaultAmount)
         );
-        poolMock.transferFrom(address(this), address(0), defaultAmount);
+        token.transferFrom(address(this), address(0), defaultAmount);
+    }
+
+    function testPermit() public {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privateKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, user, address(0xCAFE), 1e18, 0, block.timestamp))
+                )
+            )
+        );
+
+        token.permit(user, address(0xCAFE), 1e18, block.timestamp, v, r, s);
+
+        assertEq(token.allowance(user, address(0xCAFE)), 1e18);
+        assertEq(token.nonces(user), 1);
+    }
+
+    function testFailPermitBadNonce() public {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privateKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, user, address(0xCAFE), 1e18, 1, block.timestamp))
+                )
+            )
+        );
+
+        token.permit(user, address(0xCAFE), 1e18, block.timestamp, v, r, s);
+    }
+
+    function testFailPermitBadDeadline() public {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privateKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, user, address(0xCAFE), 1e18, 0, block.timestamp))
+                )
+            )
+        );
+
+        token.permit(user, address(0xCAFE), 1e18, block.timestamp + 1, v, r, s);
+    }
+
+    function testFailPermitPastDeadline() public {
+        uint256 oldTimestamp = block.timestamp;
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privateKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, user, address(0xCAFE), 1e18, 0, oldTimestamp))
+                )
+            )
+        );
+
+        vm.warp(block.timestamp + 1);
+        token.permit(user, address(0xCAFE), 1e18, oldTimestamp, v, r, s);
+    }
+
+    function testFailPermitReplay() public {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privateKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, user, address(0xCAFE), 1e18, 0, block.timestamp))
+                )
+            )
+        );
+
+        token.permit(user, address(0xCAFE), 1e18, block.timestamp, v, r, s);
+        token.permit(user, address(0xCAFE), 1e18, block.timestamp, v, r, s);
+    }
+
+    function testPermit__Fuzz(uint248 privKey, address to, uint256 amount, uint256 deadline) public {
+        if (deadline < block.timestamp) deadline = block.timestamp;
+        if (privKey == 0) privKey = 1;
+        if (to == address(0)) to = address(1);
+
+        address usr = vm.addr(privKey);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, usr, to, amount, 0, deadline))
+                )
+            )
+        );
+
+        token.permit(usr, to, amount, deadline, v, r, s);
+
+        assertEq(token.allowance(usr, to), amount);
+        assertEq(token.nonces(usr), 1);
+    }
+
+    function testFailPermitBadNonce__Fuzz(
+        uint256 privKey,
+        address to,
+        uint256 amount,
+        uint256 deadline,
+        uint256 nonce
+    ) public {
+        if (deadline < block.timestamp) deadline = block.timestamp;
+        if (privKey == 0) privKey = 1;
+        if (nonce == 0) nonce = 1;
+
+        address usr = vm.addr(privKey);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, usr, to, amount, nonce, deadline))
+                )
+            )
+        );
+
+        token.permit(usr, to, amount, deadline, v, r, s);
+    }
+
+    function testFailPermitBadDeadline__Fuzz(uint256 privKey, address to, uint256 amount, uint256 deadline) public {
+        if (deadline < block.timestamp) deadline = block.timestamp;
+        if (privKey == 0) privKey = 1;
+
+        address usr = vm.addr(privKey);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, usr, to, amount, 0, deadline))
+                )
+            )
+        );
+
+        token.permit(usr, to, amount, deadline + 1, v, r, s);
+    }
+
+    function testFailPermitPastDeadline__Fuzz(uint256 privKey, address to, uint256 amount, uint256 deadline) public {
+        deadline = bound(deadline, 0, block.timestamp - 1);
+        if (privKey == 0) privKey = 1;
+
+        address usr = vm.addr(privKey);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, usr, to, amount, 0, deadline))
+                )
+            )
+        );
+
+        token.permit(usr, to, amount, deadline, v, r, s);
+    }
+
+    function testFailPermitReplay__Fuzz(uint256 privKey, address to, uint256 amount, uint256 deadline) public {
+        if (deadline < block.timestamp) deadline = block.timestamp;
+        if (privKey == 0) privKey = 1;
+
+        address usr = vm.addr(privKey);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+            privKey,
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    token.DOMAIN_SEPARATOR(),
+                    keccak256(abi.encode(PERMIT_TYPEHASH, usr, to, amount, 0, deadline))
+                )
+            )
+        );
+
+        token.permit(usr, to, amount, deadline, v, r, s);
+        token.permit(usr, to, amount, deadline, v, r, s);
     }
 }
