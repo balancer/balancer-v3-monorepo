0c03421703f3bce0500cf27435df98da4565dcde
Jeff Bennett <endymionjkb@gmail.com>
2024-02-13 16:25:23 -0500

    refactor: remove unused imports

--- pkg/interfaces/contracts/test/IVaultMock.sol
+++ pkg/interfaces/contracts/test/IVaultMock.sol
@@ -3,15 +3,12 @@
 pragma solidity ^0.8.4;
 
 import { IERC20Errors } from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IAuthentication } from "../solidity-utils/helpers/IAuthentication.sol";
 import { IVault } from "../vault/IVault.sol";
 import { IVaultEvents } from "../vault/IVaultEvents.sol";
 import { IVaultMainMock } from "./IVaultMainMock.sol";
 import { IVaultExtensionMock } from "./IVaultExtensionMock.sol";
-import { TokenType, TokenConfig } from "../vault/VaultTypes.sol";
-import { IRateProvider } from "../vault/IRateProvider.sol";
 
 /// @dev One-fits-all solution for hardhat tests. Use the typechain type for errors, events and functions.
 interface IVaultMock is IVault, IVaultMainMock, IVaultExtensionMock, IERC20Errors, IAuthentication {
