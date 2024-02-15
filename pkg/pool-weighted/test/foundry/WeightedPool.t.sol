9838220da5071b3513e9f3014726fc90fe59e440
Igor Yalovoy <to@ylv.io>
2024-02-15 17:10:26 -0300

    Support ERC20Permit for BalancerPoolToken  (#292)

--- pkg/pool-weighted/test/foundry/WeightedPool.t.sol
+++ pkg/pool-weighted/test/foundry/WeightedPool.t.sol
@@ -86,8 +86,8 @@ contract WeightedPoolTest is BaseVaultTest {
         );
 
         assertFalse(paused);
-        assertApproxEqAbs(pauseWindow, 365 days, 1);
-        assertApproxEqAbs(bufferPeriod, 365 days + 30 days, 1);
+        assertApproxEqAbs(pauseWindow, START_TIMESTAMP + 365 days, 1);
+        assertApproxEqAbs(bufferPeriod, START_TIMESTAMP + 365 days + 30 days, 1);
         assertEq(pauseManager, address(0));
     }
 
