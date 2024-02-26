42afc728aedfecfb049964fe71697251b07ad1e0
João Bruno <joaobrunoah@gmail.com>
2024-02-26 18:55:38 -0300

    Removing unused console import

--- pkg/vault/test/foundry/fork/ERC4626RebalanceRateValidation.t.sol
+++ pkg/vault/test/foundry/fork/ERC4626RebalanceRateValidation.t.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
