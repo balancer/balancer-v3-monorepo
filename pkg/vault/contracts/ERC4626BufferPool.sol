5b2f384650e297c335d304af365c923884e32ffc
João Bruno <joaobrunoah@gmail.com>
2024-02-29 19:08:14 -0300

    Removing console.sol import

--- pkg/vault/contracts/ERC4626BufferPool.sol
+++ pkg/vault/contracts/ERC4626BufferPool.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.4;
 
-import "forge-std/console.sol";
-
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
