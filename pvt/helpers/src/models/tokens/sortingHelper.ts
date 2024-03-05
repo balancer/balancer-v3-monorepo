8c12e8624641e0b2bc3202f177d9188e844e815e
Jeff Bennett <endymionjkb@gmail.com>
2024-03-05 13:43:48 -0500

    test: port token sorting helper function from V2

---
+++ pvt/helpers/src/models/tokens/sortingHelper.ts
@@ -0,0 +1,18 @@
+export function assert(condition: unknown, error: string): asserts condition {
+  if (condition == false) throw new Error(error);
+}
+
+const cmpTokens = (tokenA: string, tokenB: string): number => (tokenA.toLowerCase() > tokenB.toLowerCase() ? 1 : -1);
+
+const transposeMatrix = (matrix: unknown[][]): unknown[][] =>
+  matrix[0].map((_, columnIndex) => matrix.map((row) => row[columnIndex]));
+
+export function sortTokens(tokens: string[], ...others: unknown[][]): [string[], ...unknown[][]] {
+  others.forEach((array) => assert(tokens.length === array.length, 'array length mismatch'));
+
+  const transpose = transposeMatrix([tokens, ...others]) as [string, ...unknown[]][];
+  const sortedTranspose = transpose.sort(([tokenA], [tokenB]) => cmpTokens(tokenA, tokenB));
+  const [sortedTokens, ...sortedOthers] = transposeMatrix(sortedTranspose) as [string[], ...unknown[][]];
+
+  return [sortedTokens, ...sortedOthers];
+}
