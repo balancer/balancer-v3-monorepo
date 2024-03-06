0d297873d863a9f081da5cbc2df04b42230837a0
Jeff Bennett <endymionjkb@gmail.com>
2024-03-06 00:52:28 -0500

    checkpoint: starting point of port

---
+++ pkg/pool-stable/contracts/StablePool.sol
@@ -0,0 +1,227 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.4;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
+import { SwapKind } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+import { IBasePool } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePool.sol";
+import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/vault/IRateProvider.sol";
+
+import { BalancerPoolToken } from "@balancer-labs/v3-vault/contracts/BalancerPoolToken.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+import { StableMath } from "@balancer-labs/v3-solidity-utils/contracts/math/StableMath.sol";
+import { InputHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/InputHelpers.sol";
+
+// https://etherscan.deth.net/address/0xc66Ba2B6595D3613CCab350C886aCE23866EDe24
+
+/// @notice Basic Stable Pool.
+contract StablePool is IBasePool, BalancerPoolToken {
+    uint256 private immutable _totalTokens;
+
+    // This contract uses timestamps to slowly update its Amplification parameter over time. These changes must occur
+    // over a minimum time period much larger than the blocktime, making timestamp manipulation a non-issue.
+    // solhint-disable not-rely-on-time
+
+    // Amplification factor changes must happen over a minimum period of one day, and can at most divide or multiple the
+    // current value by 2 every day.
+    // WARNING: this only limits *a single* amplification change to have a maximum rate of change of twice the original
+    // value daily. It is possible to perform multiple amplification changes in sequence to increase this value more
+    // rapidly: for example, by doubling the value every day it can increase by a factor of 8 over three days (2^3).
+    uint256 private constant _MIN_UPDATE_TIME = 1 days;
+    uint256 private constant _MAX_AMP_UPDATE_DAILY_RATE = 2;
+
+    bytes32 private _packedAmplificationData;
+
+    event AmpUpdateStarted(uint256 startValue, uint256 endValue, uint256 startTime, uint256 endTime);
+    event AmpUpdateStopped(uint256 currentValue);
+
+    struct NewPoolParams {
+        string name;
+        string symbol;
+        IERC20[] tokens;
+        IRateProvider[] rateProviders;
+        uint256 amplificationParameter;
+    }
+
+    constructor(NewPoolParams memory params, IVault vault) BalancerPoolToken(vault, params.name, params.symbol) {
+        uint256 numTokens = params.tokens.length;
+
+        _totalTokens = numTokens;
+
+        _require(amplificationParameter >= _MIN_AMP, Errors.MIN_AMP);
+        _require(amplificationParameter <= _MAX_AMP, Errors.MAX_AMP);
+
+        uint256 initialAmp = Math.mul(amplificationParameter, _AMP_PRECISION);
+        _setAmplificationData(initialAmp);
+    }
+
+    /// @inheritdoc IBasePool
+    function getPoolTokens() public view returns (IERC20[] memory tokens) {
+        return getVault().getPoolTokens(address(this));
+    }
+
+    /// @inheritdoc IBasePool
+    function computeInvariant(uint256[] memory balancesLiveScaled18) public view returns (uint256) {
+        (uint256 currentAmp, ) = _getAmplificationParameter();
+        
+        return StableMath._calculateInvariant(currentAmp, balances, true);
+    }
+
+    /// @inheritdoc IBasePool
+    function computeBalance(
+        uint256[] memory balancesLiveScaled18,
+        uint256 tokenInIndex,
+        uint256 invariantRatio
+    ) external view returns (uint256 newBalance) {
+        (uint256 currentAmp, ) = _getAmplificationParameter();
+
+        return StableMath._getTokenBalanceGivenInvariantAndAllOtherBalances(currentAmp, balancesLiveScaled18, invariantRatio, tokenInIndex);
+    }
+
+    /// @inheritdoc IBasePool
+    function onSwap(IBasePool.SwapParams memory request) public view onlyVault returns (uint256) {
+        uint256 balanceTokenInScaled18 = request.balancesScaled18[request.indexIn];
+        uint256 balanceTokenOutScaled18 = request.balancesScaled18[request.indexOut];
+
+        if (request.kind == SwapKind.EXACT_IN) {
+            uint256 amountOutScaled18 = WeightedMath.computeOutGivenExactIn(
+                balanceTokenInScaled18,
+                _getNormalizedWeight(request.indexIn),
+                balanceTokenOutScaled18,
+                _getNormalizedWeight(request.indexOut),
+                request.amountGivenScaled18
+            );
+
+            return amountOutScaled18;
+        } else {
+            uint256 amountInScaled18 = WeightedMath.computeInGivenExactOut(
+                balanceTokenInScaled18,
+                _getNormalizedWeight(request.indexIn),
+                balanceTokenOutScaled18,
+                _getNormalizedWeight(request.indexOut),
+                request.amountGivenScaled18
+            );
+
+            // Fees are added after scaling happens, to reduce the complexity of the rounding direction analysis.
+            return amountInScaled18;
+        }
+    }
+
+    /**
+     * @dev Begins changing the amplification parameter to `rawEndValue` over time. The value will change linearly until
+     * `endTime` is reached, when it will be `rawEndValue`.
+     *
+     * NOTE: Internally, the amplification parameter is represented using higher precision. The values returned by
+     * `getAmplificationParameter` have to be corrected to account for this when comparing to `rawEndValue`.
+     */
+    function startAmplificationParameterUpdate(uint256 rawEndValue, uint256 endTime) external authenticate {
+        _require(rawEndValue >= _MIN_AMP, Errors.MIN_AMP);
+        _require(rawEndValue <= _MAX_AMP, Errors.MAX_AMP);
+
+        uint256 duration = Math.sub(endTime, block.timestamp);
+        _require(duration >= _MIN_UPDATE_TIME, Errors.AMP_END_TIME_TOO_CLOSE);
+
+        (uint256 currentValue, bool isUpdating) = _getAmplificationParameter();
+        _require(!isUpdating, Errors.AMP_ONGOING_UPDATE);
+
+        uint256 endValue = Math.mul(rawEndValue, _AMP_PRECISION);
+
+        // daily rate = (endValue / currentValue) / duration * 1 day
+        // We perform all multiplications first to not reduce precision, and round the division up as we want to avoid
+        // large rates. Note that these are regular integer multiplications and divisions, not fixed point.
+        uint256 dailyRate = endValue > currentValue
+            ? Math.divUp(Math.mul(1 days, endValue), Math.mul(currentValue, duration))
+            : Math.divUp(Math.mul(1 days, currentValue), Math.mul(endValue, duration));
+        _require(dailyRate <= _MAX_AMP_UPDATE_DAILY_RATE, Errors.AMP_RATE_TOO_HIGH);
+
+        _setAmplificationData(currentValue, endValue, block.timestamp, endTime);
+    }
+
+    /**
+     * @dev Stops the amplification parameter change process, keeping the current value.
+     */
+    function stopAmplificationParameterUpdate() external authenticate {
+        (uint256 currentValue, bool isUpdating) = _getAmplificationParameter();
+        _require(isUpdating, Errors.AMP_NO_ONGOING_UPDATE);
+
+        _setAmplificationData(currentValue);
+    }
+
+    function getAmplificationParameter()
+        external
+        view
+        returns (
+            uint256 value,
+            bool isUpdating,
+            uint256 precision
+        )
+    {
+        (value, isUpdating) = _getAmplificationParameter();
+        precision = _AMP_PRECISION;
+    }
+
+    function _getAmplificationParameter() internal view returns (uint256 value, bool isUpdating) {
+        (uint256 startValue, uint256 endValue, uint256 startTime, uint256 endTime) = _getAmplificationData();
+
+        // Note that block.timestamp >= startTime, since startTime is set to the current time when an update starts
+
+        if (block.timestamp < endTime) {
+            isUpdating = true;
+
+            // We can skip checked arithmetic as:
+            //  - block.timestamp is always larger or equal to startTime
+            //  - endTime is alawys larger than startTime
+            //  - the value delta is bounded by the largest amplification paramater, which never causes the
+            //    multiplication to overflow.
+            // This also means that the following computation will never revert nor yield invalid results.
+            if (endValue > startValue) {
+                value = startValue + ((endValue - startValue) * (block.timestamp - startTime)) / (endTime - startTime);
+            } else {
+                value = startValue - ((startValue - endValue) * (block.timestamp - startTime)) / (endTime - startTime);
+            }
+        } else {
+            isUpdating = false;
+            value = endValue;
+        }
+    }
+
+    function _setAmplificationData(uint256 value) private {
+        _setAmplificationData(value, value, block.timestamp, block.timestamp);
+
+        emit AmpUpdateStopped(value);
+    }
+
+    function _setAmplificationData(
+        uint256 startValue,
+        uint256 endValue,
+        uint256 startTime,
+        uint256 endTime
+    ) private {
+        _packedAmplificationData =
+            WordCodec.encodeUint(uint64(startValue), 0) |
+            WordCodec.encodeUint(uint64(endValue), 64) |
+            WordCodec.encodeUint(uint64(startTime), 64 * 2) |
+            WordCodec.encodeUint(uint64(endTime), 64 * 3);
+
+        emit AmpUpdateStarted(startValue, endValue, startTime, endTime);
+    }
+
+    function _getAmplificationData()
+        private
+        view
+        returns (
+            uint256 startValue,
+            uint256 endValue,
+            uint256 startTime,
+            uint256 endTime
+        )
+    {
+        startValue = _packedAmplificationData.decodeUint64(0);
+        endValue = _packedAmplificationData.decodeUint64(64);
+        startTime = _packedAmplificationData.decodeUint64(64 * 2);
+        endTime = _packedAmplificationData.decodeUint64(64 * 3);
+    }
+}
