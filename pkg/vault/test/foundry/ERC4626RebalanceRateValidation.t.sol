997d22dbfa9f6981b3f06decf71b2a441861395e
João Bruno <joaobrunoah@gmail.com>
2024-03-06 16:51:34 -0300

    Transforming forked rate tests into normal tests (cheaper)

--- pkg/vault/test/foundry/ERC4626RebalanceRateValidation.t.sol
+++ pkg/vault/test/foundry/ERC4626RebalanceRateValidation.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 
 import "forge-std/Test.sol";
 
+import { ERC20TestToken } from "@balancer-labs/v3-solidity-utils/contracts/test/ERC20TestToken.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
@@ -34,9 +35,9 @@ import { VaultMock } from "@balancer-labs/v3-vault/contracts/test/VaultMock.sol"
 import { PoolConfigBits, PoolConfigLib } from "@balancer-labs/v3-vault/contracts/lib/PoolConfigLib.sol";
 import { BaseVaultTest } from "@balancer-labs/v3-vault/test/foundry/utils/BaseVaultTest.sol";
 
-import { ERC4626BufferPoolFactoryMock } from "../utils/ERC4626BufferPoolFactoryMock.sol";
-import { ERC4626BufferPoolMock } from "../utils/ERC4626BufferPoolMock.sol";
-import "../utils/ERC4626TokenMock.sol";
+import { ERC4626BufferPoolFactoryMock } from "./utils/ERC4626BufferPoolFactoryMock.sol";
+import { ERC4626BufferPoolMock } from "./utils/ERC4626BufferPoolMock.sol";
+import "./utils/ERC4626TokenMock.sol";
 
 contract ERC4626RebalanceRateValidation is BaseVaultTest {
     using ArrayHelpers for *;
@@ -47,49 +48,32 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
 
     ERC4626BufferPoolFactoryMock factory;
     ERC4626BufferPoolMock internal bufferPoolDai;
-    ERC4626BufferPoolMock internal bufferPoolSand;
+    ERC4626BufferPoolMock internal bufferPoolWsteth;
 
+    ERC20TestToken mockedDai;
     IERC20 daiMainnet;
     IERC4626 wDAI;
 
-    IERC20 sandMainnet;
-    IERC4626 wSAND;
+    ERC20TestToken mockedWsteth;
+    IERC20 wstethMainnet;
+    IERC4626 wWstEth;
 
-    uint256 constant BLOCK_NUMBER = 19314200;
-
-    address constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address wDAI_ADDRESS;
-    address constant SAND_ADDRESS = 0x3845badAde8e6dFF049820680d1F14bD3903a5d0;
-    address wSAND_ADDRESS;
-
-    // Owner of DAI and SAND in Mainnet
-    address constant DONOR_WALLET_ADDRESS = 0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503;
-
-    address payable donor;
+    address wWSTETH_ADDRESS;
 
     uint256 constant BUFFER_BASE_TOKENS = 1e6 * 1e18;
     uint256 bufferDaiWrapped;
-    uint256 bufferSandWrapped;
+    uint256 buffeWstethWrapped;
 
     uint256 constant DELTA = 1e12;
 
     uint256 internal bptAmountOutDai;
-    uint256 internal bptAmountOutSand;
+    uint256 internal bptAmountOutWsteth;
 
     uint256 constant SMALL_AMOUNT = 3e6;
     uint256 constant BIG_AMOUNT = 1e12;
 
     function setUp() public virtual override {
-        vm.createSelectFork({ blockNumber: BLOCK_NUMBER, urlOrAlias: "mainnet" });
-
-        donor = payable(DONOR_WALLET_ADDRESS);
-        vm.label(donor, "TokenDonor");
-        vm.label(DAI_ADDRESS, "DAI");
-        vm.label(SAND_ADDRESS, "SAND");
-
-        daiMainnet = IERC20(DAI_ADDRESS);
-        sandMainnet = IERC20(SAND_ADDRESS);
-
         _createTokens();
 
         BaseVaultTest.setUp();
@@ -102,16 +86,16 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
         vault.registerBufferPoolFactory(address(factory));
 
         bufferPoolDai = ERC4626BufferPoolMock(_createBuffer(wDAI));
-        bufferPoolSand = ERC4626BufferPoolMock(_createBuffer(wSAND));
+        bufferPoolWsteth = ERC4626BufferPoolMock(_createBuffer(wWstEth));
 
         return address(bufferPoolDai);
     }
 
     function initPool() internal override {
-        _transferTokensFromDonorToUsers();
+        _giveTokensToLPs();
         // The swap calculation of the buffer is a bit imprecise to save gas,
         // so it needs to have some ERC20 to rebalance
-        _transferTokensFromDonorToBuffers();
+        _giveTokensToBufferContracts();
         _setPermissions();
 
         vm.startPrank(lp);
@@ -122,7 +106,7 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
         uint256[] memory amountsInDai = [uint256(bufferDaiWrapped), uint256(BUFFER_BASE_TOKENS)].toMemoryArray();
         bptAmountOutDai = router.initialize(
             address(bufferPoolDai),
-            [wDAI_ADDRESS, DAI_ADDRESS].toMemoryArray().asIERC20(),
+            [wDAI_ADDRESS, address(mockedDai)].toMemoryArray().asIERC20(),
             amountsInDai,
             // Account for the precision loss
             BUFFER_BASE_TOKENS - DELTA - 1e6,
@@ -130,14 +114,14 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
             bytes("")
         );
 
-        // Creating SAND Buffer
-        bufferSandWrapped = wSAND.convertToShares(BUFFER_BASE_TOKENS);
-        wSAND.deposit(BUFFER_BASE_TOKENS, address(lp));
-        uint256[] memory amountsInSand = [uint256(bufferSandWrapped), uint256(BUFFER_BASE_TOKENS)].toMemoryArray();
-        bptAmountOutSand = router.initialize(
-            address(bufferPoolSand),
-            [wSAND_ADDRESS, SAND_ADDRESS].toMemoryArray().asIERC20(),
-            amountsInSand,
+        // Creating WSTETH Buffer
+        buffeWstethWrapped = wWstEth.convertToShares(BUFFER_BASE_TOKENS);
+        wWstEth.deposit(BUFFER_BASE_TOKENS, address(lp));
+        uint256[] memory amountsInWsteth = [uint256(buffeWstethWrapped), uint256(BUFFER_BASE_TOKENS)].toMemoryArray();
+        bptAmountOutWsteth = router.initialize(
+            address(bufferPoolWsteth),
+            [wWSTETH_ADDRESS, address(mockedWsteth)].toMemoryArray().asIERC20(),
+            amountsInWsteth,
             // Account for the precision loss
             BUFFER_BASE_TOKENS - DELTA - 1e6,
             false,
@@ -146,7 +130,7 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
         vm.stopPrank();
     }
 
-    function testInitializeDai__Fork() public {
+    function testInitializeDai() public {
         // Tokens are stored in the Vault
         assertEq(
             wDAI.balanceOf(address(vault)),
@@ -183,52 +167,52 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
         );
     }
 
-    function testInitializeSand__Fork() public {
+    function testInitializeWsteth() public {
         // Tokens are stored in the Vault
         assertEq(
-            wSAND.balanceOf(address(vault)),
-            bufferSandWrapped,
-            "Vault should have the deposited amount of wSand"
+            wWstEth.balanceOf(address(vault)),
+            buffeWstethWrapped,
+            "Vault should have the deposited amount of wWsteth"
         );
         assertEq(
-            sandMainnet.balanceOf(address(vault)),
+            wstethMainnet.balanceOf(address(vault)),
             BUFFER_BASE_TOKENS,
-            "Vault should have the deposited amount of SAND"
+            "Vault should have the deposited amount of WSTETH"
         );
 
         // Check if tokens are deposited in the pool
-        (, , uint256[] memory balancesRaw, , ) = vault.getPoolTokenInfo(address(bufferPoolSand));
+        (, , uint256[] memory balancesRaw, , ) = vault.getPoolTokenInfo(address(bufferPoolWsteth));
         assertEq(
             balancesRaw[0],
-            bufferSandWrapped,
-            "wSand BufferPool balance should have the deposited amount of wSand"
+            buffeWstethWrapped,
+            "wWsteth BufferPool balance should have the deposited amount of wWsteth"
         );
         assertEq(
             balancesRaw[1],
             BUFFER_BASE_TOKENS,
-            "wSand BufferPool balance should have the deposited amount of SAND"
+            "wWsteth BufferPool balance should have the deposited amount of WSTETH"
         );
 
         // should mint correct amount of BPT tokens for buffer
         // Account for the precision loss
         assertApproxEqAbs(
-            bufferPoolSand.balanceOf(lp),
-            bptAmountOutSand,
+            bufferPoolWsteth.balanceOf(lp),
+            bptAmountOutWsteth,
             DELTA,
-            "lp should have the BPT issued by the wSand BufferPool"
+            "lp should have the BPT issued by the wWsteth BufferPool"
         );
         assertApproxEqAbs(
-            bptAmountOutSand,
+            bptAmountOutWsteth,
             2 * BUFFER_BASE_TOKENS,
             DELTA,
             string.concat(
-                "The amount of BPT issued by the wSand BufferPool should be very ",
-                "close to the sum of SAND+wSand (or 2 * SAND, as the buffer is balanced)"
+                "The amount of BPT issued by the wWsteth BufferPool should be very ",
+                "close to the sum of WSTETH+wWsteth (or 2 * WSTETH, as the buffer is balanced)"
             )
         );
     }
 
-    function testDaiSmallRateWithMoreBase__Fuzz__Fork(uint256 assetsToTransfer) public {
+    function testDaiSmallRateWithMoreBase__Fuzz(uint256 assetsToTransfer) public {
         // assetsToTransfer will set the amount of assets that will unbalance the pool, and
         // varies from 0.001% of BUFFER_BASE_TOKENS to 95% of BUFFER_BASE_TOKENS
         assetsToTransfer = bound(assetsToTransfer, BUFFER_BASE_TOKENS / 100000, (95 * BUFFER_BASE_TOKENS) / 100);
@@ -267,7 +251,7 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
         );
     }
 
-    function testDaiSmallRateWithMoreWrapped__Fuzz__Fork(uint256 assetsToTransfer) public {
+    function testDaiSmallRateWithMoreWrapped__Fuzz(uint256 assetsToTransfer) public {
         // assetsToTransfer will set the amount of assets that will unbalance the pool, and
         // varies from 0.001% of BUFFER_BASE_TOKENS to 95% of BUFFER_BASE_TOKENS
         assetsToTransfer = bound(assetsToTransfer, BUFFER_BASE_TOKENS / 100000, (95 * BUFFER_BASE_TOKENS) / 100);
@@ -306,134 +290,136 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
         );
     }
 
-    function testSandBigRateWithMoreBase__Fuzz__Fork(uint256 assetsToTransfer) public {
+    function testWstethBigRateWithMoreBase__Fuzz(uint256 assetsToTransfer) public {
         // assetsToTransfer will set the amount of assets that will unbalance the pool, and
         // varies from 0.001% of BUFFER_BASE_TOKENS to 95% of BUFFER_BASE_TOKENS
         assetsToTransfer = bound(assetsToTransfer, BUFFER_BASE_TOKENS / 100000, (95 * BUFFER_BASE_TOKENS) / 100);
-        bufferPoolSand.unbalanceThePool(assetsToTransfer, SwapKind.EXACT_IN);
+        bufferPoolWsteth.unbalanceThePool(assetsToTransfer, SwapKind.EXACT_IN);
 
         // Check pool balances before rebalance to make sure it's unbalanced
-        (uint256 sandBalanceBeforeRebalance, uint256 wSandBalanceBeforeRebalance) = _checkBufferPoolBalance(
+        (uint256 wstethBalanceBeforeRebalance, uint256 wWstethBalanceBeforeRebalance) = _checkBufferPoolBalance(
             vault,
-            address(bufferPoolSand),
-            wSAND_ADDRESS,
+            address(bufferPoolWsteth),
+            wWSTETH_ADDRESS,
             BUFFER_BASE_TOKENS + assetsToTransfer,
-            bufferSandWrapped - wSAND.convertToShares(assetsToTransfer)
+            buffeWstethWrapped - wWstEth.convertToShares(assetsToTransfer)
         );
 
         vm.prank(admin);
-        bufferPoolSand.rebalance();
+        bufferPoolWsteth.rebalance();
 
         // Check pool balances after rebalance to make sure it's balanced
-        (uint256 sandBalanceAfterRebalance, uint256 wSandBalanceAfterRebalance) = _checkBufferPoolBalance(
+        (uint256 wstethBalanceAfterRebalance, uint256 wWstethBalanceAfterRebalance) = _checkBufferPoolBalance(
             vault,
-            address(bufferPoolSand),
-            wSAND_ADDRESS,
+            address(bufferPoolWsteth),
+            wWSTETH_ADDRESS,
             BUFFER_BASE_TOKENS,
-            wSAND.previewDeposit(BUFFER_BASE_TOKENS)
+            wWstEth.previewDeposit(BUFFER_BASE_TOKENS)
         );
 
         // Makes sure the rebalance rounding errors was in favour of the vault
-        uint256 assetsInOneShare = wSAND.convertToAssets(1);
+        uint256 assetsInOneShare = wWstEth.convertToAssets(1);
         assetsInOneShare = assetsInOneShare > 0 ? assetsInOneShare : 1;
         _checkBufferContractBalanceAfterRebalance(
-            sandBalanceBeforeRebalance,
-            sandBalanceAfterRebalance,
-            wSandBalanceBeforeRebalance,
-            wSandBalanceAfterRebalance,
+            wstethBalanceBeforeRebalance,
+            wstethBalanceAfterRebalance,
+            wWstethBalanceBeforeRebalance,
+            wWstethBalanceAfterRebalance,
             assetsInOneShare
         );
     }
 
-    function testSandBigRateWithMoreWrapped__Fuzz__Fork(uint256 assetsToTransfer) public {
+    function testWstethBigRateWithMoreWrapped__Fuzz(uint256 assetsToTransfer) public {
         // assetsToTransfer will set the amount of assets that will unbalance the pool, and
         // varies from 0.001% of BUFFER_BASE_TOKENS to 95% of BUFFER_BASE_TOKENS
         assetsToTransfer = bound(assetsToTransfer, BUFFER_BASE_TOKENS / 100000, (95 * BUFFER_BASE_TOKENS) / 100);
-        bufferPoolSand.unbalanceThePool(assetsToTransfer, SwapKind.EXACT_OUT);
+        bufferPoolWsteth.unbalanceThePool(assetsToTransfer, SwapKind.EXACT_OUT);
 
         // Check pool balances before rebalance to make sure it's unbalanced
-        (uint256 sandBalanceBeforeRebalance, uint256 wSandBalanceBeforeRebalance) = _checkBufferPoolBalance(
+        (uint256 wstethBalanceBeforeRebalance, uint256 wWstethBalanceBeforeRebalance) = _checkBufferPoolBalance(
             vault,
-            address(bufferPoolSand),
-            wSAND_ADDRESS,
+            address(bufferPoolWsteth),
+            wWSTETH_ADDRESS,
             BUFFER_BASE_TOKENS - assetsToTransfer,
-            bufferSandWrapped + wSAND.convertToShares(assetsToTransfer)
+            buffeWstethWrapped + wWstEth.convertToShares(assetsToTransfer)
         );
 
         vm.prank(admin);
-        bufferPoolSand.rebalance();
+        bufferPoolWsteth.rebalance();
 
         // Check pool balances after rebalance to make sure it's balanced
-        (uint256 sandBalanceAfterRebalance, uint256 wSandBalanceAfterRebalance) = _checkBufferPoolBalance(
+        (uint256 wstethBalanceAfterRebalance, uint256 wWstethBalanceAfterRebalance) = _checkBufferPoolBalance(
             vault,
-            address(bufferPoolSand),
-            wSAND_ADDRESS,
+            address(bufferPoolWsteth),
+            wWSTETH_ADDRESS,
             BUFFER_BASE_TOKENS,
-            wSAND.previewDeposit(BUFFER_BASE_TOKENS)
+            wWstEth.previewDeposit(BUFFER_BASE_TOKENS)
         );
 
         // Makes sure the rebalance rounding errors was in favour of the vault
-        uint256 assetsInOneShare = wSAND.convertToAssets(1);
+        uint256 assetsInOneShare = wWstEth.convertToAssets(1);
         assetsInOneShare = assetsInOneShare > 0 ? assetsInOneShare : 1;
         _checkBufferContractBalanceAfterRebalance(
-            sandBalanceBeforeRebalance,
-            sandBalanceAfterRebalance,
-            wSandBalanceBeforeRebalance,
-            wSandBalanceAfterRebalance,
+            wstethBalanceBeforeRebalance,
+            wstethBalanceAfterRebalance,
+            wWstethBalanceBeforeRebalance,
+            wWstethBalanceAfterRebalance,
             assetsInOneShare
         );
     }
 
     function _createTokens() private {
-        wDAI = new ERC4626TokenMock("Wrapped Dai", "wDAI", SMALL_AMOUNT, BIG_AMOUNT, IERC20(DAI_ADDRESS));
+        mockedDai = createERC20("DAI", 18);
+        daiMainnet = IERC20(address(mockedDai));
+
+        mockedWsteth = createERC20("WSTETH", 18);
+        wstethMainnet = IERC20(address(mockedWsteth));
+
+        wDAI = new ERC4626TokenMock("Wrapped Dai", "wDAI", SMALL_AMOUNT, BIG_AMOUNT, daiMainnet);
         wDAI_ADDRESS = address(wDAI);
         vm.label(wDAI_ADDRESS, "wDAI");
 
-        wSAND = new ERC4626TokenMock("Wrapped Sand", "wSAND", BIG_AMOUNT, SMALL_AMOUNT, IERC20(SAND_ADDRESS));
-        wSAND_ADDRESS = address(wSAND);
-        vm.label(wSAND_ADDRESS, "wSAND");
+        wWstEth = new ERC4626TokenMock("Wrapped WSTETH", "wWSTETH", BIG_AMOUNT, SMALL_AMOUNT, wstethMainnet);
+        wWSTETH_ADDRESS = address(wWstEth);
+        vm.label(wWSTETH_ADDRESS, "wWSTETH");
     }
 
     function _createBuffer(IERC4626 wrappedToken) private returns (address) {
         return factory.createMocked(wrappedToken);
     }
 
-    function _transferTokensFromDonorToUsers() private {
+    function _giveTokensToLPs() private {
         address[] memory usersToTransfer = [address(lp)].toMemoryArray();
 
         for (uint256 index = 0; index < usersToTransfer.length; index++) {
             address userAddress = usersToTransfer[index];
 
-            vm.startPrank(donor);
-            daiMainnet.transfer(userAddress, 4 * BUFFER_BASE_TOKENS);
-            sandMainnet.transfer(userAddress, 4 * BUFFER_BASE_TOKENS);
-            vm.stopPrank();
+            mockedDai.mint(userAddress, 4 * BUFFER_BASE_TOKENS);
+            mockedWsteth.mint(userAddress, 4 * BUFFER_BASE_TOKENS);
 
             vm.startPrank(userAddress);
             daiMainnet.approve(address(vault), type(uint256).max);
             wDAI.approve(address(vault), type(uint256).max);
             daiMainnet.approve(address(wDAI), type(uint256).max);
 
-            sandMainnet.approve(address(vault), type(uint256).max);
-            wSAND.approve(address(vault), type(uint256).max);
-            sandMainnet.approve(address(wSAND), type(uint256).max);
+            wstethMainnet.approve(address(vault), type(uint256).max);
+            wWstEth.approve(address(vault), type(uint256).max);
+            wstethMainnet.approve(address(wWstEth), type(uint256).max);
             vm.stopPrank();
         }
     }
 
-    function _transferTokensFromDonorToBuffers() private {
-        address[] memory buffersToTransfer = [address(bufferPoolDai), address(bufferPoolSand)].toMemoryArray();
+    function _giveTokensToBufferContracts() private {
+        address[] memory buffersToTransfer = [address(bufferPoolDai), address(bufferPoolWsteth)].toMemoryArray();
 
         for (uint256 index = 0; index < buffersToTransfer.length; index++) {
             address bufferAddress = buffersToTransfer[index];
 
-            vm.startPrank(donor);
             uint256 daiToConvert = wDAI.previewRedeem(1e18);
-            daiMainnet.transfer(bufferAddress, daiToConvert + 1e18);
+            mockedDai.mint(bufferAddress, daiToConvert + 1e18);
 
-            uint256 sandToConvert = wSAND.previewDeposit(1e14);
-            sandMainnet.transfer(bufferAddress, sandToConvert + 1e18);
-            vm.stopPrank();
+            uint256 wstethToConvert = wWstEth.previewDeposit(1e14);
+            mockedWsteth.mint(bufferAddress, wstethToConvert + 1e18);
 
             vm.startPrank(bufferAddress);
             daiMainnet.approve(address(vault), type(uint256).max);
@@ -441,17 +427,17 @@ contract ERC4626RebalanceRateValidation is BaseVaultTest {
             daiMainnet.approve(address(wDAI), type(uint256).max);
             wDAI.deposit(daiToConvert, bufferAddress);
 
-            sandMainnet.approve(address(vault), type(uint256).max);
-            wSAND.approve(address(vault), type(uint256).max);
-            sandMainnet.approve(address(wSAND), type(uint256).max);
-            wSAND.deposit(sandToConvert, bufferAddress);
+            wstethMainnet.approve(address(vault), type(uint256).max);
+            wWstEth.approve(address(vault), type(uint256).max);
+            wstethMainnet.approve(address(wWstEth), type(uint256).max);
+            wWstEth.deposit(wstethToConvert, bufferAddress);
             vm.stopPrank();
         }
     }
 
     function _setPermissions() private {
         authorizer.grantRole(bufferPoolDai.getActionId(IBufferPool.rebalance.selector), admin);
-        authorizer.grantRole(bufferPoolSand.getActionId(IBufferPool.rebalance.selector), admin);
+        authorizer.grantRole(bufferPoolWsteth.getActionId(IBufferPool.rebalance.selector), admin);
     }
 
     function _checkBufferPoolBalance(
