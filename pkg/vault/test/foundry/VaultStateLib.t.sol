bdfb26ca93bb7dbd9f378fa89f1dc0e63eec6ac1
João Bruno <joaobrunoah@gmail.com>
2024-03-12 12:03:31 -0300

    Fixing wrongly written test

--- pkg/vault/test/foundry/VaultStateLib.t.sol
+++ pkg/vault/test/foundry/VaultStateLib.t.sol
@@ -8,10 +8,10 @@ import { VaultState } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTy
 import { VaultStateBits, VaultStateLib } from "../../contracts/lib/VaultStateLib.sol";
 
 contract VaultStateLibTest is Test {
-    uint256 private constant CONFIG_MSB = 2162720;
+    uint256 private constant CONFIG_MSB = 22;
 
     function testToAndFromVaultStateBits__Fuzz(uint256 rawConfigInt) public {
-        rawConfigInt = bound(rawConfigInt, 0, uint256(1 << CONFIG_MSB));
+        rawConfigInt = bound(rawConfigInt, 0, uint256(1 << CONFIG_MSB) - 1);
         bytes32 rawConfig = bytes32(rawConfigInt);
         VaultState memory config = VaultStateLib.toVaultState(VaultStateBits.wrap(rawConfig));
         bytes32 configBytes32 = VaultStateBits.unwrap(VaultStateLib.fromVaultState(config));
