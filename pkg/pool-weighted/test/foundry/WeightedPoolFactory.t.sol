c16d4f2da01a9ab27683fe2bb1f91e227f19d6f6
Jeff Bennett <endymionjkb@gmail.com>
2024-03-18 14:05:49 -0400

    cleanup: remove unused imports

--- pkg/pool-weighted/test/foundry/WeightedPoolFactory.t.sol
+++ pkg/pool-weighted/test/foundry/WeightedPoolFactory.t.sol
@@ -5,10 +5,8 @@ pragma solidity ^0.8.4;
 import "forge-std/Test.sol";
 
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
-import { Vault } from "@balancer-labs/v3-vault/contracts/Vault.sol";
 import { VaultMockDeployer } from "@balancer-labs/v3-vault/test/foundry/utils/VaultMockDeployer.sol";
 import { VaultMock } from "@balancer-labs/v3-vault/contracts/test/VaultMock.sol";
-import { VaultExtensionMock } from "@balancer-labs/v3-vault/contracts/test/VaultExtensionMock.sol";
 import { WeightedPoolFactory } from "@balancer-labs/v3-pool-weighted/contracts/WeightedPoolFactory.sol";
 
 contract WeightedPoolFactoryTest is Test {
