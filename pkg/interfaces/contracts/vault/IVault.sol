7acad4c2c88fb46d19435394aa0cf4f2756aea9f
João Bruno <joaobrunoah@gmail.com>
2024-03-18 17:07:30 -0300

    Change pragma to 0.8.24

--- pkg/interfaces/contracts/vault/IVault.sol
+++ pkg/interfaces/contracts/vault/IVault.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import { IVaultAdmin } from "./IVaultAdmin.sol";
 import { IVaultExtension } from "./IVaultExtension.sol";
