3394de6b11ae2d8f2b5b10b87e6f87a0690aea9e
João Bruno <joaobrunoah@gmail.com>
2024-03-19 12:41:47 -0300

    CLs

--- pkg/vault/contracts/Router.sol
+++ pkg/vault/contracts/Router.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.24;
 
+import "hardhat/console.sol";
+
 import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
@@ -75,6 +77,7 @@ contract Router is IRouter, RouterCommon, ReentrancyGuard {
             params.minBptAmountOut,
             params.userData
         );
+        console.log('Router.80');
 
         uint256 ethAmountIn;
         for (uint256 i = 0; i < params.tokens.length; ++i) {
@@ -85,6 +88,7 @@ contract Router is IRouter, RouterCommon, ReentrancyGuard {
             // There can be only one WETH token in the pool
             if (params.wethIsEth && address(token) == address(_weth)) {
                 if (msg.value < amountIn) {
+                    console.log('Router.91');
                     revert InsufficientEth();
                 }
                 _weth.deposit{ value: amountIn }();
