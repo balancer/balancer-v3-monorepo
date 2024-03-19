1fcec2e50fa3935529cc69963ff7b3eb3bf482c8
João Bruno <joaobrunoah@gmail.com>
2024-03-19 13:12:34 -0300

    Fixing IERC20 Metadata import

--- pkg/vault/contracts/Vault.sol
+++ pkg/vault/contracts/Vault.sol
@@ -6,6 +6,7 @@ import { Proxy } from "@openzeppelin/contracts/proxy/Proxy.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
