a18e25ff862a5073722d057ac80c77847e57ed89
Jeff Bennett <endymionjkb@gmail.com>
2024-03-19 17:14:22 -0400

    refactor: use uint256 for memory structs; remove casting in AmplificationDataLib

--- pkg/pool-stable/contracts/lib/AmplificationDataLib.sol
+++ pkg/pool-stable/contracts/lib/AmplificationDataLib.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.24;
 
-import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-
 import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
 
 import { WordCodec } from "@balancer-labs/v3-solidity-utils/contracts/helpers/WordCodec.sol";
@@ -15,15 +13,14 @@ type AmplificationDataBits is bytes32;
 using AmplificationDataLib for AmplificationDataBits global;
 
 struct AmplificationData {
-    uint64 startValue;
-    uint64 endValue;
-    uint64 startTime;
-    uint64 endTime;
+    uint256 startValue;
+    uint256 endValue;
+    uint256 startTime;
+    uint256 endTime;
 }
 
 library AmplificationDataLib {
     using WordCodec for bytes32;
-    using SafeCast for uint256;
 
     // Bit offsets for amplification data
     uint8 public constant START_VALUE_OFFSET = 0;
@@ -34,20 +31,20 @@ library AmplificationDataLib {
     uint8 private constant _AMP_VALUE_BIT_LENGTH = 64;
     uint8 private constant _TIMESTAMP_BIT_LENGTH = 64;
 
-    function getStartValue(AmplificationDataBits data) internal pure returns (uint64) {
-        return AmplificationDataBits.unwrap(data).decodeUint(START_VALUE_OFFSET, _AMP_VALUE_BIT_LENGTH).toUint64();
+    function getStartValue(AmplificationDataBits data) internal pure returns (uint256) {
+        return AmplificationDataBits.unwrap(data).decodeUint(START_VALUE_OFFSET, _AMP_VALUE_BIT_LENGTH);
     }
 
-    function getEndValue(AmplificationDataBits data) internal pure returns (uint64) {
-        return AmplificationDataBits.unwrap(data).decodeUint(END_VALUE_OFFSET, _AMP_VALUE_BIT_LENGTH).toUint64();
+    function getEndValue(AmplificationDataBits data) internal pure returns (uint256) {
+        return AmplificationDataBits.unwrap(data).decodeUint(END_VALUE_OFFSET, _AMP_VALUE_BIT_LENGTH);
     }
 
-    function getStartTime(AmplificationDataBits data) internal pure returns (uint64) {
-        return AmplificationDataBits.unwrap(data).decodeUint(START_TIME_OFFSET, _TIMESTAMP_BIT_LENGTH).toUint64();
+    function getStartTime(AmplificationDataBits data) internal pure returns (uint256) {
+        return AmplificationDataBits.unwrap(data).decodeUint(START_TIME_OFFSET, _TIMESTAMP_BIT_LENGTH);
     }
 
-    function getEndTime(AmplificationDataBits data) internal pure returns (uint64) {
-        return AmplificationDataBits.unwrap(data).decodeUint(END_TIME_OFFSET, _TIMESTAMP_BIT_LENGTH).toUint64();
+    function getEndTime(AmplificationDataBits data) internal pure returns (uint256) {
+        return AmplificationDataBits.unwrap(data).decodeUint(END_TIME_OFFSET, _TIMESTAMP_BIT_LENGTH);
     }
 
     function fromAmpData(AmplificationData memory data) internal pure returns (AmplificationDataBits) {
