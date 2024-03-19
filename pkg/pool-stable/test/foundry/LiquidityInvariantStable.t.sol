8344f5e604cef315ecdb3a88306cb1f95f049b9e
Weißer Hase <wei3erHase@protonmail.com>
2024-03-19 14:43:18 +0100

    feat: adding liquidity invariant tests for #407

---
+++ pkg/pool-stable/test/foundry/LiquidityInvariantStable.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.4;
+
+import "forge-std/Test.sol";
+
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+
+import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/ArrayHelpers.sol";
+
+import { StablePoolFactory } from "../../contracts/StablePoolFactory.sol";
+import { StablePool } from "../../contracts/StablePool.sol";
+
+import { LiquidityInvariantTest } from "vault/test/foundry/LiquidityInvariant.t.sol";
+
+contract LiquidityInvariantStableTest is LiquidityInvariantTest {
+    using ArrayHelpers for *;
+
+    uint256 constant DEFAULT_AMP_FACTOR = 200;
+
+    uint256 nonce;
+
+    function setUp() public virtual override {
+        LiquidityInvariantTest.setUp();
+    }
+
+    function _createPool(address[] memory tokens, string memory label) internal override returns (address) {
+        StablePoolFactory factory = new StablePoolFactory(IVault(address(vault)), 365 days);
+
+        StablePool newPool = StablePool(
+            factory.create("ERC20 Pool", "ERC20POOL", vault.buildTokenConfig(tokens.asIERC20()), DEFAULT_AMP_FACTOR, ZERO_BYTES32)
+        );
+        vm.label(address(newPool), label);
+        return address(newPool);
+    }
+}
