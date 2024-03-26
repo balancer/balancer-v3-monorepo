dadda0745cbffb3a59e80eb023512c8fc95fa5cb
Jeff Bennett <endymionjkb@gmail.com>
2024-03-26 14:25:10 -0400

    test: Recovery Mode permissionless features

--- pkg/vault/test/foundry/RecoveryMode.t.sol
+++ pkg/vault/test/foundry/RecoveryMode.t.sol
@@ -5,11 +5,10 @@ pragma solidity ^0.8.24;
 import "forge-std/Test.sol";
 
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
-import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
+import { IAuthentication } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/IAuthentication.sol";
+import { IVaultAdmin } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultAdmin.sol";
 
 import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/ArrayHelpers.sol";
-
-import { PoolMock } from "../../contracts/test/PoolMock.sol";
 
 import { BaseVaultTest } from "./utils/BaseVaultTest.sol";
 
@@ -58,5 +57,114 @@ contract RecoveryModeTest is BaseVaultTest {
 
             shouldBeEqual ? assertTrue(areEqual) : assertFalse(areEqual);
         }
+    }
+
+    // Test permissionless Recovery Mode scenarios
+
+    function testRecoveryModePermissionlessWhenVaultPaused() public {
+        // When Vault is not paused, `enableRecoveryMode` is permissioned.
+        assertFalse(vault.isVaultPaused());
+
+        vm.expectRevert(abi.encodeWithSelector(IAuthentication.SenderNotAllowed.selector));
+        vm.prank(lp);
+        vault.enableRecoveryMode(pool);
+
+        // Pause Vault
+        vault.manualPauseVault();
+
+        assertTrue(vault.isVaultPaused());
+        assertFalse(vault.isPoolInRecoveryMode(pool));
+
+        // Can enable recovery mode by an LP with no permission grant.
+        vm.prank(lp);
+        vault.enableRecoveryMode(pool);
+
+        assertTrue(vault.isVaultPaused());
+        assertTrue(vault.isPoolInRecoveryMode(pool));
+    }
+
+    function testRecoveryModePermissionlessWhenPoolPaused() public {
+        // When Pool is not paused, `enableRecoveryMode` is permissioned.
+        assertFalse(vault.isPoolPaused(pool));
+        // Also ensure Vault is not paused.
+        assertFalse(vault.isVaultPaused());
+
+        vm.expectRevert(abi.encodeWithSelector(IAuthentication.SenderNotAllowed.selector));
+        vm.prank(lp);
+        vault.enableRecoveryMode(pool);
+
+        // Pause Pool
+        vault.manualSetPoolPaused(pool, true);
+
+        assertTrue(vault.isPoolPaused(pool));
+        assertFalse(vault.isPoolInRecoveryMode(pool));
+
+        // Can enable recovery mode by an LP with no permission grant.
+        vm.prank(lp);
+        vault.enableRecoveryMode(pool);
+
+        assertFalse(vault.isVaultPaused());
+        assertTrue(vault.isPoolPaused(pool));
+        assertTrue(vault.isPoolInRecoveryMode(pool));
+    }
+
+    function testRecoveryModePermissionedWhenVaultPermissionless() public {
+        // Pause Vault
+        vault.manualPauseVault();
+        assertTrue(vault.isVaultPaused());
+        assertFalse(vault.isPoolPaused(pool));
+
+        // Enter the permissionless period of the Vault.
+        skip(500 days);
+
+        // Confirm the Vault is permissionless
+        uint256 bufferPeriodEndTime = vault.getBufferPeriodEndTime();
+        assertTrue(block.timestamp > bufferPeriodEndTime);
+
+        // Recovery Mode is permissioned even though the Vault's pause bit is set, because it's no longer pausable.
+        assertFalse(vault.isVaultPaused());
+        vm.expectRevert(abi.encodeWithSelector(IAuthentication.SenderNotAllowed.selector));
+        vm.prank(lp);
+        vault.enableRecoveryMode(pool);
+
+        // Can still set it if granted permission
+        authorizer.grantRole(vault.getActionId(IVaultAdmin.enableRecoveryMode.selector), admin);
+
+        vm.prank(admin);
+        vault.enableRecoveryMode(pool);
+
+        assertTrue(vault.isPoolInRecoveryMode(pool));
+    }
+
+    function testRecoveryModePermissionedWhenPoolPermissionless() public {
+        // Also ensure Vault is not paused.
+        assertFalse(vault.isVaultPaused());
+
+        // Pause pool
+        vault.manualSetPoolPaused(pool, true);
+
+        assertTrue(vault.isPoolPaused(pool));
+        assertFalse(vault.isPoolInRecoveryMode(pool));
+
+        // Enter the permissionless period of the Pool.
+        skip(500 days);
+
+        // Confirm the Pool is permissionless
+        (, , uint256 bufferPeriodEndTime, ) = vault.getPoolPausedState(pool);
+        assertTrue(block.timestamp > bufferPeriodEndTime);
+
+        // Recovery Mode is permissioned even though the Pool's pause bit is set, because it's no longer pausable.
+        assertFalse(vault.isPoolPaused(pool));
+        vm.expectRevert(abi.encodeWithSelector(IAuthentication.SenderNotAllowed.selector));
+        vm.prank(lp);
+        vault.enableRecoveryMode(pool);
+
+        // Can still set it if granted permission
+        authorizer.grantRole(vault.getActionId(IVaultAdmin.enableRecoveryMode.selector), admin);
+
+        vm.prank(admin);
+        vault.enableRecoveryMode(pool);
+
+        assertTrue(vault.isPoolInRecoveryMode(pool));
     }
 }
