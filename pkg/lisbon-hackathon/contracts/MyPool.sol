5eb9d883511f624111f72481c2f6b412a3f9699f
Daniel <danielBeets@protonmail.com>
2024-04-11 10:58:57 +0100

    Add pool

---
+++ pkg/lisbon-hackathon/contracts/MyPool.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
+import { SwapKind } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+import { IBasePool } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePool.sol";
+import { BalancerPoolToken } from "@balancer-labs/v3-vault/contracts/BalancerPoolToken.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+
+
+contract MyPool is IBasePool, BalancerPoolToken {
+    using FixedPoint for uint256;
+
+    constructor(IVault vault, string memory name, string memory symbol) BalancerPoolToken(vault, name, symbol) {}
+    
+        /// @inheritdoc IBasePool
+    function getPoolTokens() public view returns (IERC20[] memory tokens) {
+        return getVault().getPoolTokens(address(this));
+    }
+
+    
+    /// @inheritdoc IBasePool
+    function onSwap(IBasePool.PoolSwapParams memory request) external pure returns (uint256 amountCalculatedScaled18) {
+        amountCalculatedScaled18 = request.amountGivenScaled18;
+    }
+    
+    
+    /// @inheritdoc IBasePool
+    function computeInvariant(uint256[] memory balancesLiveScaled18) public pure returns (uint256 invariant) {
+        invariant = balancesLiveScaled18[0] + balancesLiveScaled18[1];
+    }
+
+   
+    /// @inheritdoc IBasePool
+    function computeBalance(
+        uint256[] memory balancesLiveScaled18,
+        uint256 tokenInIndex,
+        uint256 invariantRatio
+    ) external pure returns (uint256 newBalance) {
+        uint256 invariant = computeInvariant(balancesLiveScaled18);
+        
+        newBalance = (balancesLiveScaled18[tokenInIndex] + invariant.mulDown(invariantRatio)) - invariant;
+    }
+}
