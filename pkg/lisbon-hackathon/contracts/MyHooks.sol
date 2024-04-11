08d852e5112d5e4e79378a26bf91cf70339d4cb6
Daniel <danielBeets@protonmail.com>
2024-04-11 10:50:47 +0100

    Add dynamic fee

--- pkg/lisbon-hackathon/contracts/MyHooks.sol
+++ pkg/lisbon-hackathon/contracts/MyHooks.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.4;
 
 import { PoolHooks } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 import { BaseHooks } from "@balancer-labs/v3-pool-with-hooks/contracts/BaseHooks.sol";
+import { IPoolHooks } from "@balancer-labs/v3-interfaces/contracts/vault/IPoolHooks.sol";
+import { IBasePool } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePool.sol";
+import { SwapLocals } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultMain.sol";
+import { PoolData } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 
 
 /// @notice A scaffold hooks implementation
@@ -25,8 +29,16 @@ contract MyHooks is BaseHooks {
             });
     }
 
+    function supportsDynamicFee() external pure override returns (bool) {
+        return true;
+    }
+
     /// @dev Checks if the trader has passed the required cooldown period between trades.
     function _onBeforeSwap(IBasePool.PoolSwapParams memory params) internal virtual override returns (bool) {
         return true;
+    }
+
+    function _computeFee(PoolData memory poolData, SwapLocals memory vars) internal virtual override returns (uint256) {
+        return 0;
     }
 }
