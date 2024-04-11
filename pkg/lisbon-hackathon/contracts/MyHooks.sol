b7d360e6fc2e0c5f74e762cf03505440a332bbb4
Daniel <danielBeets@protonmail.com>
2024-04-11 15:41:02 +0100

    Auto stash before merge of "lisbon-hackathon" and "origin/lisbon-hackathon"

--- pkg/lisbon-hackathon/contracts/MyHooks.sol
+++ pkg/lisbon-hackathon/contracts/MyHooks.sol
@@ -30,7 +30,7 @@ contract MyHooks is BaseHooks {
     }
 
     function supportsDynamicFee() external pure override returns (bool) {
-        return false;
+        return true;
     }
 
     /// @dev Checks if the trader has passed the required cooldown period between trades.
