2c330bcb53a0fb75c0dddca7ebfc6e86e22135f2
João Bruno <joaobrunoah@gmail.com>
2024-04-24 15:38:34 -0300

    Implementing tests to wrap/unwrap functions of ERC4626

--- pkg/vault/test/foundry/BufferVaultPrimitive.t.sol
+++ pkg/vault/test/foundry/BufferVaultPrimitive.t.sol
@@ -6,13 +6,12 @@ import "forge-std/Test.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import { IERC20Errors } from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
 import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
 import { IBatchRouter } from "@balancer-labs/v3-interfaces/contracts/vault/IBatchRouter.sol";
 
-import {
-ERC4626TestToken
-} from "@balancer-labs/v3-solidity-utils/contracts/test/ERC4626TestToken.sol";
+import { ERC4626TestToken } from "@balancer-labs/v3-solidity-utils/contracts/test/ERC4626TestToken.sol";
 
 import { BaseVaultTest } from "./utils/BaseVaultTest.sol";
 
@@ -20,8 +19,8 @@ contract BufferVaultPrimitiveTest is BaseVaultTest {
     ERC4626TestToken internal waDAI;
     ERC4626TestToken internal waUSDC;
 
-    uint256 userAmount = 10e6 * 1e18;
-    uint256 wrapAmount = userAmount / 100;
+    uint256 private _userAmount = 10e6 * 1e18;
+    uint256 private _wrapAmount = _userAmount / 100;
 
     function setUp() public virtual override {
         BaseVaultTest.setUp();
@@ -39,17 +38,18 @@ contract BufferVaultPrimitiveTest is BaseVaultTest {
     function initializeLp() private {
         // Create and fund buffer pools
         vm.startPrank(lp);
-        dai.mint(address(lp), userAmount);
-        // Minting wrong token to wrapped token contract, to test changing the asset
-        dai.mint(address(waUSDC), userAmount);
-        dai.approve(address(waDAI), userAmount);
-        waDAI.deposit(userAmount, address(lp));
 
-        usdc.mint(address(lp), userAmount);
-        // Minting wrong token to wrapped token contract, to test changing the asset
-        usdc.mint(address(waDAI), userAmount);
-        usdc.approve(address(waUSDC), userAmount);
-        waUSDC.deposit(userAmount, address(lp));
+        dai.mint(address(lp), _userAmount);
+        dai.approve(address(waDAI), _userAmount);
+        waDAI.deposit(_userAmount, address(lp));
+
+        usdc.mint(address(lp), _userAmount);
+        usdc.approve(address(waUSDC), _userAmount);
+        waUSDC.deposit(_userAmount, address(lp));
+
+        // Minting wrong token to wrapped token contracts, to test changing the asset
+        dai.mint(address(waUSDC), _userAmount);
+        usdc.mint(address(waDAI), _userAmount);
 
         waDAI.approve(address(permit2), MAX_UINT256);
         permit2.approve(address(waDAI), address(router), type(uint160).max, type(uint48).max);
@@ -60,15 +60,19 @@ contract BufferVaultPrimitiveTest is BaseVaultTest {
         vm.stopPrank();
     }
 
+    /********************************************************************************
+                                        Asset
+    ********************************************************************************/
     function testChangeAssetOfWrappedToken() public {
         // Change Asset to wrong underlying
         waDAI.setAsset(usdc);
 
         // Wrap token should pass, since there's no liquidity in the buffer
-        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapPath(
-            wrapAmount,
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
             0,
             usdc,
+            IERC20(address(waDAI)),
             IERC20(address(waDAI))
         );
 
@@ -80,7 +84,7 @@ contract BufferVaultPrimitiveTest is BaseVaultTest {
 
         // Add Liquidity with the right asset
         vm.prank(lp);
-        router.addLiquidityBuffer(IERC4626(address(waDAI)), wrapAmount, wrapAmount, address(lp));
+        router.addLiquidityBuffer(IERC4626(address(waDAI)), _wrapAmount, _wrapAmount, address(lp));
 
         // Change Asset to the wrong asset
         waDAI.setAsset(usdc);
@@ -91,12 +95,16 @@ contract BufferVaultPrimitiveTest is BaseVaultTest {
         batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
     }
 
-    // Deposit returns wrong shares
+    /********************************************************************************
+                                        Deposit
+    ********************************************************************************/
+
     function testDepositReturnsWrongShares() public {
-        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapPath(
-            wrapAmount,
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
             0,
             dai,
+            IERC20(address(waDAI)),
             IERC20(address(waDAI))
         );
 
@@ -107,29 +115,303 @@ contract BufferVaultPrimitiveTest is BaseVaultTest {
         batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
     }
 
-    // Deposit consumes wrong assets
+    function testDepositConsumesLessAssets() public {
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
+            0,
+            dai,
+            IERC20(address(waDAI)),
+            IERC20(address(waDAI))
+        );
 
-// Mint returns wrong shares or assets
-// Redeem returns wrong shares or assets
-// Withdraw returns wrong shares or assets
-// Disable Vault Buffers
+        waDAI.setAssetsToConsume(_wrapAmount - 1);
 
-    function _exactInWrapPath(
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongBaseAmountOnDeposit.selector, address(waDAI)));
+        batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testDepositConsumesMoreAssets() public {
+        uint256 changedWrapAmount = _wrapAmount + 1;
+
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
+            0,
+            dai,
+            IERC20(address(waDAI)),
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToConsume(changedWrapAmount);
+
+        vm.prank(lp);
+        // When the assets amount is higher than predicted, vault did not give allowance to make the transfer
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientAllowance.selector,
+                address(waDAI),
+                _wrapAmount,
+                changedWrapAmount
+            )
+        );
+        batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    /********************************************************************************
+                                        Mint
+    ********************************************************************************/
+
+    function testMintReturnsLessShares() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            dai,
+            IERC20(address(waDAI)),
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setSharesToReturn(_wrapAmount - 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongWrappedAmountOnDeposit.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testMintReturnsMoreShares() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            dai,
+            IERC20(address(waDAI)),
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setSharesToReturn(_wrapAmount + 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongWrappedAmountOnDeposit.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testMintConsumesLessAssets() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            dai,
+            IERC20(address(waDAI)),
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToConsume(_wrapAmount - 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongBaseAmountOnDeposit.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testMintConsumesMoreAssets() public {
+        uint256 changedWrapAmount = _wrapAmount + 1;
+
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            dai,
+            IERC20(address(waDAI)),
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToConsume(changedWrapAmount);
+
+        vm.prank(lp);
+        // When the assets amount is higher than predicted, vault did not give allowance to make the transfer
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientAllowance.selector,
+                address(waDAI),
+                _wrapAmount,
+                changedWrapAmount
+            )
+        );
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    /********************************************************************************
+                                        Redeem
+    ********************************************************************************/
+    function testRedeemConsumesLessShares() public {
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
+            0,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setSharesToConsume(_wrapAmount - 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongWrappedAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testRedeemConsumesMoreShares() public {
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
+            0,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setSharesToConsume(_wrapAmount + 1);
+
+        vm.startPrank(lp);
+        // Call addLiquidity so vault has enough liquidity to cover extra wrapped amount
+        router.addLiquidityBuffer(IERC4626(address(waDAI)), _wrapAmount, _wrapAmount, address(lp));
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongWrappedAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
+        vm.stopPrank();
+    }
+
+    function testRedeemReturnsMoreAssets() public {
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
+            0,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToReturn(waDAI.previewRedeem(_wrapAmount) + 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongBaseAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testRedeemReturnsLessAssets() public {
+        IBatchRouter.SwapPathExactAmountIn[] memory paths = _exactInWrapUnwrapPath(
+            _wrapAmount,
+            0,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToReturn(waDAI.previewRedeem(_wrapAmount) - 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongBaseAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactIn(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    /********************************************************************************
+                                        Withdraw
+    ********************************************************************************/
+
+    function testWithdrawConsumesLessShares() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setSharesToConsume(waDAI.previewWithdraw(_wrapAmount) - 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongWrappedAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testWithdrawConsumesMoreShares() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setSharesToConsume(waDAI.previewWithdraw(_wrapAmount) + 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongWrappedAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testWithdrawReturnsMoreAssets() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToReturn(_wrapAmount + 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongBaseAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    function testWithdrawReturnsLessAssets() public {
+        IBatchRouter.SwapPathExactAmountOut[] memory paths = _exactOutWrapUnwrapPath(
+            2 * _wrapAmount,
+            _wrapAmount,
+            IERC20(address(waDAI)),
+            dai,
+            IERC20(address(waDAI))
+        );
+
+        waDAI.setAssetsToReturn(_wrapAmount - 1);
+
+        vm.prank(lp);
+        vm.expectRevert(abi.encodeWithSelector(IVaultErrors.WrongBaseAmountOnWithdraw.selector, address(waDAI)));
+        batchRouter.swapExactOut(paths, MAX_UINT256, false, bytes(""));
+    }
+
+    // Disable Vault Buffers
+
+    function _exactInWrapUnwrapPath(
         uint256 exactAmountIn,
         uint256 minAmountOut,
-        IERC20 baseToken,
+        IERC20 tokenFrom,
+        IERC20 tokenTo,
         IERC20 wrappedToken
     ) private view returns (IBatchRouter.SwapPathExactAmountIn[] memory paths) {
         IBatchRouter.SwapPathStep[] memory steps = new IBatchRouter.SwapPathStep[](1);
         paths = new IBatchRouter.SwapPathExactAmountIn[](1);
 
-        steps[0] = IBatchRouter.SwapPathStep({ pool: address(wrappedToken), tokenOut: wrappedToken, isBuffer: true });
+        steps[0] = IBatchRouter.SwapPathStep({ pool: address(wrappedToken), tokenOut: tokenTo, isBuffer: true });
 
         paths[0] = IBatchRouter.SwapPathExactAmountIn({
-            tokenIn: baseToken,
+            tokenIn: tokenFrom,
             steps: steps,
             exactAmountIn: exactAmountIn,
             minAmountOut: minAmountOut
+        });
+    }
+
+    function _exactOutWrapUnwrapPath(
+        uint256 maxAmountIn,
+        uint256 exactAmountOut,
+        IERC20 tokenFrom,
+        IERC20 tokenTo,
+        IERC20 wrappedToken
+    ) private view returns (IBatchRouter.SwapPathExactAmountOut[] memory paths) {
+        IBatchRouter.SwapPathStep[] memory steps = new IBatchRouter.SwapPathStep[](1);
+        paths = new IBatchRouter.SwapPathExactAmountOut[](1);
+
+        steps[0] = IBatchRouter.SwapPathStep({ pool: address(wrappedToken), tokenOut: tokenTo, isBuffer: true });
+
+        paths[0] = IBatchRouter.SwapPathExactAmountOut({
+            tokenIn: tokenFrom,
+            steps: steps,
+            maxAmountIn: maxAmountIn,
+            exactAmountOut: exactAmountOut
         });
     }
 }
