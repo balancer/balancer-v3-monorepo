df669b726e91b90e9e04a796f936ce8fadd2a8fc
Weißer Hase <wei3erHase@protonmail.com>
2024-05-07 20:31:39 +0200

    Refactoring tests pt.1 (#499)

--- pkg/vault/contracts/VaultStorage.sol
+++ pkg/vault/contracts/VaultStorage.sol
@@ -44,6 +44,10 @@ contract VaultStorage {
     // Maximum pool swap fee percentage.
     uint256 internal constant _MAX_SWAP_FEE_PERCENTAGE = 10e16; // 10%
 
+    // Maximum pause and buffer period durations.
+    uint256 internal constant _MAX_PAUSE_WINDOW_DURATION = 356 days * 4;
+    uint256 internal constant _MAX_BUFFER_PERIOD_DURATION = 90 days;
+
     // Code extension for Vault.
     IVaultExtension internal immutable _vaultExtension;
 
@@ -90,9 +94,6 @@ contract VaultStorage {
 
     // Upgradeable contract in charge of setting permissions.
     IAuthorizer internal _authorizer;
-
-    uint256 public constant MAX_PAUSE_WINDOW_DURATION = 356 days * 4;
-    uint256 public constant MAX_BUFFER_PERIOD_DURATION = 90 days;
 
     // The Pause Window and Buffer Period are timestamp-based: they should not be relied upon for sub-minute accuracy.
     // solhint-disable not-rely-on-time
