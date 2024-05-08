6b8022fa20846f5b802edb26d80f02d7ca817017
João Bruno <joaobrunoah@gmail.com>
2024-05-08 18:21:01 -0300

    Adjust documentation of PackedTokenBalance library

--- pkg/vault/contracts/lib/PackedTokenBalance.sol
+++ pkg/vault/contracts/lib/PackedTokenBalance.sol
@@ -2,20 +2,12 @@
 
 pragma solidity ^0.8.24;
 
-/** TODO refactor
+/**
  * @notice This library represents a data structure for packing a token's current raw and derived balances. A derived
  * balance can be the "last" live balance scaled18 of the raw token, or the balance of the wrapped version of the
  * token in a vault buffer, among others.
  *
- * @dev `rawBalance` represents the actual number of tokens in the Vault allocated to the pool, in native decimal
- * encoding. `lastLiveBalanceScaled18` represents the "last live" balance, which is stored as an 18-decimal floating
- * point value so that it can be conveniently compared to other scaled values.
- *
- * "Last" refers to the final live balance as of the last time the raw balance changed (e.g., after a swap or liquidity
- * operation). The "live" balance is the balance presented to the Pool in hooks: with any applicable decimal and
- * rate scaling applied, and any applicable protocol fees (swap and yield) deducted.
- *
- * We could use a Solidity struct to pack these three values together in a single storage slot, but unfortunately
+ * @dev We could use a Solidity struct to pack balance values together in a single storage slot, but unfortunately
  * Solidity only allows for structs to live in either storage, calldata or memory. Because a memory struct still takes
  * up a slot in the stack (to store its memory location), and because the entire balance fits in a single stack slot
  * (two 128 bit values), using memory is strictly less gas performant. Therefore, we do manual packing and unpacking.
@@ -26,23 +18,20 @@ pragma solidity ^0.8.24;
 library PackedTokenBalance {
     // The 'rawBalance' portion of the balance is stored in the least significant 128 bits of a 256 bit word, while the
     // 'derivedBalance' part uses the remaining 128 bits.
-
     uint256 private constant _MAX_BALANCE = 2 ** (128) - 1;
 
     /// @dev One of the balances is above the maximum value that can be stored.
     error BalanceOverflow();
 
-    /// @dev TODO comment
     function getBalanceRaw(bytes32 balance) internal pure returns (uint256) {
         return uint256(balance) & _MAX_BALANCE;
     }
 
-    /// @dev TODO comment
     function getBalanceDerived(bytes32 balance) internal pure returns (uint256) {
         return uint256(balance >> 128) & _MAX_BALANCE;
     }
 
-    /// @dev TODO comment
+    /// @dev Set both raw and derived balances and returns the new bytes32 balance
     function setBalances(
         bytes32 balance,
         uint256 newBalanceRaw,
@@ -51,11 +40,13 @@ library PackedTokenBalance {
         return toPackedBalance(newBalanceRaw, newBalanceDerived);
     }
 
+    /// @dev Sets only the raw balance of balances and returns the new bytes32 balance
     function setBalanceRaw(bytes32 balance, uint256 newBalanceRaw) internal pure returns (bytes32) {
         return toPackedBalance(newBalanceRaw, getBalanceDerived(balance));
     }
 
-    /// @dev TODO comment
+    /// @dev Receives balanceRaw and balanceDerived, checks if it's smaller than 128 bits, and creates a bytes32
+    /// balance
     function toPackedBalance(uint256 balanceRaw, uint256 balanceDerived) internal pure returns (bytes32) {
         if (balanceRaw > _MAX_BALANCE || balanceDerived > _MAX_BALANCE) {
             revert BalanceOverflow();
