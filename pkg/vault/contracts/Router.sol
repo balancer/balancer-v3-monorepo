b58138c5949de157defacd4d70c7272c9e761857
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2024-05-09 19:23:28 -0300

    Update pkg/vault/contracts/Router.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/vault/contracts/Router.sol
+++ pkg/vault/contracts/Router.sol
@@ -682,7 +682,7 @@ contract Router is IRouter, RouterCommon, ReentrancyGuardTransient {
     /**
      * @notice Hook for removing liquidity from vault buffers.
      * @dev Can only be called by the Vault.
-     * @param wrappedToken Address of the wrapped token that implements IERC4626 interface
+     * @param wrappedToken Address of the wrapped token that implements the IERC4626 interface
      * @param sharesToRemove Amount of shares to remove from the buffer. Cannot be greater than sharesOwner
      *        total shares
      * @param sharesOwner Address of contract that owns the deposited liquidity.
