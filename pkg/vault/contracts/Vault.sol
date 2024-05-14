b6ceca208119eee749a76f9a9fb827c6a907b7b6
João Bruno <joaobrunoah@gmail.com>
2024-05-14 19:12:29 -0300

    Add comment

--- pkg/vault/contracts/Vault.sol
+++ pkg/vault/contracts/Vault.sol
@@ -1103,6 +1103,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
             (amountInUnderlying, amountOutWrapped) = (amountCalculated, amountGiven);
         }
 
+        // Checking isStaticCall first, so we only parse _vaultState in static calls
         if (!EVMCallModeHelpers.isStaticCall() || _vaultState.toVaultState().isQueryDisabled) {
             if (bufferBalances.getBalanceDerived() > amountOutWrapped) {
                 // The buffer has enough liquidity to facilitate the wrap without making an external call.
@@ -1221,6 +1222,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
             (amountOutUnderlying, amountInWrapped) = (amountGiven, amountCalculated);
         }
 
+        // Checking isStaticCall first, so we only parse _vaultState in static calls
         if (!EVMCallModeHelpers.isStaticCall() || _vaultState.toVaultState().isQueryDisabled) {
             if (bufferBalances.getBalanceRaw() > amountOutUnderlying) {
                 // the buffer has enough liquidity to facilitate the wrap without making an external call.
