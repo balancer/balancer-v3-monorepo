cbbcdba483802e71d36c1621bd00a89a79d463d3
Jeff Bennett <endymionjkb@gmail.com>
2024-05-17 17:05:37 -0400

    refactor: remove protocol fee change events from Vault; they are done in the collector. Allow changing the collector

--- pkg/interfaces/contracts/vault/IVaultEvents.sol
+++ pkg/interfaces/contracts/vault/IVaultEvents.sol
@@ -6,7 +6,8 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 import { IAuthorizer } from "./IAuthorizer.sol";
-import { LiquidityManagement, PoolHooks, PoolRoleAccounts, TokenConfig } from "./VaultTypes.sol";
+import { IProtocolFeeCollector } from "./IProtocolFeeCollector.sol";
+import "./VaultTypes.sol";
 
 interface IVaultEvents {
     /**
@@ -14,6 +15,7 @@ interface IVaultEvents {
      * @param pool The pool being registered
      * @param factory The factory creating the pool
      * @param tokenConfig The pool's tokens
+     * @param feeConfig The fees associated with this pool (swap, protocol, creator)
      * @param pauseWindowEndTime The pool's pause window end time
      * @param roleAccounts Addresses the Vault will allow to change certain pool settings
      * @param poolHooks Flags indicating which hooks the pool supports
@@ -23,6 +25,7 @@ interface IVaultEvents {
         address indexed pool,
         address indexed factory,
         TokenConfig[] tokenConfig,
+        PoolFeeConfig feeConfig,
         uint256 pauseWindowEndTime,
         PoolRoleAccounts roleAccounts,
         PoolHooks poolHooks,
@@ -107,34 +110,6 @@ interface IVaultEvents {
     event PoolPausedStateChanged(address indexed pool, bool paused);
 
     /**
-     * @notice Emitted when the protocol swap fee percentage is updated.
-     * @param swapFeePercentage The updated protocol swap fee percentage
-     */
-    event ProtocolSwapFeePercentageChanged(uint256 swapFeePercentage);
-
-    /**
-     * @notice Emitted when the protocol yield fee percentage is updated.
-     * @param yieldFeePercentage The updated protocol yield fee percentage
-     */
-    event ProtocolYieldFeePercentageChanged(uint256 yieldFeePercentage);
-
-    /**
-     * @notice Logs the collection of protocol swap fees in a specific token and amount.
-     * @param pool The pool on which the swap fee was charged
-     * @param token The token in which the swap fee was charged
-     * @param amount The amount of the token collected in fees
-     */
-    event ProtocolSwapFeeCollected(address indexed pool, IERC20 indexed token, uint256 amount);
-
-    /**
-     * @notice Logs the collection of protocol yield fees in a specific token and amount.
-     * @param pool The pool on which the yield fee was charged
-     * @param token The token in which the yield fee was charged
-     * @param amount The amount of the token collected in fees
-     */
-    event ProtocolYieldFeeCollected(address indexed pool, IERC20 indexed token, uint256 amount);
-
-    /**
      * @notice Emitted when the swap fee percentage of a pool is updated.
      * @param swapFeePercentage The new swap fee percentage for the pool
      */
@@ -152,4 +127,10 @@ interface IVaultEvents {
      * @param newAuthorizer The address of the new authorizer
      */
     event AuthorizerChanged(IAuthorizer indexed newAuthorizer);
+
+    /**
+     * @notice A new protocol fee collector is set by `setProtocolFeeCollector`.
+     * @param newProtocolFeeCollector The address of the new protocol fee collector
+     */
+    event ProtocolFeeCollectorChanged(IProtocolFeeCollector indexed newProtocolFeeCollector);
 }
