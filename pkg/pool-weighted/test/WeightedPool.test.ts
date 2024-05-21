1b455e7bfbc02566ad171001d28a35fc23177d1b
Jeff Bennett <endymionjkb@gmail.com>
2024-05-20 23:22:50 -0400

    test: fix hardhat test

--- pkg/pool-weighted/test/WeightedPool.test.ts
+++ pkg/pool-weighted/test/WeightedPool.test.ts
@@ -14,6 +14,7 @@ import {
   MAX_UINT48,
   ZERO_BYTES32,
   ZERO_ADDRESS,
+  ZERO,
 } from '@balancer-labs/v3-helpers/src/constants';
 import * as VaultDeployer from '@balancer-labs/v3-helpers/src/models/vault/VaultDeployer';
 import { IVaultMock } from '@balancer-labs/v3-interfaces/typechain-types';
@@ -138,7 +139,7 @@ describe('WeightedPool', function () {
       });
     });
   });
-
+  
   describe('protocol fee events on swap', () => {
     const WEIGHTS = [fp(0.5), fp(0.5)];
     const REAL_POOL_INITIAL_BALANCES = [TOKEN_AMOUNT, TOKEN_AMOUNT];
@@ -161,9 +162,7 @@ describe('WeightedPool', function () {
         'Test',
         tokenConfig,
         WEIGHTS,
-        0,
-        ZERO_ADDRESS,
-        [ZERO_ADDRESS, ZERO_ADDRESS],
+        [ZERO_ADDRESS, ZERO_ADDRESS, ZERO_ADDRESS],
         SWAP_FEE,
         ZERO_BYTES32
       );
