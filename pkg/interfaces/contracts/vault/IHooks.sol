a9d86d05ffa32a87cec9207daf9a36e05461964c
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2024-05-30 14:24:15 -0300

    Update pkg/interfaces/contracts/vault/IHooks.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/interfaces/contracts/vault/IHooks.sol
+++ pkg/interfaces/contracts/vault/IHooks.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.24;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IVault } from "./IVault.sol";
-import { SwapKind, AddLiquidityKind, HooksConfig, RemoveLiquidityKind, TokenConfig } from "./VaultTypes.sol";
 import { IBasePool } from "./IBasePool.sol";
+import "./VaultTypes.sol";
 
 /// @notice Interface for pool hooks
 interface IHooks {
