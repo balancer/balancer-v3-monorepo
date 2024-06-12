792fd9054a79dd8335140b0cf85beb4e2bb42d4e
João Bruno <joaobrunoah@gmail.com>
2024-06-12 18:37:13 -0300

    Create pool-hooks package

---
+++ pkg/pool-hooks/contracts/FeeTakingHook.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+
+import { BasePoolHooks } from "@balancer-labs/v3-vault/contracts/BasePoolHooks.sol";
+
+contract FeeTakingHook is BasePoolHooks {
+    uint256 public hookSwapFeePercentage;
+    uint256 public addLiquidityHookFeePercentage;
+    uint256 public removeLiquidityHookFeePercentage;
+
+    constructor(IVault vault) BasePoolHooks(vault) {}
+
+    /// @inheritdoc IHooks
+    function getHookFlags() external view override returns (HookFlags memory) {
+        HookFlags memory hookFlags;
+        hookFlags.shouldCallAfterSwap = true;
+        hookFlags.shouldCallAfterAddLiquidity = true;
+        hookFlags.shouldCallAfterRemoveLiquidity = true;
+        return hookFlags;
+    }
+
+    /// @inheritdoc IHooks
+    function onAfterSwap(
+        AfterSwapParams calldata params
+    ) external override onlyVault returns (bool success, uint256 hookAdjustedAmountCalculatedRaw) {
+        hookAdjustedAmountCalculatedRaw = params.amountCalculatedRaw;
+        if (hookSwapFeePercentage > 0) {
+            uint256 hookFee = hookAdjustedAmountCalculatedRaw.mulDown(hookSwapFeePercentage);
+            if (params.kind == SwapKind.EXACT_IN) {
+                // In EXACT_IN, the amount calculated is the amount of tokens out. The hook needs to charge the fee
+                // from amount calculated, so we will take an amount from the total tokens out, and update the amount
+                // calculated accordingly
+                hookAdjustedAmountCalculatedRaw -= hookFee;
+                // Vault sends tokens to hook and registers the debt. Since amounts calculated is the amount out of
+                // the swap operation, a credit was already supplied to the current operation, so this hookFee debt
+                // will be discounted from there
+                _vault.sendTo(params.tokenOut, address(this), hookFee);
+            } else {
+                // In EXACT_OUT, the amount calculated is the amount of tokens in. The hook needs to charge the fee
+                // from amount calculated, so we will increase the amount of tokens in that the user needs to pay.
+                hookAdjustedAmountCalculatedRaw += hookFee;
+                // Vault sends tokens to hook and registers the debt. Since amounts calculated is the amount in of
+                // the swap operation, this debt will increase the amount of tokens in that needs to be settled by the
+                // router
+                _vault.sendTo(params.tokenIn, address(this), hookFee);
+            }
+        }
+        return (true, hookAdjustedAmountCalculatedRaw);
+    }
+
+    // Setters
+
+    // Sets the hook swap fee percentage, which will be accrued after a swap was executed
+    function setHookSwapFeePercentage(uint256 feePercentage) external {
+        hookSwapFeePercentage = feePercentage;
+    }
+}
