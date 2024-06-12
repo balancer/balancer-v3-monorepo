98d7d395109787b43deadfb71135045d91a5e421
elshan_eth <raiman.elshan@gmail.com>
2024-06-12 20:26:20 +0200

    fix codestyle

--- pkg/solidity-utils/contracts/openzeppelin/ReentrancyGuardTransient.sol
+++ pkg/solidity-utils/contracts/openzeppelin/ReentrancyGuardTransient.sol
@@ -12,7 +12,8 @@ import { StorageSlot } from "./StorageSlot.sol";
 abstract contract ReentrancyGuardTransient {
     using StorageSlot for *;
 
-    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.ReentrancyGuard")) - 1)) & ~bytes32(uint256(0xff))
+    // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.ReentrancyGuard")) - 1)) &
+    // ~bytes32(uint256(0xff))
     bytes32 private constant _REENTRANCY_GUARD_STORAGE =
         0x9b779b17422d0df92223018b32b4d1fa46e071723d6817e2486d003becc55f00;
 
