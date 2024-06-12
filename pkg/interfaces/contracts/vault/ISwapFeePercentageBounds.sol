3e38a0167672e4a9acd6bee577ace396e14cb331
Jeff Bennett <endymionjkb@gmail.com>
2024-06-12 11:55:16 -0400

    lint

--- pkg/interfaces/contracts/vault/ISwapFeePercentageBounds.sol
+++ pkg/interfaces/contracts/vault/ISwapFeePercentageBounds.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.24;
  * @notice Return the minimum/maximum swap fee percentages for a pool.
  * @dev The Vault does not enforce bounds on swap fee percentages; `IBasePool` implements this interface to ensure
  * that new pool developers think about and set these bounds according to their specific pool type.
- * 
+ *
  * A minimum swap fee might be necessary to ensure mathematical soundness (e.g., Weighted Pools, which use the power
  * function in the invariant). A maximum swap fee is general protection for users. With no limits at the Vault level,
  * a pool could specify a 100% swap fee, effectively disabling trading. Though there are some use cases, such as
