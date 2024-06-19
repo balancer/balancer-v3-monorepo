44a3b301401d96156c40e10d80fa1c19e45ef25e
João Bruno <joaobrunoah@gmail.com>
2024-06-19 11:10:06 -0300

    Fix lint

--- pkg/pool-utils/contracts/test/PoolMockWithDonation.sol
+++ pkg/pool-utils/contracts/test/PoolMockWithDonation.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.24;
 
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
-import { PoolDonation } from "@balancer-labs/v3-pool-utils/contracts/PoolDonation.sol";
-
 import { PoolMockCommon } from "@balancer-labs/v3-vault/contracts/test/PoolMockCommon.sol";
+
+import { PoolDonation } from "../PoolDonation.sol";
 
 contract PoolMockWithDonation is PoolMockCommon, PoolDonation {
     constructor(IVault vault, string memory name, string memory symbol) PoolMockCommon(vault, name, symbol) {
