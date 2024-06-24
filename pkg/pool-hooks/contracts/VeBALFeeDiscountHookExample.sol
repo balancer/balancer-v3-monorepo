c993dc3bf8952ba283885d5bc209e8f67c601967
João Bruno <joaobrunoah@gmail.com>
2024-06-24 18:27:32 -0300

    Vebal hook example

---
+++ pkg/pool-hooks/contracts/VeBALFeeDiscountHookExample.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { IBasePool } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePool.sol";
+import { IHooks } from "@balancer-labs/v3-interfaces/contracts/vault/IHooks.sol";
+import { IRouter } from "@balancer-labs/v3-interfaces/contracts/vault/IRouter.sol";
+import { LiquidityManagement, TokenConfig } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+
+import { BasePoolHooks } from "@balancer-labs/v3-vault/contracts/BasePoolHooks.sol";
+
+contract VeBALFeeDiscountHookExample is BasePoolHooks {
+    // only pools from the allowedFactory are able to register and use this hook
+    address private _allowedFactory;
+    IERC20 private _veBAL;
+
+    constructor(address allowedFactory, address veBAL) {
+        // verify that this hook can only be used by pools created from `_allowedFactory`
+        _allowedFactory = allowedFactory;
+        _veBAL = IERC20(veBAL);
+    }
+
+    /// @inheritdoc IHooks
+    function getHookFlags() external override returns (IHooks.HookFlags memory hookFlags) {
+        return
+            IHooks.HookFlags({
+                enableHookAdjustedAmounts: false,
+                shouldCallBeforeInitialize: false,
+                shouldCallAfterInitialize: false,
+                shouldCallComputeDynamicSwapFee: true,
+                shouldCallBeforeSwap: false,
+                shouldCallAfterSwap: false,
+                shouldCallBeforeAddLiquidity: false,
+                shouldCallAfterAddLiquidity: false,
+                shouldCallBeforeRemoveLiquidity: false,
+                shouldCallAfterRemoveLiquidity: false
+            });
+    }
+
+    /// @inheritdoc IHooks
+    function onRegister(
+        address factory,
+        address pool,
+        TokenConfig[] memory tokenConfig,
+        LiquidityManagement calldata liquidityManagement
+    ) external override returns (bool) {
+        // reverts if the pool is not from the allowed factory
+        return factory == _allowedFactory;
+    }
+
+    /// @inheritdoc IHooks
+    function onComputeDynamicSwapFee(
+        IBasePool.PoolSwapParams calldata params,
+        uint256 staticSwapFeePercentage
+    ) external view override returns (bool, uint256) {
+        address user = IRouter(params.router).getSender();
+
+        if (_veBAL.balanceOf(user) == 0) {
+            return (true, staticSwapFeePercentage);
+        }
+        // If user has veBAL, apply 50% discount in the current fee
+        return (true, staticSwapFeePercentage / 2);
+    }
+}
