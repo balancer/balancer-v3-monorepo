8604c9c3796ec44cfbeea4b8b4bd65f1ed9fbd10
João Bruno <joaobrunoah@gmail.com>
2024-07-01 18:13:07 -0300

    WIP on fix-coverage

--- pvt/common/hardhat-base-config.ts
+++ pvt/common/hardhat-base-config.ts
@@ -17,7 +17,7 @@ export const compilers: [SolcConfig] = [
   {
     version: '0.8.24',
     settings: {
-      viaIR: true,
+      viaIR: false,
       evmVersion: 'cancun',
       optimizer: {
         enabled: true,
@@ -40,17 +40,17 @@ const contractSettings: ContractSettings = {
   '@balancer-labs/v3-vault/contracts': {
     version: compilers[0].version,
     runs: compilers[0].settings.optimizer.runs,
-    viaIR: true,
+    viaIR: false,
   },
   '@balancer-labs/v3-vault/contracts/Vault.sol': {
     version: '0.8.24',
     runs: 200,
-    viaIR: true,
+    viaIR: false,
   },
   '@balancer-labs/v3-vault/contracts/VaultExtension.sol': {
     version: '0.8.24',
     runs: 500,
-    viaIR: true,
+    viaIR: false,
   },
 };
 
