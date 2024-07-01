b24dfc7a58fd87bc64d507f0ffa10d5cb9394420
EndymionJkb <EndymionJkb@gmail.com>
2024-07-01 12:07:02 -0400

    Remove unused HooksConfigLib arguments (#698)

--- pkg/vault/contracts/Vault.sol
+++ pkg/vault/contracts/Vault.sol
@@ -203,7 +203,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
         IBasePool.PoolSwapParams memory swapParams = _buildPoolSwapParams(params, state, poolData);
 
         if (poolData.poolConfigBits.shouldCallBeforeSwap()) {
-            poolData.poolConfigBits.callBeforeSwapHook(swapParams, params.pool, _hooksContracts[params.pool]);
+            HooksConfigLib.callBeforeSwapHook(swapParams, params.pool, _hooksContracts[params.pool]);
 
             // The call to `onBeforeSwap` could potentially update token rates and balances.
             // We update `poolData.tokenRates`, `poolData.rawBalances` and `poolData.balancesLiveScaled18`
@@ -222,9 +222,11 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
         // to be used unless the pool has a dynamic swap fee. It is also passed into the hook, to support common cases
         // where the dynamic fee computation logic uses it.
         if (poolData.poolConfigBits.shouldCallComputeDynamicSwapFee()) {
-            (bool dynamicSwapFeeCalculated, uint256 dynamicSwapFee) = poolData
-                .poolConfigBits
-                .callComputeDynamicSwapFeeHook(swapParams, state.swapFeePercentage, _hooksContracts[params.pool]);
+            (bool dynamicSwapFeeCalculated, uint256 dynamicSwapFee) = HooksConfigLib.callComputeDynamicSwapFeeHook(
+                swapParams,
+                state.swapFeePercentage,
+                _hooksContracts[params.pool]
+            );
 
             if (dynamicSwapFeeCalculated) {
                 state.swapFeePercentage = dynamicSwapFee;
@@ -536,7 +538,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
         );
 
         if (poolData.poolConfigBits.shouldCallBeforeAddLiquidity()) {
-            poolData.poolConfigBits.callBeforeAddLiquidityHook(
+            HooksConfigLib.callBeforeAddLiquidityHook(
                 msg.sender,
                 maxAmountsInScaled18,
                 params,
@@ -774,7 +776,7 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
 
         // Uses msg.sender as the router (the contract that called the vault)
         if (poolData.poolConfigBits.shouldCallBeforeRemoveLiquidity()) {
-            poolData.poolConfigBits.callBeforeRemoveLiquidityHook(
+            HooksConfigLib.callBeforeRemoveLiquidityHook(
                 minAmountsOutScaled18,
                 msg.sender,
                 params,
