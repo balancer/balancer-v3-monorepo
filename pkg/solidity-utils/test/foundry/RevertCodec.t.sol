3a44336d038bc17644fded84808082022c0b54a6
Jeff Bennett <endymionjkb@gmail.com>
2024-07-02 13:30:47 -0400

    test: suppress warnings

--- pkg/solidity-utils/test/foundry/RevertCodec.t.sol
+++ pkg/solidity-utils/test/foundry/RevertCodec.t.sol
@@ -19,7 +19,7 @@ contract RevertCodecTest is Test {
         RevertCodec.catchEncodedResult(bytes(abi.encodeWithSelector(TestCustomError.selector, uint256(123))));
     }
 
-    function testCatchEncodedResultOk() public {
+    function testCatchEncodedResultOk() public pure {
         bytes memory encodedError = abi.encodeWithSelector(RevertCodec.Result.selector, abi.encode(uint256(987), true));
         bytes memory result = RevertCodec.catchEncodedResult(encodedError);
         (uint256 decodedResultInt, bool decodedResultBool) = abi.decode(result, (uint256, bool));
@@ -33,7 +33,7 @@ contract RevertCodecTest is Test {
         RevertCodec.parseSelector("");
     }
 
-    function testParseSelector() public {
+    function testParseSelector() public pure {
         bytes4 selector = RevertCodec.parseSelector(abi.encodePacked(hex"112233445566778899aabbccddeeff"));
         assertEq(selector, bytes4(0x11223344), "Incorrect selector");
     }
