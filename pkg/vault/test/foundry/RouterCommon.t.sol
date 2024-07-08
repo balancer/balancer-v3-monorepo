dd8763fc89159023754cf54fb2ee3523c9e8e433
João Bruno <joaobrunoah@gmail.com>
2024-07-08 14:52:44 -0300

    Fix PR comments

--- pkg/vault/test/foundry/RouterCommon.t.sol
+++ pkg/vault/test/foundry/RouterCommon.t.sol
@@ -10,7 +10,6 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
 import { ReentrancyAttack } from "@balancer-labs/v3-solidity-utils/contracts/test/ReentrancyAttack.sol";
-import { BaseTest } from "@balancer-labs/v3-solidity-utils/test/foundry/utils/BaseTest.sol";
 import { StorageSlotExtension } from "@balancer-labs/v3-solidity-utils/contracts/openzeppelin/StorageSlotExtension.sol";
 
 import { BaseVaultTest } from "./utils/BaseVaultTest.sol";
@@ -19,24 +18,24 @@ import { RouterCommonMock } from "../../contracts/test/RouterCommonMock.sol";
 
 contract RouterCommonTest is BaseVaultTest {
     ReentrancyAttack internal reentrancyAttack;
-    RouterCommonMock internal routerCommonMock;
+    RouterCommonMock internal routerMock;
 
     function setUp() public virtual override {
         super.setUp();
 
-        routerCommonMock = new RouterCommonMock(IVault(address(vault)), weth, permit2);
+        routerMock = new RouterCommonMock(IVault(address(vault)), weth, permit2);
         reentrancyAttack = new ReentrancyAttack();
     }
 
     function testSaveSenderAndCall() external {
         vm.expectEmit();
         emit RouterCommonMock.CurrentSenderMock(address(this));
-        routerCommonMock.call(address(routerCommonMock), abi.encodeWithSelector(RouterCommonMock.emitSender.selector));
+        routerMock.call(address(routerMock), abi.encodeWithSelector(RouterCommonMock.emitSender.selector));
     }
 
     function testSenderSlot() external view {
         assertEq(
-            StorageSlotExtension.AddressSlotType.unwrap(routerCommonMock.manualGetSenderSlot()),
+            StorageSlotExtension.AddressSlotType.unwrap(routerMock.manualGetSenderSlot()),
             keccak256(abi.encode(uint256(keccak256("balancer-labs.v3.storage.RouterCommon.sender")) - 1)) &
                 ~bytes32(uint256(0xff))
         );
@@ -47,13 +46,13 @@ contract RouterCommonTest is BaseVaultTest {
         vm.expectEmit();
         emit RouterCommonMock.CurrentSenderMock(address(this));
 
-        routerCommonMock.call(
+        routerMock.call(
             address(reentrancyAttack),
             abi.encodeWithSelector(
                 ReentrancyAttack.callSender.selector,
                 abi.encodeWithSelector(
                     RouterCommonMock.call.selector,
-                    routerCommonMock,
+                    routerMock,
                     abi.encodeWithSelector(RouterCommonMock.emitSender.selector)
                 )
             )
@@ -61,64 +60,91 @@ contract RouterCommonTest is BaseVaultTest {
     }
 
     function testTakeTokenInInsufficientEth() public {
-        uint256 routerEthBalance = address(routerCommonMock).balance;
+        uint256 routerEthBalance = address(routerMock).balance;
 
         vm.expectRevert(RouterCommon.InsufficientEth.selector);
-        routerCommonMock.mockTakeTokenIn(bob, IERC20(weth), routerEthBalance + 1, true);
+        routerMock.mockTakeTokenIn(bob, IERC20(weth), routerEthBalance + 1, true);
     }
 
     function testTakeTokenInWEthIsNotEth() public {
         vault.manualSetIsUnlocked(true);
         uint256 amountToDeposit = weth.balanceOf(bob) / 100;
 
-        uint256 wethBobBefore = IERC20(address(weth)).balanceOf(bob);
-        uint256 wethVaultBefore = IERC20(address(weth)).balanceOf(address(vault));
+        EthStateTest memory vars = _createEthStateTest();
 
         vm.startPrank(bob);
         IERC20(weth).approve(address(permit2), type(uint256).max);
-        permit2.approve(address(weth), address(routerCommonMock), type(uint160).max, type(uint48).max);
+        permit2.approve(address(weth), address(routerMock), type(uint160).max, type(uint48).max);
 
-        routerCommonMock.mockTakeTokenIn(bob, IERC20(weth), amountToDeposit, false);
+        routerMock.mockTakeTokenIn(bob, IERC20(weth), amountToDeposit, false);
         vm.stopPrank();
 
-        uint256 wethBobAfter = IERC20(address(weth)).balanceOf(bob);
-        uint256 wethVaultAfter = IERC20(address(weth)).balanceOf(address(vault));
+        _fillEthStateTestAfter(vars);
 
-        assertEq(wethBobAfter, wethBobBefore - amountToDeposit, "Bob weth balance is wrong");
-        assertEq(wethVaultAfter, wethVaultBefore + amountToDeposit, "Vault weth balance is wrong");
+        assertEq(vars.bobEthAfter, vars.bobEthBefore, "Bob ETH balance is wrong");
+        assertEq(vars.vaultEthAfter, vars.vaultEthBefore, "Vault ETH balance is wrong");
+        assertEq(vars.bobWethAfter, vars.bobWethBefore - amountToDeposit, "Bob WETH balance is wrong");
+        assertEq(vars.vaultWethAfter, vars.vaultWethBefore + amountToDeposit, "Vault WETH balance is wrong");
     }
 
     function testSendTokenOutWethIsEth() public {
         vault.manualSetIsUnlocked(true);
 
-        uint256 bobEthBalanceBefore = bob.balance;
-        uint256 vaultEthBalanceBefore = address(vault).balance;
+        EthStateTest memory vars = _createEthStateTest();
 
-        uint256 amountToWithdraw = vaultEthBalanceBefore / 100;
+        uint256 amountToWithdraw = vars.vaultEthBefore / 100;
 
-        routerCommonMock.mockSendTokenOut(bob, IERC20(weth), amountToWithdraw, true);
+        routerMock.mockSendTokenOut(bob, IERC20(weth), amountToWithdraw, true);
 
-        uint256 bobEthBalanceAfter = bob.balance;
-        uint256 vaultEthBalanceAfter = address(vault).balance;
+        _fillEthStateTestAfter(vars);
 
-        assertEq(bobEthBalanceAfter, bobEthBalanceBefore + amountToWithdraw, "Bob ETH balance is wrong");
-        assertEq(vaultEthBalanceAfter, vaultEthBalanceBefore - amountToWithdraw, "Vault ETH balance is wrong");
+        assertEq(vars.bobEthAfter, vars.bobEthBefore + amountToWithdraw, "Bob ETH balance is wrong");
+        assertEq(vars.vaultEthAfter, vars.vaultEthBefore - amountToWithdraw, "Vault ETH balance is wrong");
+        assertEq(vars.bobWethAfter, vars.bobWethBefore, "Bob WETH balance is wrong");
+        assertEq(vars.vaultWethAfter, vars.vaultWethBefore, "Vault WETH balance is wrong");
     }
 
     function testSendTokenOutWethIsNotEth() public {
         vault.manualSetIsUnlocked(true);
 
-        uint256 wethBobBefore = IERC20(address(weth)).balanceOf(bob);
-        uint256 wethVaultBefore = IERC20(address(weth)).balanceOf(address(vault));
+        EthStateTest memory vars = _createEthStateTest();
 
-        uint256 amountToWithdraw = wethVaultBefore / 100;
+        uint256 amountToWithdraw = vars.vaultWethBefore / 100;
 
-        routerCommonMock.mockSendTokenOut(bob, IERC20(weth), amountToWithdraw, false);
+        routerMock.mockSendTokenOut(bob, IERC20(weth), amountToWithdraw, false);
 
-        uint256 wethBobAfter = IERC20(address(weth)).balanceOf(bob);
-        uint256 wethVaultAfter = IERC20(address(weth)).balanceOf(address(vault));
+        _fillEthStateTestAfter(vars);
 
-        assertEq(wethBobAfter, wethBobBefore + amountToWithdraw, "Bob WETH balance is wrong");
-        assertEq(wethVaultAfter, wethVaultBefore - amountToWithdraw, "Vault WETH balance is wrong");
+        assertEq(vars.bobEthAfter, vars.bobEthBefore, "Bob ETH balance is wrong");
+        assertEq(vars.vaultEthAfter, vars.vaultEthBefore, "Vault ETH balance is wrong");
+        assertEq(vars.bobWethAfter, vars.bobWethBefore + amountToWithdraw, "Bob WETH balance is wrong");
+        assertEq(vars.vaultWethAfter, vars.vaultWethBefore - amountToWithdraw, "Vault WETH balance is wrong");
+    }
+
+    struct EthStateTest {
+        uint256 bobEthBefore;
+        uint256 bobEthAfter;
+        uint256 bobWethBefore;
+        uint256 bobWethAfter;
+        uint256 vaultEthBefore;
+        uint256 vaultEthAfter;
+        uint256 vaultWethBefore;
+        uint256 vaultWethAfter;
+    }
+
+    function _createEthStateTest() private returns (EthStateTest memory vars) {
+        vars.bobEthBefore = bob.balance;
+        vars.bobWethBefore = IERC20(address(weth)).balanceOf(bob);
+
+        vars.vaultEthBefore = address(vault).balance;
+        vars.vaultWethBefore = IERC20(address(weth)).balanceOf(address(vault));
+    }
+
+    function _fillEthStateTestAfter(EthStateTest memory vars) private {
+        vars.bobEthAfter = bob.balance;
+        vars.bobWethAfter = IERC20(address(weth)).balanceOf(bob);
+
+        vars.vaultEthAfter = address(vault).balance;
+        vars.vaultWethAfter = IERC20(address(weth)).balanceOf(address(vault));
     }
 }
