2e0c741c7366aadb1e0b3550a08375c7e3c34ba0
Jeff Bennett <endymionjkb@gmail.com>
2024-07-11 14:00:46 -0400

    lint

--- pkg/interfaces/contracts/vault/IVaultExplorer.sol
+++ pkg/interfaces/contracts/vault/IVaultExplorer.sol
@@ -3,13 +3,7 @@
 pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {
-    TokenInfo,
-    PoolRoleAccounts,
-    PoolData,
-    PoolConfig,
-    HooksConfig
-} from "./VaultTypes.sol";
+import { TokenInfo, PoolRoleAccounts, PoolData, PoolConfig, HooksConfig } from "./VaultTypes.sol";
 
 import { IBasePool } from "./IBasePool.sol";
 
