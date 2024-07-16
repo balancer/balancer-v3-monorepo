97ddaaee919d5aae885854aabc9529119e7674eb
Jeff Bennett <endymionjkb@gmail.com>
2024-07-16 19:34:46 -0400

    lint

--- pkg/pool-hooks/test/foundry/DirectionalFeeHookExample.t.sol
+++ pkg/pool-hooks/test/foundry/DirectionalFeeHookExample.t.sol
@@ -224,19 +224,21 @@ contract DirectionalHookExampleTest is BaseVaultTest {
         );
 
         // Call dynamic fee hook to fetch the expected swap fee percentage
-        (, uint256 expectedSwapFeePercentage) = DirectionalFeeHookExample(poolHooksContract).onComputeDynamicSwapFeePercentage(
-            PoolSwapParams({
-                kind: SwapKind.EXACT_IN,
-                amountGivenScaled18: daiExactAmountIn,
-                balancesScaled18: balancesScaled18,
-                indexIn: daiIdx,
-                indexOut: usdcIdx,
-                router: address(0), // The router is not used by the hook
-                userData: bytes("") // User data is not used by the hook
-            }),
-            pool,
-            SWAP_FEE_PERCENTAGE
-        );
+        vm.prank(address(vault));
+        (, uint256 expectedSwapFeePercentage) = DirectionalFeeHookExample(poolHooksContract)
+            .onComputeDynamicSwapFeePercentage(
+                PoolSwapParams({
+                    kind: SwapKind.EXACT_IN,
+                    amountGivenScaled18: daiExactAmountIn,
+                    balancesScaled18: balancesScaled18,
+                    indexIn: daiIdx,
+                    indexOut: usdcIdx,
+                    router: address(0), // The router is not used by the hook
+                    userData: bytes("") // User data is not used by the hook
+                }),
+                pool,
+                SWAP_FEE_PERCENTAGE
+            );
 
         // Brings pool nearer equilibrium by swapping DAI for USDC
         vm.prank(bob);
