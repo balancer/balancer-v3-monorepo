5b2222bbf4203b75c78673a5a638eb43b80f871f
elshan_eth <raiman.elshan@gmail.com>
2024-07-18 18:32:41 +0200

    add rest

--- pkg/interfaces/contracts/vault/IBatchRouter.sol
+++ pkg/interfaces/contracts/vault/IBatchRouter.sol
@@ -102,7 +102,8 @@ interface IBatchRouter {
      * @param userData Additional (optional) data required for adding liquidity
      * @return bptAmountOut Actual amount of pool tokens received
      */
-    function addLiquidityUnbalancedToBoostedPool(
+    function 
+     (
         address pool,
         uint256[] memory exactUnderlyingAmountsIn,
         uint256 minBptAmountOut,
