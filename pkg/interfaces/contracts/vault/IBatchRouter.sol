da86cef674ac03e9d5f4525efaa48b8e5df66879
Elshan Dzhafarov <raiman.elshan@gmail.com>
2024-07-23 14:48:13 +0200

    Update pkg/interfaces/contracts/vault/IBatchRouter.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/interfaces/contracts/vault/IBatchRouter.sol
+++ pkg/interfaces/contracts/vault/IBatchRouter.sol
@@ -98,7 +98,7 @@ interface IBatchRouter {
      * @param pool Address of the liquidity pool
      * @param exactUnderlyingAmountsIn Exact amounts of underling tokens to be added, sorted in token registration order
      * @param minBptAmountOut Minimum amount of pool tokens to be received
-     * @param wethIsEth If true, incoming ETH will be wrapped to WETH; otherwise the Vault will pull WETH tokens
+     * @param wethIsEth If true, incoming ETH will be wrapped to WETH and outgoing WETH will be unwrapped to ETH
      * @param userData Additional (optional) data required for adding liquidity
      * @return bptAmountOut Actual amount of pool tokens received
      */
