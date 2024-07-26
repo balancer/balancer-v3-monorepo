eb180277c6cb3372f841d53b09a96b20d6004108
Jeff Bennett <endymionjkb@gmail.com>
2024-07-25 20:03:21 -0400

    docs: straggler

--- pkg/vault/test/foundry/VaultSwapRate.t.sol
+++ pkg/vault/test/foundry/VaultSwapRate.t.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 import { IBasePool } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePool.sol";
 import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/vault/IRateProvider.sol";
