e6c31a5bd522b356e7e1733ccb96fcb957622d9c
elshan_eth <raiman.elshan@gmail.com>
2024-07-30 23:06:06 +0200

    add BaseERC4626BufferTest

--- pkg/vault/test/foundry/QueryERC4626Buffer.t.sol
+++ pkg/vault/test/foundry/QueryERC4626Buffer.t.sol
@@ -6,96 +6,74 @@ import "forge-std/Test.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { TokenConfig, TokenType } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
-import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 import { IBatchRouter } from "@balancer-labs/v3-interfaces/contracts/vault/IBatchRouter.sol";
-import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/vault/IRateProvider.sol";
-
-import { ERC4626TestToken } from "@balancer-labs/v3-solidity-utils/contracts/test/ERC4626TestToken.sol";
 import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/ArrayHelpers.sol";
 
-import { PoolMock } from "../../contracts/test/PoolMock.sol";
+import { BaseERC4626BufferTest } from "./utils/BaseERC4626BufferTest.sol";
 
-import { BaseVaultTest } from "./utils/BaseVaultTest.sol";
-
-contract QueryERC4626BufferTest is BaseVaultTest {
+contract QueryERC4626BufferTest is BaseERC4626BufferTest {
     using ArrayHelpers for *;
 
-    ERC4626TestToken internal waDAI;
-    ERC4626TestToken internal waUSDC;
-
-    // For two-token pools with waDAI/waUSDC, keep track of sorted token order.
-    uint256 internal waDaiIdx;
-    uint256 internal waUsdcIdx;
-
-    address internal yieldBearingPool;
-
-    // The yield-bearing pool will have 100x the liquidity of the buffer.
-    uint256 internal yieldBearingPoolAmount = 10e6 * 1e18;
-    uint256 internal bufferAmount = yieldBearingPoolAmount / 100;
-    uint256 internal tooLargeSwapAmount = yieldBearingPoolAmount / 2;
+    uint256 internal tooLargeSwapAmount = erc4626PoolInitialAmount / 2;
     // We will swap with 10% of the buffer.
-    uint256 internal swapAmount = bufferAmount / 10;
+    uint256 internal swapAmount = bufferInitialAmount / 10;
 
     function setUp() public virtual override {
-        BaseVaultTest.setUp();
-
-        waDAI = new ERC4626TestToken(dai, "Wrapped aDAI", "waDAI", 18);
-        vm.label(address(waDAI), "waDAI");
-
-        // "USDC" is deliberately 18 decimals to test one thing at a time.
-        waUSDC = new ERC4626TestToken(usdc, "Wrapped aUSDC", "waUSDC", 18);
-        vm.label(address(waUSDC), "waUSDC");
-
-        (waDaiIdx, waUsdcIdx) = getSortedIndexes(address(waDAI), address(waUSDC));
-
-        _initializeBuffers();
-        _initializeYieldBearingPool();
+        bufferInitialAmount = erc4626PoolInitialAmount / 100;
+        BaseERC4626BufferTest.setUp();
         _initializeUser();
     }
 
     function testSwapPreconditions() public view {
         // Bob should have the full yield-bearing pool BPT.
         assertEq(
-            IERC20(yieldBearingPool).balanceOf(bob),
-            yieldBearingPoolAmount * 2 - MIN_BPT,
+            IERC20(erc4626Pool).balanceOf(bob),
+            erc4626PoolInitialAmount * 2 - MIN_BPT,
             "Wrong yield-bearing pool BPT amount"
         );
 
-        (IERC20[] memory tokens, , uint256[] memory balancesRaw, ) = vault.getPoolTokenInfo(yieldBearingPool);
-        // The yield-bearing pool should have `yieldBearingPoolAmount` of both tokens.
+        (IERC20[] memory tokens, , uint256[] memory balancesRaw, ) = vault.getPoolTokenInfo(erc4626Pool);
+        // The yield-bearing pool should have `erc4626PoolInitialAmount` of both tokens.
         assertEq(address(tokens[waDaiIdx]), address(waDAI), "Wrong yield-bearing pool token (waDAI)");
         assertEq(address(tokens[waUsdcIdx]), address(waUSDC), "Wrong yield-bearing pool token (waUSDC)");
-        assertEq(balancesRaw[0], yieldBearingPoolAmount, "Wrong yield-bearing pool balance [0]");
-        assertEq(balancesRaw[1], yieldBearingPoolAmount, "Wrong yield-bearing pool balance [1]");
+        assertEq(balancesRaw[0], erc4626PoolInitialAmount, "Wrong yield-bearing pool balance [0]");
+        assertEq(balancesRaw[1], erc4626PoolInitialAmount, "Wrong yield-bearing pool balance [1]");
 
         // LP should have correct amount of shares from buffer (invested amount in underlying minus burned "BPTs").
         assertEq(
             vault.getBufferOwnerShares(IERC20(waDAI), lp),
-            bufferAmount * 2 - MIN_BPT,
+            bufferInitialAmount * 2 - MIN_BPT,
             "Wrong share of waDAI buffer belonging to LP"
         );
         assertEq(
             vault.getBufferOwnerShares(IERC20(waUSDC), lp),
-            bufferAmount * 2 - MIN_BPT,
+            bufferInitialAmount * 2 - MIN_BPT,
             "Wrong share of waUSDC buffer belonging to LP"
         );
 
         // Buffer should have the correct amount of issued shares.
-        assertEq(vault.getBufferTotalShares(IERC20(waDAI)), bufferAmount * 2, "Wrong issued shares of waDAI buffer");
-        assertEq(vault.getBufferTotalShares(IERC20(waUSDC)), bufferAmount * 2, "Wrong issued shares of waUSDC buffer");
+        assertEq(
+            vault.getBufferTotalShares(IERC20(waDAI)),
+            bufferInitialAmount * 2,
+            "Wrong issued shares of waDAI buffer"
+        );
+        assertEq(
+            vault.getBufferTotalShares(IERC20(waUSDC)),
+            bufferInitialAmount * 2,
+            "Wrong issued shares of waUSDC buffer"
+        );
 
         uint256 baseBalance;
         uint256 wrappedBalance;
 
-        // The vault buffers should each have `bufferAmount` of their respective tokens.
+        // The vault buffers should each have `bufferInitialAmount` of their respective tokens.
         (baseBalance, wrappedBalance) = vault.getBufferBalance(IERC20(waDAI));
-        assertEq(baseBalance, bufferAmount, "Wrong waDAI buffer balance for base token");
-        assertEq(wrappedBalance, bufferAmount, "Wrong waDAI buffer balance for wrapped token");
+        assertEq(baseBalance, bufferInitialAmount, "Wrong waDAI buffer balance for base token");
+        assertEq(wrappedBalance, bufferInitialAmount, "Wrong waDAI buffer balance for wrapped token");
 
         (baseBalance, wrappedBalance) = vault.getBufferBalance(IERC20(waUSDC));
-        assertEq(baseBalance, bufferAmount, "Wrong waUSDC buffer balance for base token");
-        assertEq(wrappedBalance, bufferAmount, "Wrong waUSDC buffer balance for wrapped token");
+        assertEq(baseBalance, bufferInitialAmount, "Wrong waUSDC buffer balance for base token");
+        assertEq(wrappedBalance, bufferInitialAmount, "Wrong waUSDC buffer balance for wrapped token");
     }
 
     function testQuerySwapWithinBufferRangeExactIn() public {
@@ -183,7 +161,7 @@ contract QueryERC4626BufferTest is BaseVaultTest {
         // and finally post-swap the waUSDC through the USDC buffer to calculate the USDC amount out.
         // The only token transfers are DAI in (given) and USDC out (calculated).
         steps[0] = IBatchRouter.SwapPathStep({ pool: address(waDAI), tokenOut: waDAI, isBuffer: true });
-        steps[1] = IBatchRouter.SwapPathStep({ pool: yieldBearingPool, tokenOut: waUSDC, isBuffer: false });
+        steps[1] = IBatchRouter.SwapPathStep({ pool: erc4626Pool, tokenOut: waUSDC, isBuffer: false });
         steps[2] = IBatchRouter.SwapPathStep({ pool: address(waUSDC), tokenOut: usdc, isBuffer: true });
 
         paths[0] = IBatchRouter.SwapPathExactAmountIn({
@@ -205,7 +183,7 @@ contract QueryERC4626BufferTest is BaseVaultTest {
         // and finally post-swap the waDAI for DAI through the DAI buffer to calculate the DAI amount in.
         // The only token transfers are DAI in (calculated) and USDC out (given).
         steps[0] = IBatchRouter.SwapPathStep({ pool: address(waDAI), tokenOut: waDAI, isBuffer: true });
-        steps[1] = IBatchRouter.SwapPathStep({ pool: yieldBearingPool, tokenOut: waUSDC, isBuffer: false });
+        steps[1] = IBatchRouter.SwapPathStep({ pool: erc4626Pool, tokenOut: waUSDC, isBuffer: false });
         steps[2] = IBatchRouter.SwapPathStep({ pool: address(waUSDC), tokenOut: usdc, isBuffer: true });
 
         paths[0] = IBatchRouter.SwapPathExactAmountOut({
@@ -216,72 +194,7 @@ contract QueryERC4626BufferTest is BaseVaultTest {
         });
     }
 
-    function _initializeBuffers() private {
-        // Create and fund buffer pools.
-        vm.startPrank(lp);
-        dai.mint(lp, bufferAmount);
-        dai.approve(address(waDAI), bufferAmount);
-        waDAI.deposit(bufferAmount, lp);
-
-        usdc.mint(lp, bufferAmount);
-        usdc.approve(address(waUSDC), bufferAmount);
-        waUSDC.deposit(bufferAmount, lp);
-        vm.stopPrank();
-
-        vm.startPrank(lp);
-        waDAI.approve(address(permit2), MAX_UINT256);
-        permit2.approve(address(waDAI), address(router), type(uint160).max, type(uint48).max);
-        permit2.approve(address(waDAI), address(batchRouter), type(uint160).max, type(uint48).max);
-        waUSDC.approve(address(permit2), MAX_UINT256);
-        permit2.approve(address(waUSDC), address(router), type(uint160).max, type(uint48).max);
-        permit2.approve(address(waUSDC), address(batchRouter), type(uint160).max, type(uint48).max);
-
-        router.addLiquidityToBuffer(waDAI, bufferAmount, bufferAmount, lp);
-        router.addLiquidityToBuffer(waUSDC, bufferAmount, bufferAmount, lp);
-        vm.stopPrank();
-    }
-
-    function _initializeYieldBearingPool() private {
-        TokenConfig[] memory tokenConfig = new TokenConfig[](2);
-        tokenConfig[waDaiIdx].token = IERC20(waDAI);
-        tokenConfig[waUsdcIdx].token = IERC20(waUSDC);
-        tokenConfig[0].tokenType = TokenType.WITH_RATE;
-        tokenConfig[1].tokenType = TokenType.WITH_RATE;
-        tokenConfig[waDaiIdx].rateProvider = IRateProvider(address(waDAI));
-        tokenConfig[waUsdcIdx].rateProvider = IRateProvider(address(waUSDC));
-
-        PoolMock newPool = new PoolMock(IVault(address(vault)), "Yield-bearing Pool", "YIELDYBOI");
-
-        factoryMock.registerTestPool(address(newPool), tokenConfig);
-
-        vm.label(address(newPool), "yield-bearing pool");
-        yieldBearingPool = address(newPool);
-
-        vm.startPrank(bob);
-        waDAI.approve(address(permit2), MAX_UINT256);
-        permit2.approve(address(waDAI), address(router), type(uint160).max, type(uint48).max);
-        permit2.approve(address(waDAI), address(batchRouter), type(uint160).max, type(uint48).max);
-        waUSDC.approve(address(permit2), MAX_UINT256);
-        permit2.approve(address(waUSDC), address(router), type(uint160).max, type(uint48).max);
-        permit2.approve(address(waUSDC), address(batchRouter), type(uint160).max, type(uint48).max);
-
-        dai.mint(bob, yieldBearingPoolAmount);
-        dai.approve(address(waDAI), yieldBearingPoolAmount);
-        waDAI.deposit(yieldBearingPoolAmount, bob);
-
-        usdc.mint(bob, yieldBearingPoolAmount);
-        usdc.approve(address(waUSDC), yieldBearingPoolAmount);
-        waUSDC.deposit(yieldBearingPoolAmount, bob);
-
-        _initPool(
-            yieldBearingPool,
-            [yieldBearingPoolAmount, yieldBearingPoolAmount].toMemoryArray(),
-            yieldBearingPoolAmount * 2 - MIN_BPT
-        );
-        vm.stopPrank();
-    }
-
     function _initializeUser() private {
-        dai.mint(alice, yieldBearingPoolAmount);
+        dai.mint(alice, erc4626PoolInitialAmount);
     }
 }
