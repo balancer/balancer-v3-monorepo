e7116837966ac362150f9adb63b42aca8c21ecc9
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2024-08-05 12:20:39 -0300

    Update pkg/vault/test/foundry/E2eSwap.t.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/vault/test/foundry/E2eSwap.t.sol
+++ pkg/vault/test/foundry/E2eSwap.t.sol
@@ -78,7 +78,7 @@ contract E2eSwapTest is BaseVaultTest {
     }
 
     /**
-     * @notice Override pool created by BaseVaultTest
+     * @notice Override pool created by BaseVaultTest.
      * @dev For this test to be generic and support tokens with different decimals, tokenA and tokenB must be set by
      * _setUpVariables function. If this function runs before `BaseVaultTest.setUp()`, in the `setUp()` function, tokens
      * defined by BaseTest (like dai and usdc) cannot be used. If it runs after, we don't know which tokens are used to
