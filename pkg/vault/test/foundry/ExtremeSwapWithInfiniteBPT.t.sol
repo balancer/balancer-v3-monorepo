6d4fa77e0822881ee4f508df93ee0bdd20004486
elshan_eth <raiman.elshan@gmail.com>
2024-08-06 18:17:34 +0200

    fix import

--- pkg/vault/test/foundry/ExtremeSwapWithInfiniteBPT.t.sol
+++ pkg/vault/test/foundry/ExtremeSwapWithInfiniteBPT.t.sol
@@ -8,7 +8,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 import { TokenConfig } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 
-import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/ArrayHelpers.sol";
+import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/test/ArrayHelpers.sol";
 
 import { PoolMock } from "../../contracts/test/PoolMock.sol";
 
