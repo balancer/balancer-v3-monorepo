e19f5a15ad4cf9f5ae9fa5c1f1f481bef8a8ed59
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2024-08-20 10:54:43 -0300

    Update pkg/vault/test/foundry/utils/BaseERC4626BufferTest.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/vault/test/foundry/utils/BaseERC4626BufferTest.sol
+++ pkg/vault/test/foundry/utils/BaseERC4626BufferTest.sol
@@ -32,7 +32,7 @@ abstract contract BaseERC4626BufferTest is BaseVaultTest {
 
     // Rounding issues are introduced when dealing with tokens with rates different than 1:1. For example, to scale the
     // tokens of an yield-bearing pool, the amount of tokens is multiplied by the rate of the token, which is
-    // calculated using `convertToAssets(FixedPoint.ONE)`. It generates an 18 decimals rate, but quantities bigger than
+    // calculated using `convertToAssets(FixedPoint.ONE)`. It generates an 18 decimal rate, but quantities bigger than
     // 1e18 will have rounding issues. Another example is the different between convert (used to calculate query
     // results of buffer operations) and the actual operation.
     uint256 internal errorTolerance = 1e8;
