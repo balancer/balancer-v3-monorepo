8afdb62d6364f760d02c37906a2cee5785fb4715
johngrantuk <johngrantuk@googlemail.com>
2024-08-29 13:03:29 +0100

    feat: Add StableSurge hook example with tests.

---
+++ pkg/pool-hooks/contracts/StableSurgeHookExample.sol
@@ -0,0 +1,237 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+import { IBasePoolFactory } from "@balancer-labs/v3-interfaces/contracts/vault/IBasePoolFactory.sol";
+import { IHooks } from "@balancer-labs/v3-interfaces/contracts/vault/IHooks.sol";
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import { IStablePool } from "@balancer-labs/v3-interfaces/contracts/pool-stable/IStablePool.sol";
+import {
+    LiquidityManagement,
+    TokenConfig,
+    PoolSwapParams,
+    HookFlags,
+    SwapKind
+} from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+import { VaultGuard } from "@balancer-labs/v3-vault/contracts/VaultGuard.sol";
+import { BaseHooks } from "@balancer-labs/v3-vault/contracts/BaseHooks.sol";
+
+import { StableMath } from "@balancer-labs/v3-solidity-utils/contracts/math/StableMath.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+
+/**
+ * @notice Hook that applies a fee for out of range or undesirable amounts of tokens in relation to a threshold.
+ * @dev Uses the dynamic fee mechanism to apply a directional fee.
+ */
+contract StableSurgeHookExample is BaseHooks, VaultGuard, Ownable {
+    using FixedPoint for uint256;
+    // Only pools from a specific factory are able to register and use this hook.
+    address private immutable _allowedFactory;
+    // Defines the range in which surging will not occur; meaning surging will occur when:
+    // B(i-after swap) / (sum(B(n-after swap))) > 1/n + gamma
+    uint256 private _threshold;
+    // An amplification coefficient to amplify the degree a fee increases upon the threshold being triggered
+    uint256 private _surgeCoefficient;
+
+    /**
+     * @notice A new `StableSurgeHookExample` contract has been registered successfully.
+     * @dev If the registration fails the call will revert, so there will be no event.
+     * @param hooksContract This contract
+     * @param factory The factory (must be the allowed factory, or the call will revert)
+     * @param pool The pool on which the hook was registered
+     */
+    event StableSurgeHookExampleRegistered(
+        address indexed hooksContract,
+        address indexed factory,
+        address indexed pool
+    );
+
+    /**
+     * @notice The threshold has been changed in a `StableSurgeHookExample` contract.
+     * @dev Note, the initial threshold is set on deployment and an event is emitted.
+     * @param hooksContract This contract
+     * @param threshold The new threshold
+     */
+    event ThresholdChanged(address indexed hooksContract, uint256 indexed threshold);
+
+    /**
+     * @notice The surgeCoefficient has been changed in a `StableSurgeHookExample` contract.
+     * @dev Note, the initial surgeCoefficient is set on deployment and an event is emitted.
+     * @param hooksContract This contract
+     * @param surgeCoefficient The new surgeCoefficient
+     */
+    event SurgeCoefficientChanged(address indexed hooksContract, uint256 indexed surgeCoefficient);
+
+    constructor(
+        IVault vault,
+        address allowedFactory,
+        uint256 threshold,
+        uint256 surgeCoefficient
+    ) VaultGuard(vault) Ownable(msg.sender) {
+        _allowedFactory = allowedFactory;
+        _threshold = threshold;
+        _surgeCoefficient = surgeCoefficient;
+        emit ThresholdChanged(address(this), threshold);
+        emit SurgeCoefficientChanged(address(this), surgeCoefficient);
+    }
+
+    /// @inheritdoc IHooks
+    function getHookFlags() public pure override returns (HookFlags memory hookFlags) {
+        hookFlags.shouldCallComputeDynamicSwapFee = true;
+    }
+
+    /// @inheritdoc IHooks
+    function onRegister(
+        address factory,
+        address pool,
+        TokenConfig[] memory,
+        LiquidityManagement calldata
+    ) public override onlyVault returns (bool) {
+        // This hook implements a restrictive approach, where we check if the factory is an allowed factory and if
+        // the pool was created by the allowed factory.
+        emit StableSurgeHookExampleRegistered(address(this), factory, pool);
+
+        return factory == _allowedFactory && IBasePoolFactory(factory).isPoolFromFactory(pool);
+    }
+
+    /// @inheritdoc IHooks
+    function onComputeDynamicSwapFeePercentage(
+        PoolSwapParams calldata params,
+        address pool,
+        uint256 staticSwapFeePercentage
+    ) public view override onlyVault returns (bool, uint256) {
+        uint256 amp;
+        (amp, , ) = IStablePool(pool).getAmplificationParameter();
+
+        // In order to calculate weightAfterSwap we need balances after swap so we must compute swap amount
+        uint256 invariant = StableMath.computeInvariant(amp, params.balancesScaled18);
+        uint256 weightAfterSwap;
+        if (params.kind == SwapKind.EXACT_IN) {
+            uint256 amountCalculatedScaled18 = StableMath.computeOutGivenExactIn(
+                amp,
+                params.balancesScaled18,
+                params.indexIn,
+                params.indexOut,
+                params.amountGivenScaled18,
+                invariant
+            );
+            // Swap fee is always a percentage of the amountCalculated. On ExactIn, subtract it from the calculated
+            // amountOut. Round up to avoid losses during precision loss.
+            uint256 swapFeeAmountScaled18 = amountCalculatedScaled18.mulUp(staticSwapFeePercentage);
+            amountCalculatedScaled18 -= swapFeeAmountScaled18;
+            weightAfterSwap = getWeightAfterSwap(
+                params.balancesScaled18,
+                params.indexIn,
+                params.amountGivenScaled18,
+                amountCalculatedScaled18
+            );
+        } else {
+            uint256 amountCalculatedScaled18 = StableMath.computeInGivenExactOut(
+                amp,
+                params.balancesScaled18,
+                params.indexIn,
+                params.indexOut,
+                params.amountGivenScaled18,
+                invariant
+            );
+            // To ensure symmetry with EXACT_IN, the swap fee used by ExactOut is
+            // `amountCalculated * fee% / (100% - fee%)`. Add it to the calculated amountIn. Round up to avoid losses
+            // during precision loss.
+            uint256 swapFeeAmountScaled18 = amountCalculatedScaled18.mulDivUp(
+                staticSwapFeePercentage,
+                staticSwapFeePercentage.complement()
+            );
+
+            amountCalculatedScaled18 += swapFeeAmountScaled18;
+            weightAfterSwap = getWeightAfterSwap(
+                params.balancesScaled18,
+                params.indexIn,
+                amountCalculatedScaled18,
+                params.amountGivenScaled18
+            );
+        }
+
+        uint256 thresholdBoundary = getThresholdBoundary(params.balancesScaled18.length, _threshold);
+        if (weightAfterSwap > thresholdBoundary) {
+            return (true, getSurgeFee(weightAfterSwap, thresholdBoundary, staticSwapFeePercentage, _surgeCoefficient));
+        } else {
+            return (true, staticSwapFeePercentage);
+        }
+    }
+
+    /**
+     * Defines the range in which surging will not occur.
+     * @dev An expected value for threshold in a 2 token (n=2) would be 0.1.
+     * This would mean surging would occur if any token reaches 60% of the total of balances.
+     * @param numberOfAssets Number of assets in the pool.
+     * @param threshold Theshold value.
+     */
+    function getThresholdBoundary(uint256 numberOfAssets, uint256 threshold) public pure returns (uint256) {
+        return 1e18 / numberOfAssets + threshold;
+    }
+
+    /**
+     * The weight after swap, used to determine if surge fee should be applied.
+     * @param balancesScaled18 Balances of pool
+     * @param indexIn Index of token in
+     * @param amountInScaled18 Amount in of swap
+     * @param amountOutScaled18 Amount out of swap
+     */
+    function getWeightAfterSwap(
+        uint256[] memory balancesScaled18,
+        uint256 indexIn,
+        uint256 amountInScaled18,
+        uint256 amountOutScaled18
+    ) public pure returns (uint256) {
+        uint256 balancesTotal;
+        for (uint256 i = 0; i < balancesScaled18.length; ++i) {
+            balancesTotal += balancesScaled18[i];
+        }
+        uint256 balanceTokenInAfterSwap = balancesScaled18[indexIn] + amountInScaled18;
+        uint256 balancesTotalAfterSwap = balancesTotal + amountInScaled18 - amountOutScaled18;
+        return balanceTokenInAfterSwap.divDown(balancesTotalAfterSwap);
+    }
+
+    /**
+     * A fee based on the virtual weights of the tokens.
+     * @param weightAfterSwap Weight after swap
+     * @param thresholdBoundary Threshold that surge fee will be applied
+     * @param swapFeePercentage Pools static swap fee
+     * @param surgeCoefficient Amplification coefficient to amplify the degree a fee increases
+     */
+    function getSurgeFee(
+        uint256 weightAfterSwap,
+        uint256 thresholdBoundary,
+        uint256 swapFeePercentage,
+        uint256 surgeCoefficient
+    ) public pure returns (uint256) {
+        uint256 weightRatio = weightAfterSwap.divDown(thresholdBoundary);
+        return swapFeePercentage.mulDown(surgeCoefficient).mulDown(weightRatio);
+    }
+
+    // Permissioned functions
+
+    /**
+     * @notice Sets the hook threshold.
+     * @dev This function must be permissioned.
+     */
+    function setThreshold(uint64 newThreshold) external onlyOwner {
+        _threshold = newThreshold;
+
+        emit ThresholdChanged(address(this), newThreshold);
+    }
+
+    /**
+     * @notice Sets the hook surgeCoefficient.
+     * @dev This function must be permissioned.
+     */
+    function setSurgeCoefficient(uint64 newSurgeCoefficient) external onlyOwner {
+        _surgeCoefficient = newSurgeCoefficient;
+
+        emit SurgeCoefficientChanged(address(this), newSurgeCoefficient);
+    }
+}
---
+++ pkg/pool-hooks/test/foundry/StableSurgeExample.t.sol
@@ -0,0 +1,333 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import { IVaultAdmin } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultAdmin.sol";
+import {
+    PoolRoleAccounts,
+    SwapKind,
+    PoolSwapParams
+} from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+
+import { CastingHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/CastingHelpers.sol";
+import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/test/ArrayHelpers.sol";
+import { StablePoolFactory } from "@balancer-labs/v3-pool-stable/contracts/StablePoolFactory.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+import { StableMath } from "@balancer-labs/v3-solidity-utils/contracts/math/StableMath.sol";
+import { BaseVaultTest } from "@balancer-labs/v3-vault/test/foundry/utils/BaseVaultTest.sol";
+
+import { StableSurgeHookExample } from "../../contracts/StableSurgeHookExample.sol";
+
+contract StableSurgeHookExampleTest is BaseVaultTest {
+    using CastingHelpers for address[];
+    using ArrayHelpers for *;
+    using FixedPoint for uint256;
+
+    uint256 internal daiIdx;
+    uint256 internal usdcIdx;
+
+    StablePoolFactory internal stablePoolFactory;
+
+    uint64 SWAP_FEE_PERCENTAGE = 0.0004e18;
+    uint256 AMP_FACTOR = 200;
+    // A threshold of 0.1 for a 2 token pool would mean surging would occur if any token reaches 60% of the total of balances.
+    uint256 THRESHOLD = 0.1e18;
+    uint256 SURGE_COEFFICIENT = 50e18;
+
+    // The following reference values were calculated manually using settings above
+    uint256 initialAmount = 50e18;
+    uint256[] balancesScaled18 = [initialAmount, initialAmount].toMemoryArray();
+    uint256 expectedThresholdBoundary = 0.6e18;
+    uint256 amountInBelowThreshold = 1e18;
+    uint256 amountInAboveThreshold = 30e18;
+    // Return from GivenIn swap, input=amountInAboveThreshold, minus fee
+    uint256 amountOutAboveThreshold = 29.850106042102014697e18;
+    uint256 expectedSurgeFee = 0.026626754770082000e18;
+
+    function setUp() public override {
+        super.setUp();
+        (daiIdx, usdcIdx) = getSortedIndexes(address(dai), address(usdc));
+    }
+
+    function createHook() internal override returns (address) {
+        stablePoolFactory = new StablePoolFactory(IVault(address(vault)), 365 days, "Factory v1", "Pool v1");
+        // LP will be the owner of the hook.
+        vm.prank(lp);
+        address stableSurgeHook = address(
+            new StableSurgeHookExample(IVault(address(vault)), address(stablePoolFactory), THRESHOLD, SURGE_COEFFICIENT)
+        );
+        vm.label(stableSurgeHook, "Stable Surge Hook");
+        return stableSurgeHook;
+    }
+
+    // Overrides pool creation to use StablePool
+    function _createPool(address[] memory tokens, string memory label) internal override returns (address) {
+        PoolRoleAccounts memory roleAccounts;
+
+        vm.expectEmit(true, true, false, false);
+        emit StableSurgeHookExample.StableSurgeHookExampleRegistered(
+            poolHooksContract,
+            address(stablePoolFactory),
+            address(0)
+        );
+
+        address newPool = address(
+            stablePoolFactory.create(
+                "Stable Pool Test",
+                "STABLE-TEST",
+                vault.buildTokenConfig(tokens.asIERC20()),
+                AMP_FACTOR,
+                roleAccounts,
+                MIN_SWAP_FEE,
+                poolHooksContract,
+                false, // Does not allow donations
+                false, // Do not disable unbalanced add/remove liquidity
+                ZERO_BYTES32
+            )
+        );
+        vm.label(newPool, label);
+
+        authorizer.grantRole(vault.getActionId(IVaultAdmin.setStaticSwapFeePercentage.selector), admin);
+        vm.prank(admin);
+        vault.setStaticSwapFeePercentage(newPool, SWAP_FEE_PERCENTAGE);
+
+        return newPool;
+    }
+
+    // Overrides pool init to use initial amounts matches to maths test
+    function initPool() internal virtual override {
+        vm.startPrank(lp);
+        _initPool(pool, balancesScaled18, 0);
+        vm.stopPrank();
+    }
+
+    function testGetThresholdBoundary() public view {
+        uint256 thresholdBoundary = StableSurgeHookExample(poolHooksContract).getThresholdBoundary(
+            balancesScaled18.length,
+            THRESHOLD
+        );
+        assertEq(thresholdBoundary, expectedThresholdBoundary, "Incorrect threshold boundary calculation");
+    }
+
+    function testComputeSurgeFee() public view {
+        uint256 thresholdBoundary = StableSurgeHookExample(poolHooksContract).getThresholdBoundary(
+            balancesScaled18.length,
+            THRESHOLD
+        );
+        uint256 weightAfterSwap = StableSurgeHookExample(poolHooksContract).getWeightAfterSwap(
+            balancesScaled18,
+            daiIdx,
+            amountInAboveThreshold,
+            amountOutAboveThreshold
+        );
+        uint256 surgeFee = StableSurgeHookExample(poolHooksContract).getSurgeFee(
+            weightAfterSwap,
+            thresholdBoundary,
+            SWAP_FEE_PERCENTAGE,
+            SURGE_COEFFICIENT
+        );
+        assertEq(surgeFee, expectedSurgeFee, "Incorrect surge fee calculation");
+    }
+
+    function testHookSwapFeeBelowThreshold() public {
+        // amount in results in below threshold so fee should be static
+        vm.prank(address(vault));
+        (, uint256 hookSwapFeePercentage) = StableSurgeHookExample(poolHooksContract).onComputeDynamicSwapFeePercentage(
+            PoolSwapParams({
+                kind: SwapKind.EXACT_IN,
+                amountGivenScaled18: amountInBelowThreshold,
+                balancesScaled18: balancesScaled18,
+                indexIn: daiIdx,
+                indexOut: usdcIdx,
+                router: address(0), // The router is not used by the hook
+                userData: bytes("") // User data is not used by the hook
+            }),
+            pool,
+            SWAP_FEE_PERCENTAGE
+        );
+        assertEq(hookSwapFeePercentage, SWAP_FEE_PERCENTAGE, "Should be static swap fee");
+    }
+
+    function testHookSwapFeeAboveThreshold() public {
+        // amount in results in above threshold so fee should surge
+        vm.prank(address(vault));
+        (, uint256 hookSwapFeePercentage) = StableSurgeHookExample(poolHooksContract).onComputeDynamicSwapFeePercentage(
+            PoolSwapParams({
+                kind: SwapKind.EXACT_IN,
+                amountGivenScaled18: amountInAboveThreshold,
+                balancesScaled18: balancesScaled18,
+                indexIn: daiIdx,
+                indexOut: usdcIdx,
+                router: address(0), // The router is not used by the hook
+                userData: bytes("") // User data is not used by the hook
+            }),
+            pool,
+            SWAP_FEE_PERCENTAGE
+        );
+        assertEq(hookSwapFeePercentage, expectedSurgeFee, "Should be surge fee");
+    }
+
+    function testSwapBelowThreshold() public {
+        BaseVaultTest.Balances memory balancesBefore = getBalances(lp);
+
+        // Calculate the expected amount out (amount out without fees)
+        uint256 poolInvariant = StableMath.computeInvariant(
+            AMP_FACTOR * StableMath.AMP_PRECISION,
+            balancesBefore.poolTokens
+        );
+        uint256 expectedAmountOut = StableMath.computeOutGivenExactIn(
+            AMP_FACTOR * StableMath.AMP_PRECISION,
+            balancesBefore.poolTokens,
+            daiIdx,
+            usdcIdx,
+            amountInBelowThreshold,
+            poolInvariant
+        );
+
+        // Swap with amount that should keep within threshold
+        vm.prank(bob);
+        router.swapSingleTokenExactIn(pool, dai, usdc, amountInBelowThreshold, 0, MAX_UINT256, false, bytes(""));
+
+        BaseVaultTest.Balances memory balancesAfter = getBalances(lp);
+
+        // Measure the actual amount out, which should be `expectedAmountOut` - `swapFeeAmount`.
+        uint256 actualAmountOut = balancesAfter.bobTokens[usdcIdx] - balancesBefore.bobTokens[usdcIdx];
+        uint256 swapFeeAmount = expectedAmountOut - actualAmountOut;
+
+        // Check whether the calculated swap fee percentage equals the static fee percentage. It should,
+        // since the pool was taken closer to equilibrium.
+        assertEq(swapFeeAmount, expectedAmountOut.mulUp(SWAP_FEE_PERCENTAGE), "Swap Fee Amount is wrong");
+
+        // Check Bob's balances (Bob deposited DAI to receive USDC)
+        assertEq(
+            balancesBefore.bobTokens[daiIdx] - balancesAfter.bobTokens[daiIdx],
+            amountInBelowThreshold,
+            "Bob DAI balance is wrong"
+        );
+        assertEq(
+            balancesAfter.bobTokens[usdcIdx] - balancesBefore.bobTokens[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Bob USDC balance is wrong"
+        );
+
+        // Check pool balances (pool received DAI and returned USDC)
+        assertEq(
+            balancesAfter.poolTokens[daiIdx] - balancesBefore.poolTokens[daiIdx],
+            amountInBelowThreshold,
+            "Pool DAI balance is wrong"
+        );
+        // Since the protocol swap fee is 0 (was not set in this test), all swap fee amounts are returned to the pool.
+        assertEq(
+            balancesBefore.poolTokens[usdcIdx] - balancesAfter.poolTokens[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Pool USDC balance is wrong"
+        );
+
+        // Check Vault balances (must reflect pool)
+        assertEq(
+            balancesAfter.vaultTokens[daiIdx] - balancesBefore.vaultTokens[daiIdx],
+            amountInBelowThreshold,
+            "Vault DAI balance is wrong"
+        );
+        assertEq(
+            balancesBefore.vaultTokens[usdcIdx] - balancesAfter.vaultTokens[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Vault USDC balance is wrong"
+        );
+
+        // Check Vault reserves (must reflect Vault balances)
+        assertEq(
+            balancesAfter.vaultReserves[daiIdx] - balancesBefore.vaultReserves[daiIdx],
+            amountInBelowThreshold,
+            "Vault DAI reserve is wrong"
+        );
+        assertEq(
+            balancesBefore.vaultReserves[usdcIdx] - balancesAfter.vaultReserves[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Vault USDC reserve is wrong"
+        );
+    }
+
+    function testSwapAboveThreshold() public {
+        BaseVaultTest.Balances memory balancesBefore = getBalances(lp);
+
+        // Calculate the expected amount out (amount out without fees)
+        uint256 poolInvariant = StableMath.computeInvariant(
+            AMP_FACTOR * StableMath.AMP_PRECISION,
+            balancesBefore.poolTokens
+        );
+        uint256 expectedAmountOut = StableMath.computeOutGivenExactIn(
+            AMP_FACTOR * StableMath.AMP_PRECISION,
+            balancesBefore.poolTokens,
+            daiIdx,
+            usdcIdx,
+            amountInAboveThreshold,
+            poolInvariant
+        );
+
+        // Swap with amount that should push above threshold
+        vm.prank(bob);
+        router.swapSingleTokenExactIn(pool, dai, usdc, amountInAboveThreshold, 0, MAX_UINT256, false, bytes(""));
+
+        BaseVaultTest.Balances memory balancesAfter = getBalances(lp);
+
+        // Measure the actual amount out, which should be `expectedAmountOut` - `swapFeeAmount`.
+        uint256 actualAmountOut = balancesAfter.bobTokens[usdcIdx] - balancesBefore.bobTokens[usdcIdx];
+        uint256 swapFeeAmount = expectedAmountOut - actualAmountOut;
+
+        // Check whether the calculated swap fee percentage equals the surge fee.
+        assertEq(swapFeeAmount, expectedAmountOut.mulUp(expectedSurgeFee), "Swap Fee Amount is wrong");
+
+        // Check Bob's balances (Bob deposited DAI to receive USDC)
+        assertEq(
+            balancesBefore.bobTokens[daiIdx] - balancesAfter.bobTokens[daiIdx],
+            amountInAboveThreshold,
+            "Bob DAI balance is wrong"
+        );
+        assertEq(
+            balancesAfter.bobTokens[usdcIdx] - balancesBefore.bobTokens[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Bob USDC balance is wrong"
+        );
+
+        // Check pool balances (pool received DAI and returned USDC)
+        assertEq(
+            balancesAfter.poolTokens[daiIdx] - balancesBefore.poolTokens[daiIdx],
+            amountInAboveThreshold,
+            "Pool DAI balance is wrong"
+        );
+        // Since the protocol swap fee is 0 (was not set in this test), all swap fee amounts are returned to the pool.
+        assertEq(
+            balancesBefore.poolTokens[usdcIdx] - balancesAfter.poolTokens[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Pool USDC balance is wrong"
+        );
+
+        // Check Vault balances (must reflect pool)
+        assertEq(
+            balancesAfter.vaultTokens[daiIdx] - balancesBefore.vaultTokens[daiIdx],
+            amountInAboveThreshold,
+            "Vault DAI balance is wrong"
+        );
+        assertEq(
+            balancesBefore.vaultTokens[usdcIdx] - balancesAfter.vaultTokens[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Vault USDC balance is wrong"
+        );
+
+        // Check Vault reserves (must reflect Vault balances)
+        assertEq(
+            balancesAfter.vaultReserves[daiIdx] - balancesBefore.vaultReserves[daiIdx],
+            amountInAboveThreshold,
+            "Vault DAI reserve is wrong"
+        );
+        assertEq(
+            balancesBefore.vaultReserves[usdcIdx] - balancesAfter.vaultReserves[usdcIdx],
+            expectedAmountOut - swapFeeAmount,
+            "Vault USDC reserve is wrong"
+        );
+    }
+}
