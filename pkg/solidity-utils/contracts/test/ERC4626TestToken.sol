53df8bed261df9203e20c7f1a829d8c03cf010ee
João Bruno <joaobrunoah@gmail.com>
2024-08-30 15:30:52 -0300

    Fix build

--- pkg/solidity-utils/contracts/test/ERC4626TestToken.sol
+++ pkg/solidity-utils/contracts/test/ERC4626TestToken.sol
@@ -79,10 +79,10 @@ contract ERC4626TestToken is ERC4626, IRateProvider {
         // If rate is lower than one, inflates the total supply. Else, inflates the underlying amount.
         if (newRate < FixedPoint.ONE) {
             uint256 newTotalWrappedAmount = totalAssets.divDown(newRate);
-            uint256 wrappedDelta = newTotalWrappedAmount - totalSupply;
+            wrappedDelta = newTotalWrappedAmount - totalSupply;
         } else {
             uint256 newTotalUnderlyingAmount = totalSupply.mulDown(newRate);
-            uint256 underlyingDelta = newTotalUnderlyingAmount - totalAssets;
+            underlyingDelta = newTotalUnderlyingAmount - totalAssets;
         }
 
         ERC4626TestToken(address(this)).inflateUnderlyingOrWrapped(underlyingDelta, wrappedDelta);
