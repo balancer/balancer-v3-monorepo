09c43546d352673d4228d3ac768fa321fa21c083
João Bruno <joaobrunoah@gmail.com>
2024-09-25 17:34:48 -0300

    Fix contract version

--- pkg/vault/contracts/CompositeLiquidityRouter.sol
+++ pkg/vault/contracts/CompositeLiquidityRouter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IPermit2 } from "permit2/src/interfaces/IPermit2.sol";
