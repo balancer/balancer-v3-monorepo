801be0cb3978bacb4e55a8af43afc41387d92aac
João Bruno <joaobrunoah@gmail.com>
2024-10-16 15:06:49 -0300

    Create EclpPoolDeployer, to reuse the same pool creation for multiple tests

---
+++ pkg/pool-gyro/test/foundry/E2eSwapRateProviderECLP.t.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/IRateProvider.sol";
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+
+import { CastingHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/CastingHelpers.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+
+import { PoolHooksMock } from "@balancer-labs/v3-vault/contracts/test/PoolHooksMock.sol";
+import { ProtocolFeeControllerMock } from "@balancer-labs/v3-vault/contracts/test/ProtocolFeeControllerMock.sol";
+import { RateProviderMock } from "@balancer-labs/v3-vault/contracts/test/RateProviderMock.sol";
+import { E2eSwapRateProviderTest } from "@balancer-labs/v3-vault/test/foundry/E2eSwapRateProvider.t.sol";
+import { VaultContractsDeployer } from "@balancer-labs/v3-vault/test/foundry/utils/VaultContractsDeployer.sol";
+
+import { EclpPoolDeployer } from "./utils/EclpPoolDeployer.sol";
+
+contract E2eSwapRateProviderECLPTest is VaultContractsDeployer, E2eSwapRateProviderTest, EclpPoolDeployer {
+    using CastingHelpers for address[];
+    using FixedPoint for uint256;
+
+    /// @notice Overrides BaseVaultTest _createPool(). This pool is used by E2eSwapTest tests.
+    function _createPool(address[] memory tokens, string memory label) internal override returns (address) {
+        rateProviderTokenA = deployRateProviderMock();
+        rateProviderTokenB = deployRateProviderMock();
+        // Mock rates, so all tests that keep the rate constant use a rate different than 1.
+        rateProviderTokenA.mockRate(5.2453235e18);
+        rateProviderTokenB.mockRate(0.4362784e18);
+
+        IRateProvider[] memory rateProviders = new IRateProvider[](2);
+        rateProviders[tokenAIdx] = IRateProvider(address(rateProviderTokenA));
+        rateProviders[tokenBIdx] = IRateProvider(address(rateProviderTokenB));
+
+        return createEclpPool(tokens, rateProviders, label, vault, lp);
+    }
+
+    function calculateMinAndMaxSwapAmounts() internal virtual override {
+        uint256 rateTokenA = getRate(tokenA);
+        uint256 rateTokenB = getRate(tokenB);
+
+        // The vault does not allow trade amounts (amountGivenScaled18 or amountCalculatedScaled18) to be less than
+        // PRODUCTION_MIN_TRADE_AMOUNT. For "linear pools" (PoolMock), amountGivenScaled18 and amountCalculatedScaled18
+        // are the same. So, minAmountGivenScaled18 > PRODUCTION_MIN_TRADE_AMOUNT. To derive the formula below, note
+        // that `amountGivenRaw = amountGivenScaled18/(rateToken * scalingFactor)`. There's an adjustment for stable
+        // math in the following steps.
+        uint256 tokenAMinTradeAmount = PRODUCTION_MIN_TRADE_AMOUNT.divUp(rateTokenA).mulUp(10 ** decimalsTokenA);
+        uint256 tokenBMinTradeAmount = PRODUCTION_MIN_TRADE_AMOUNT.divUp(rateTokenB).mulUp(10 ** decimalsTokenB);
+
+        // Also, since we undo the operation (reverse swap with the output of the first swap), amountCalculatedRaw
+        // cannot be 0. Considering that amountCalculated is tokenB, and amountGiven is tokenA:
+        // 1) amountCalculatedRaw > 0
+        // 2) amountCalculatedRaw = amountCalculatedScaled18 * 10^(decimalsB) / (rateB * 10^18)
+        // 3) amountCalculatedScaled18 = amountGivenScaled18 // Linear math, there's a factor to stable math
+        // 4) amountGivenScaled18 = amountGivenRaw * rateA * 10^18 / 10^(decimalsA)
+        // Combining the four formulas above, we determine that:
+        // amountCalculatedRaw > rateB * 10^(decimalsA) / (rateA * 10^(decimalsB))
+        uint256 tokenACalculatedNotZero = (rateTokenB * (10 ** decimalsTokenA)) / (rateTokenA * (10 ** decimalsTokenB));
+        uint256 tokenBCalculatedNotZero = (rateTokenA * (10 ** decimalsTokenB)) / (rateTokenB * (10 ** decimalsTokenA));
+
+        // Use the larger of the two values above to calculate the minSwapAmount. Also, multiply by 100 to account for
+        // swap fees and compensate for rate and math rounding issues.
+        uint256 mathFactor = 100;
+        minSwapAmountTokenA = (
+            tokenAMinTradeAmount > tokenACalculatedNotZero
+                ? mathFactor * tokenAMinTradeAmount
+                : mathFactor * tokenACalculatedNotZero
+        );
+        minSwapAmountTokenB = (
+            tokenBMinTradeAmount > tokenBCalculatedNotZero
+                ? mathFactor * tokenBMinTradeAmount
+                : mathFactor * tokenBCalculatedNotZero
+        );
+
+        // 50% of pool init amount to make sure LP has enough tokens to pay for the swap in case of EXACT_OUT.
+        maxSwapAmountTokenA = poolInitAmountTokenA.mulDown(50e16);
+        maxSwapAmountTokenB = poolInitAmountTokenB.mulDown(50e16);
+    }
+}
