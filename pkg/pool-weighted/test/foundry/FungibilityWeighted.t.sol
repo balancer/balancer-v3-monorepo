9154b864d8962d0f2c444227c34492968d9a07ee
João Bruno <joaobrunoah@gmail.com>
2024-10-16 21:07:36 -0300

    Test fungibility of existing pool invariant calculations

---
+++ pkg/pool-weighted/test/foundry/FungibilityWeighted.t.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+
+import { ArrayHelpers } from "@balancer-labs/v3-solidity-utils/contracts/test/ArrayHelpers.sol";
+import { CastingHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/CastingHelpers.sol";
+
+import { PoolHooksMock } from "@balancer-labs/v3-vault/contracts/test/PoolHooksMock.sol";
+import { ProtocolFeeControllerMock } from "@balancer-labs/v3-vault/contracts/test/ProtocolFeeControllerMock.sol";
+import { FungibilityTest } from "@balancer-labs/v3-vault/test/foundry/Fungibility.t.sol";
+
+import { WeightedPoolFactory } from "../../contracts/WeightedPoolFactory.sol";
+import { WeightedPool } from "../../contracts/WeightedPool.sol";
+import { WeightedPoolContractsDeployer } from "./utils/WeightedPoolContractsDeployer.sol";
+
+contract FungibilityWeightedTest is WeightedPoolContractsDeployer, FungibilityTest {
+    using ArrayHelpers for *;
+    using CastingHelpers for address[];
+
+    uint256 internal constant DEFAULT_SWAP_FEE = 1e16; // 1%
+
+    uint256 internal poolCreationNonce;
+
+    function setUp() public override {
+        super.setUp();
+
+        vault.manuallySetSwapFee(pool, 0);
+    }
+
+    /// @notice Overrides BaseVaultTest _createPool(). This pool is used by FungibilityTest.
+    function _createPool(address[] memory tokens, string memory label) internal override returns (address) {
+        WeightedPoolFactory factory = deployWeightedPoolFactory(
+            IVault(address(vault)),
+            365 days,
+            "Factory v1",
+            "Pool v1"
+        );
+        PoolRoleAccounts memory roleAccounts;
+
+        // Allow pools created by `factory` to use poolHooksMock hooks.
+        PoolHooksMock(poolHooksContract).allowFactory(address(factory));
+
+        WeightedPool newPool = WeightedPool(
+            factory.create(
+                "50/50 Weighted Pool",
+                "50_50WP",
+                vault.buildTokenConfig(tokens.asIERC20()),
+                [uint256(50e16), uint256(50e16)].toMemoryArray(),
+                roleAccounts,
+                DEFAULT_SWAP_FEE, // 1% swap fee, but test will override it
+                poolHooksContract,
+                false, // Do not enable donations
+                false, // Do not disable unbalanced add/remove liquidity
+                // NOTE: sends a unique salt.
+                bytes32(poolCreationNonce++)
+            )
+        );
+        vm.label(address(newPool), label);
+
+        // Cannot set the pool creator directly on a standard Balancer weighted pool factory.
+        vault.manualSetPoolCreator(address(newPool), lp);
+
+        ProtocolFeeControllerMock feeController = ProtocolFeeControllerMock(address(vault.getProtocolFeeController()));
+        feeController.manualSetPoolCreator(address(newPool), lp);
+
+        return address(newPool);
+    }
+}
---
+++ pkg/vault/test/foundry/Fungibility.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+
+import { BalancerPoolToken } from "../../contracts/BalancerPoolToken.sol";
+import { BaseVaultTest } from "./utils/BaseVaultTest.sol";
+
+contract FungibilityTest is BaseVaultTest {
+    using FixedPoint for uint256;
+
+    function testFungibilityAddUnbalanced__Fuzz(uint256 proportion) public {
+        proportion = bound(proportion, 1e12, 2e18);
+
+        (, , uint256[] memory balancesRaw, ) = vault.getPoolTokenInfo(pool);
+
+        uint256[] memory exactAmountsIn = new uint256[](balancesRaw.length);
+        for (uint256 i = 0; i < balancesRaw.length; i++) {
+            exactAmountsIn[i] = balancesRaw[i].mulDown(proportion);
+        }
+
+        uint256 totalSupplyBefore = BalancerPoolToken(pool).totalSupply();
+
+        vm.prank(lp);
+        uint256 bptAmountOut = router.addLiquidityUnbalanced(pool, exactAmountsIn, 0, false, bytes(""));
+
+        // Ensure issued BPT is close 0.0001% from what it ideally should be.
+        assertApproxEqRel(totalSupplyBefore.mulDown(proportion), bptAmountOut, 1e12, "BPT out is wrong");
+    }
+}
