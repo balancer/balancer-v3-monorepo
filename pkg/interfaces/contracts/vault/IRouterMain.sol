696966c8bf0c5e2a5269e5f9547ff6a2470d037c
João Bruno <joaobrunoah@gmail.com>
2024-10-22 19:03:13 -0300

    Clear IRouterMain imports

--- pkg/interfaces/contracts/vault/IRouterMain.sol
+++ pkg/interfaces/contracts/vault/IRouterMain.sol
@@ -5,10 +5,7 @@ pragma solidity ^0.8.24;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
-import { AddLiquidityKind, RemoveLiquidityKind, SwapKind } from "./VaultTypes.sol";
-
 import { IRouterCommon } from "./IRouterCommon.sol";
-import { IRouterExtension } from "./IRouterExtension.sol";
 
 /// @notice User-friendly interface to basic Vault operations: swap, add/remove liquidity, and associated queries.
 interface IRouterMain {
