041fb980fa718efdfb9821cd19b7e752b77edfee
Juan Ignacio Ubeira <juani@balancer.finance>
2024-11-22 09:11:57 -0300

    Add more generic test.

--- pkg/solidity-utils/test/foundry/ScalingHelpers.t.sol
+++ pkg/solidity-utils/test/foundry/ScalingHelpers.t.sol
@@ -73,6 +73,64 @@ contract ScalingHelpersTest is Test {
         assertEq(toLengthBefore, to.length, "To length was modified");
     }
 
+    function testCopyToArray__Fuzz(uint256[8] memory input, uint256 length) public pure {
+        length = bound(length, 2, 8);
+        uint256[] memory from = new uint256[](length);
+        for (uint256 i = 0; i < length; ++i) {
+            from[i] = input[i];
+        }
+
+        uint256[] memory to = new uint256[](length);
+
+        uint256 freeMemoryPointerBefore;
+        assembly ("memory-safe") {
+            freeMemoryPointerBefore := mload(0x40)
+        }
+
+        // Array length is 4; past array is 5.
+        uint256 memoryPastArrayBefore;
+        assembly ("memory-safe") {
+            memoryPastArrayBefore := mload(add(to, mul(add(length, 1), 0x20)))
+        }
+
+        uint256 memoryPreArrayBefore;
+        assembly ("memory-safe") {
+            memoryPreArrayBefore := mload(sub(to, 0x20))
+        }
+
+        uint256 fromLengthBefore = from.length;
+        uint256 toLengthBefore = to.length;
+
+        ScalingHelpers.copyToArray(from, to);
+
+        uint256 freeMemoryPointerAfter;
+        assembly ("memory-safe") {
+            freeMemoryPointerAfter := mload(0x40)
+        }
+
+        uint256 memoryPastArrayAfter;
+        assembly ("memory-safe") {
+            memoryPastArrayAfter := mload(add(to, mul(add(length, 1), 0x20)))
+        }
+
+        uint256 memoryPreArrayAfter;
+        assembly ("memory-safe") {
+            memoryPreArrayAfter := mload(sub(to, 0x20))
+        }
+
+        for (uint256 i = 0; i < length; ++i) {
+            assertEq(from[i], input[i], "from");
+            assertEq(from[i], to[i], "to");
+        }
+
+        // Memory array and memory past to are ok
+        assertEq(freeMemoryPointerBefore, freeMemoryPointerAfter, "Free memory pointer was modified");
+        assertEq(memoryPastArrayBefore, memoryPastArrayAfter, "Memory past array was modified");
+        assertEq(memoryPreArrayBefore, memoryPreArrayAfter, "Memory before array was modified");
+        assertEq(fromLengthBefore, from.length, "From length was modified");
+        assertEq(toLengthBefore, to.length, "To length was modified");
+    }
+
     function testCopyToArrayLengthMismatch() public {
         uint256[] memory from = new uint256[](4);
         uint256[] memory to = new uint256[](3);
