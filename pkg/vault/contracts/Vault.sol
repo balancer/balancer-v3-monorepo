5d669e11e0f04dce99342e3c807d1ad04fe81ba4
EndymionJkb <EndymionJkb@gmail.com>
2024-11-26 12:59:55 -0500

    Document roundtrip fee hazards (#1135)

--- pkg/vault/contracts/Vault.sol
+++ pkg/vault/contracts/Vault.sol
@@ -882,7 +882,12 @@ contract Vault is IVaultMain, VaultCommon, Proxy {
                 bptAmountIn
             );
 
-            // Charge roundtrip fee.
+            // Charge roundtrip fee if liquidity was added to this pool in the same transaction; this is not really a
+            // valid use case, and may be an attack. Use caution when removing liquidity through a Safe or other
+            // multisig / non-EOA address. Use "sign and execute," ideally through a private node (or at least not
+            // allowing public execution) to avoid front-running, and always set strict limits so that it will revert
+            // if any unexpected fees are charged. (It is also possible to check whether the flag has been set before
+            // withdrawing, by calling `getAddLiquidityCalledFlag`.)
             if (_addLiquidityCalled().tGet(params.pool)) {
                 uint256 swapFeePercentage = poolData.poolConfigBits.getStaticSwapFeePercentage();
                 for (uint256 i = 0; i < locals.numTokens; ++i) {
