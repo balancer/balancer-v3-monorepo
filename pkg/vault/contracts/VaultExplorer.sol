4ead410fe130952592f70fb4edfcd9bdb71ff4bc
EndymionJkb <EndymionJkb@gmail.com>
2024-12-02 10:52:41 -0500

    Reorganize imports (#1150)

--- pkg/vault/contracts/VaultExplorer.sol
+++ pkg/vault/contracts/VaultExplorer.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.24;
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IVaultExplorer } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultExplorer.sol";
 import { IVaultExtension } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultExtension.sol";
+import { IVaultExplorer } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultExplorer.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 import {
     TokenInfo,
