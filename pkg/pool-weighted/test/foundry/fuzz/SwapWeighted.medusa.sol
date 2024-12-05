50603e2c008423954bc6287d04b7264c68dc952c
elshan_eth <raiman.elshan@gmail.com>
2024-12-05 13:11:05 +0100

    add tests for stable and weighted pools

--- pkg/pool-weighted/test/foundry/fuzz/SwapWeighted.medusa.sol
+++ pkg/pool-weighted/test/foundry/fuzz/SwapWeighted.medusa.sol
@@ -9,12 +9,14 @@ import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 
 import { SwapMedusaTest } from "@balancer-labs/v3-vault/test/foundry/fuzz/Swap.medusa.sol";
 
-import { StablePoolFactory } from "../../../contracts/StablePoolFactory.sol";
-import { StablePool } from "../../../contracts/StablePool.sol";
+import { WeightedPoolFactory } from "../../../contracts/WeightedPoolFactory.sol";
+import { WeightedPool } from "../../../contracts/WeightedPool.sol";
 
-contract SwapStableMedusaTest is SwapMedusaTest {
+contract SwapWeightedMedusaTest is SwapMedusaTest {
     uint256 private constant DEFAULT_SWAP_FEE = 1e16;
-    uint256 internal constant _AMPLIFICATION_PARAMETER = 1000;
+
+    uint256 private constant _WEIGHT1 = 33e16;
+    uint256 private constant _WEIGHT2 = 33e16;
 
     constructor() SwapMedusaTest() {}
 
@@ -22,15 +24,26 @@ contract SwapStableMedusaTest is SwapMedusaTest {
         IERC20[] memory tokens,
         uint256[] memory initialBalances
     ) internal override returns (address newPool) {
-        StablePoolFactory factory = new StablePoolFactory(IVault(address(vault)), 365 days, "Factory v1", "Pool v1");
+        uint256[] memory weights = new uint256[](3);
+        weights[0] = _WEIGHT1;
+        weights[1] = _WEIGHT2;
+        // Sum of weights should equal 100%.
+        weights[2] = 100e16 - (weights[0] + weights[1]);
+
+        WeightedPoolFactory factory = new WeightedPoolFactory(
+            IVault(address(vault)),
+            365 days,
+            "Factory v1",
+            "Pool v1"
+        );
         PoolRoleAccounts memory roleAccounts;
 
-        StablePool newPool = StablePool(
+        WeightedPool newPool = WeightedPool(
             factory.create(
-                "Stable Pool",
-                "STABLE",
+                "Weighted Pool",
+                "WP",
                 vault.buildTokenConfig(tokens),
-                _AMPLIFICATION_PARAMETER,
+                weights,
                 roleAccounts,
                 DEFAULT_SWAP_FEE, // Swap fee is set to 0 in the test constructor
                 address(0), // No hooks
@@ -41,7 +54,12 @@ contract SwapStableMedusaTest is SwapMedusaTest {
             )
         );
 
-        // Initialize liquidity of stable pool.
+        // Cannot set the pool creator directly on a standard Balancer weighted pool factory.
+        vault.manualSetPoolCreator(address(newPool), lp);
+
+        feeController.manualSetPoolCreator(address(newPool), lp);
+
+        // Initialize liquidity of weighted pool.
         medusa.prank(lp);
         router.initialize(address(newPool), tokens, initialBalances, 0, false, bytes(""));
 
