f8c03bc79df79120467830b33351afad8ec7cb6d
João Bruno <joaobrunoah@gmail.com>
2024-12-10 16:34:13 -0300

    Redeploy CI

--- pkg/vault/test/foundry/fork/ERC4626WrapperBase.t.sol
+++ pkg/vault/test/foundry/fork/ERC4626WrapperBase.t.sol
@@ -18,7 +18,7 @@ abstract contract ERC4626WrapperBaseTest is BaseVaultTest {
     using SafeERC20 for IERC20;
 
     // Variables to be defined by setUpForkTestVariables().
-    string internal network;
+    string internal network;a
     uint256 internal blockNumber;
     IERC4626 internal wrapper;
     address internal underlyingDonor;
