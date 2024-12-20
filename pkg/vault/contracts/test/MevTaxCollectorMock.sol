0f6d7df806095872ab4fc36b174ec10643a3044a
João Bruno <joaobrunoah@gmail.com>
2024-12-20 15:44:52 -0300

    Fix lint

--- pkg/vault/contracts/test/MevTaxCollectorMock.sol
+++ pkg/vault/contracts/test/MevTaxCollectorMock.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import { IMevTaxCollector } from "@balancer-labs/v3-interfaces/contracts/vault/IMevTaxCollector.sol";
 
 contract MevTaxCollectorMock is IMevTaxCollector {
-    function chargeMevTax(address pool) external payable {
+    function chargeMevTax(address) external payable {
         return;
     }
 }
