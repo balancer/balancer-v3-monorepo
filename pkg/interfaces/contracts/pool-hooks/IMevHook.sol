22ca8956dd389824aaadf3b19f2b5a88e94b3ef6
João Bruno <joaobrunoah@gmail.com>
2025-01-07 21:37:24 -0300

    Mev Hook

---
+++ pkg/interfaces/contracts/pool-hooks/IMevHook.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+interface IMevHook {
+    /**
+     * @notice Check if the Mev Tax is enabled in the hook.
+     * @dev If Mev Tax is disabled, all swaps will pay the static swap fee amount.
+     * @return mevTaxEnabled true if the MEV Tax is enabled to be charged by the hook, false otherwise
+     */
+    function isMevTaxEnabled() internal view returns (bool mevTaxEnabled);
+
+    /// @notice Permissioned function to disable Mev Tax to be charged in the hook.
+    function disableMevTax() internal;
+
+    /// @notice Permissioned function to enable Mev Tax to be charged in the hook.
+    function enableMevTax() internal;
+
+    /**
+     * @notice Fetch the current multiplier of the priority gas price.
+     * @dev The MEV swap fee percentage is calculated as `mevTaxMultiplier * priorityGasPrice`, where priorityGasPrice
+     * is defined as `transactionGasPrice - baseFee`. A higher mevTaxMultiplier will charge a bigger swap fee from
+     * searchers, absorb more priority fee to LPs, but a too high mevTaxMultiplier may charge big swap fees from retail
+     * users, which is not desired.
+     *
+     * @return mevTaxMultiplier The current MEV Tax Multiplier
+     */
+    function getMevTaxMultiplier() internal view returns (uint256 mevTaxMultiplier);
+
+    /**
+     * @notice Permissioned function to set the multiplier of the priority gas price.
+     * @param newMevTaxMultiplier Integer that will be used to multiply by the priority gas price and get the MEV swap
+     * fee percentage
+     */
+    function setMevTaxMultiplier(uint256 newMevTaxMultiplier) internal;
+}
---
+++ pkg/pool-hooks/contracts/MevHook.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IHooks } from "@balancer-labs/v3-interfaces/contracts/vault/IHooks.sol";
+import { IMevHook } from "@balancer-labs/v3-interfaces/contracts/pool-hooks/IMevHook.sol";
+import {
+    HookFlags,
+    LiquidityManagement,
+    PoolSwapParams,
+    TokenConfig
+} from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+
+import { Authentication } from "@balancer-labs/v3-solidity-utils/contracts/helpers/Authentication.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
+
+import { VaultGuard } from "@balancer-labs/v3-vault/contracts/VaultGuard.sol";
+import { BaseHooks } from "@balancer-labs/v3-vault/contracts/BaseHooks.sol";
+
+contract MevHook is BaseHooks, Authentication, VaultGuard, IMevHook {
+    using FixedPoint for uint256;
+
+    uint256 private constant _MEV_MAX_FEE_PERCENTAGE = 1e18;
+
+    bool internal _mevTaxEnabled = false;
+    uint256 internal _mevTaxMultiplier;
+
+    constructor(IVault vault) Authentication(bytes32(uint256(uint160(address(this))))) VaultGuard(vault) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /// @inheritdoc IHooks
+    function onRegister(
+        address,
+        address,
+        TokenConfig[] memory,
+        LiquidityManagement calldata
+    ) public override onlyVault returns (bool) {
+        return true;
+    }
+
+    /// @inheritdoc IHooks
+    function getHookFlags() public pure override returns (HookFlags memory) {
+        HookFlags memory hookFlags;
+        // The MEV Tax is charged as a swap fee. Searcher transactions pay a higher swap fee percentage.
+        hookFlags.shouldCallComputeDynamicSwapFee = true;
+        return hookFlags;
+    }
+
+    /// @inheritdoc IHooks
+    function onComputeDynamicSwapFeePercentage(
+        PoolSwapParams calldata,
+        address,
+        uint256 staticSwapFeePercentage
+    ) public view virtual returns (bool, uint256) {
+        if (_mevTaxEnabled == false) {
+            return staticSwapFeePercentage;
+        }
+
+        // If gasprice is lower than basefee, the transaction is invalid and won't be processed. Gasprice is set
+        // by the transaction sender, is always bigger than basefee and the difference between gasprice and basefee
+        // defines the priority gas price (the part of the gas cost that will be paid to the validator).
+        uint256 priorityGasPrice = tx.gasprice - block.basefee;
+        uint256 mevSwapFeePercentage = priorityGasPrice.mulDown(_mevTaxMultiplier);
+
+        // If the resulting fee percentage is greater than MAX_FEE_PERCENTAGE, returns the max fee percentage.
+        if (mevSwapFeePercentage >= _MEV_MAX_FEE_PERCENTAGE) {
+            return _MEV_MAX_FEE_PERCENTAGE;
+        }
+
+        // If static fee percentage is higher than mev fee percentage, uses the static one.
+        return staticSwapFeePercentage > mevSwapFeePercentage ? staticSwapFeePercentage : mevSwapFeePercentage;
+    }
+
+    /// @inheritdoc IMevHook
+    function isMevTaxEnabled() internal view returns (bool) {
+        return _mevTaxEnabled;
+    }
+
+    /// @inheritdoc IMevHook
+    function disableMevTax() internal authenticate {
+        _mevTaxEnabled = false;
+    }
+
+    /// @inheritdoc IMevHook
+    function enableMevTax() internal authenticate {
+        _mevTaxEnabled = true;
+    }
+
+    /// @inheritdoc IMevHook
+    function getMevTaxMultiplier() internal view returns (uint256) {
+        return _mevTaxMultiplier;
+    }
+
+    /// @inheritdoc IMevHook
+    function setMevTaxMultiplier(uint256 newMevTaxMultiplier) internal authenticate {
+        _mevTaxMultiplier = newMevTaxMultiplier;
+    }
+}
