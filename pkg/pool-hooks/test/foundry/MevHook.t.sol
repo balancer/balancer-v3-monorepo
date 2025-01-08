c769f168c895e56b9dba8ef8f43d587b46f79a1a
João Bruno <joaobrunoah@gmail.com>
2025-01-08 19:35:25 -0300

    Testing pool multipliers

--- pkg/pool-hooks/test/foundry/MevHook.t.sol
+++ pkg/pool-hooks/test/foundry/MevHook.t.sol
@@ -175,4 +175,100 @@ contract MevHookTest is BaseVaultTest {
             "defaultMevTaxThreshold is not correct"
         );
     }
+
+    /********************************************************
+                   getPoolMevTaxMultiplier()
+    ********************************************************/
+    function testGetPoolMevTaxMultiplierPoolNotRegistered() public {
+        // Creates a new mevHook and stores into _mevHook, so the pool won't be registered with the new MevHook.
+        createHook();
+
+        vm.expectRevert(abi.encodeWithSelector(IMevHook.MevHookNotRegisteredInPool.selector, pool));
+        _mevHook.getPoolMevTaxMultiplier(pool);
+    }
+
+    /********************************************************
+                   setPoolMevTaxMultiplier()
+    ********************************************************/
+    function testSetPoolMevTaxMultiplierPoolNotRegistered() public {
+        // Creates a new mevHook and stores into _mevHook, so the pool won't be registered with the new MevHook.
+        createHook();
+
+        vm.expectRevert(abi.encodeWithSelector(IMevHook.MevHookNotRegisteredInPool.selector, pool));
+        _mevHook.setPoolMevTaxMultiplier(pool, 5e18);
+    }
+
+    function testSetPoolMevTaxMultiplierIsPermissioned() public {
+        vm.expectRevert(IAuthentication.SenderNotAllowed.selector);
+        _mevHook.setPoolMevTaxMultiplier(pool, 5e18);
+    }
+
+    function testSetPoolMevTaxMultiplier() public {
+        uint256 firstPoolMevTaxMultiplier = _mevHook.getPoolMevTaxMultiplier(pool);
+        uint256 firstDefaultMevTaxMultiplier = _mevHook.getDefaultMevTaxMultiplier();
+
+        uint256 newPoolMevTaxMultiplier = 5e18;
+
+        assertNotEq(
+            firstPoolMevTaxMultiplier,
+            newPoolMevTaxMultiplier,
+            "New defaultMevTaxMultiplier cannot be equal to current value"
+        );
+
+        vm.prank(admin);
+        _mevHook.setPoolMevTaxMultiplier(pool, newPoolMevTaxMultiplier);
+        assertEq(
+            _mevHook.getPoolMevTaxMultiplier(pool),
+            newPoolMevTaxMultiplier,
+            "poolMevTaxMultiplier is not correct"
+        );
+
+        assertEq(_mevHook.getDefaultMevTaxMultiplier(), firstDefaultMevTaxMultiplier, "Default multiplier changed");
+    }
+
+    /********************************************************
+                   getPoolMevTaxThreshold()
+    ********************************************************/
+    function testGetPoolMevTaxThresholdPoolNotRegistered() public {
+        // Creates a new mevHook and stores into _mevHook, so the pool won't be registered with the new MevHook.
+        createHook();
+
+        vm.expectRevert(abi.encodeWithSelector(IMevHook.MevHookNotRegisteredInPool.selector, pool));
+        _mevHook.getPoolMevTaxThreshold(pool);
+    }
+
+    /********************************************************
+                   setPoolMevTaxThreshold()
+    ********************************************************/
+    function testSetPoolMevTaxThresholdPoolNotRegistered() public {
+        // Creates a new mevHook and stores into _mevHook, so the pool won't be registered with the new MevHook.
+        createHook();
+
+        vm.expectRevert(abi.encodeWithSelector(IMevHook.MevHookNotRegisteredInPool.selector, pool));
+        _mevHook.setPoolMevTaxThreshold(pool, 5e18);
+    }
+
+    function testSetPoolMevTaxThresholdIsPermissioned() public {
+        vm.expectRevert(IAuthentication.SenderNotAllowed.selector);
+        _mevHook.setPoolMevTaxThreshold(pool, 5e18);
+    }
+
+    function testSetPoolMevTaxThreshold() public {
+        uint256 firstPoolMevTaxThreshold = _mevHook.getPoolMevTaxThreshold(pool);
+        uint256 firstDefaultMevTaxThreshold = _mevHook.getDefaultMevTaxThreshold();
+
+        uint256 newPoolMevTaxThreshold = 5e18;
+
+        assertNotEq(
+            firstPoolMevTaxThreshold,
+            newPoolMevTaxThreshold,
+            "New defaultMevTaxThreshold cannot be equal to current value"
+        );
+
+        vm.prank(admin);
+        _mevHook.setPoolMevTaxThreshold(pool, newPoolMevTaxThreshold);
+        assertEq(_mevHook.getPoolMevTaxThreshold(pool), newPoolMevTaxThreshold, "poolMevTaxThreshold is not correct");
+
+        assertEq(_mevHook.getDefaultMevTaxThreshold(), firstDefaultMevTaxThreshold, "Default threshold changed");
+    }
 }
