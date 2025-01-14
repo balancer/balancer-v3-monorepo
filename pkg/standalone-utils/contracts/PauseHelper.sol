ebabee2b4db1863b2f8fff135a633bafa8772cb4
Elshan Dzhafarov <22689890+elshan-eth@users.noreply.github.com>
2025-01-14 21:50:39 +0100

    Update pkg/standalone-utils/contracts/PauseHelper.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/standalone-utils/contracts/PauseHelper.sol
+++ pkg/standalone-utils/contracts/PauseHelper.sol
@@ -56,7 +56,14 @@ contract PauseHelper is SingletonAuthentication {
     }
 
     /**
-     * @notice Pause pools
+     * @notice Pause a set of pools.
+     * @dev This is a permissioned function. Governance must first grant this contract permission to call `pausePool`
+     * on the Vault, then grant another account permission to call `pausePools` here. Note that this is not necessarily
+     * the same account that can add or remove pools from the pausable list.
+     *
+     * Note that there is no `unpause`. This is a helper contract designed to react quickly to emergencies. Unpausing
+     * is a more deliberate action that should be performed by accounts approved by governance for this purpose, or by
+     * the individual pools' pause managers.
      * @param pools List of pools to pause
      */
     function pause(address[] memory pools) public authenticate {
