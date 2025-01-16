3aa06b453c86800b8225195fc3852c42ec951f88
Jeff Bennett <endymionjkb@gmail.com>
2025-01-15 19:40:49 -0500

    lint

--- pkg/interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol
+++ pkg/interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol
@@ -5,5 +5,10 @@ pragma solidity ^0.8.24;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IProtocolFeeBurner {
-    function burn(IERC20 tokenIn, uint256 tokenInAmount, IERC20 tokenOut, address recipient) external returns(uint256 tokenOutAmount);
+    function burn(
+        IERC20 tokenIn,
+        uint256 tokenInAmount,
+        IERC20 tokenOut,
+        address recipient
+    ) external returns (uint256 tokenOutAmount);
 }
