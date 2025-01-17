361eb24391de08519b9cd135d367955ab4f5fb4d
Jeff Bennett <endymionjkb@gmail.com>
2025-01-16 19:20:54 -0500

    refactor: add event to burner (and pool argument for docs), and add a mock

--- pkg/interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol
+++ pkg/interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.24;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IProtocolFeeBurner {
+    event ProtocolFeeBurned(
+        address indexed pool,
+        IERC20 indexed feeToken,
+        uint256 feeTokenAmount,
+        IERC20 indexed targetToken,
+        uint256 targetTokenAmount,
+        address recipient
+    );
+
+    /**
+     * @notice Swap an exact amount of `feeToken` for the `targetToken`, and send proceeds to the `recipient`.
+     * @param pool The pool the fees came from (only used for documentation in the event)
+     * @param feeToken The feeToken collected from the pool
+     * @param feeTokenAmount The number of fee tokens collected
+     * @param targetToken The desired target token (token out of the swap)
+     * @param recipient The recipient of the swap proceeds
+     */
     function burn(
+        address pool,
         IERC20 feeToken,
         uint256 feeTokenAmount,
         IERC20 targetToken,
---
+++ pkg/standalone-utils/contracts/test/ProtocolFeeBurnerMock.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { IProtocolFeeSweeper } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IProtocolFeeSweeper.sol";
+import { IProtocolFeeBurner } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol";
+
+import { ERC20TestToken } from "@balancer-labs/v3-solidity-utils/contracts/test/ERC20TestToken.sol";
+
+contract ProtocolFeeBurnerMock is IProtocolFeeBurner {
+    using SafeERC20 for IERC20;
+
+    IProtocolFeeSweeper private _feeSweeper;
+
+    constructor(IProtocolFeeSweeper feeSweeper) {
+        _feeSweeper = feeSweeper;
+    }
+
+    /// @inheritdoc IProtocolFeeBurner
+    function burn(
+        address pool,
+        IERC20 feeToken,
+        uint256 feeTokenAmount,
+        IERC20 targetToken,
+        address recipient
+    ) external {
+        // Just emit the event, simulating the tokens being exchanged at 1-to-1.
+        feeToken.safeTransferFrom(address(_feeSweeper), address(this), feeTokenAmount);
+        // Simulate the swap by minting the same amount of target to the recipient.
+        ERC20TestToken(address(targetToken)).mint(recipient, feeTokenAmount);
+
+        emit ProtocolFeeBurned(pool, feeToken, feeTokenAmount, targetToken, feeTokenAmount, recipient);
+    }
+}
