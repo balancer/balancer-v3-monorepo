edd03c8fe606bed52f97540cc0736ee9ab8992d9
João Bruno <joaobrunoah@gmail.com>
2025-01-20 14:07:10 -0300

    Fix MevHookMock to use interface of registry

--- pkg/pool-hooks/contracts/test/MevHookMock.sol
+++ pkg/pool-hooks/contracts/test/MevHookMock.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.24;
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
 import { MevHook } from "../MevHook.sol";
-import { BalancerContractRegistry } from "@balancer-labs/v3-vault/contracts/BalancerContractRegistry.sol";
+import { IBalancerContractRegistry } from "@balancer-labs/v3-interfaces/contracts/vault/IBalancerContractRegistry.sol";
 
 contract MevHookMock is MevHook {
-    constructor(IVault vault, BalancerContractRegistry registry) MevHook(vault, registry) {
+    constructor(IVault vault, IBalancerContractRegistry registry) MevHook(vault, registry) {
         // solhint-disable-previous-line no-empty-blocks
     }
 
