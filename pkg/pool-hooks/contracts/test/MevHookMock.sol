64e59bb264276537c65185caf08cdba2a4bbaf3b
Jeff Bennett <endymionjkb@gmail.com>
2025-01-21 17:39:37 -0500

    refactor: update registry reference

--- pkg/pool-hooks/contracts/test/MevHookMock.sol
+++ pkg/pool-hooks/contracts/test/MevHookMock.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
 import { MevHook } from "../MevHook.sol";
-import { IBalancerContractRegistry } from "@balancer-labs/v3-interfaces/contracts/vault/IBalancerContractRegistry.sol";
+import { IBalancerContractRegistry } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IBalancerContractRegistry.sol";
 
 contract MevHookMock is MevHook {
     constructor(IVault vault, IBalancerContractRegistry registry) MevHook(vault, registry) {
