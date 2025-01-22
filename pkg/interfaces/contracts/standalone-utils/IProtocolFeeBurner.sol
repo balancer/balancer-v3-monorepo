86261041305fa3cb1fad5de8469f2ab08f4360a3
Jeff Bennett <endymionjkb@gmail.com>
2025-01-22 11:17:06 -0500

    feat: add limit and deadline to the burner interface

--- pkg/interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol
+++ pkg/interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol
@@ -5,6 +5,15 @@ pragma solidity ^0.8.24;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IProtocolFeeBurner {
+    /**
+     * @notice A protocol fee token has been "burned" (i.e., swapped for the desired target token).
+     * @param pool The pool on which the fee was collected (used for event tracking)
+     * @param feeToken The token in which the fee was originally collected
+     * @param feeTokenAmount The number of feeTokens collected
+     * @param targetToken The preferred token for fee collection (e.g., USDC)
+     * @param targetTokenAmount The number of target tokens actually received
+     * @param recipient The address where the target tokens were sent
+     */
     event ProtocolFeeBurned(
         address indexed pool,
         IERC20 indexed feeToken,
@@ -15,19 +24,34 @@ interface IProtocolFeeBurner {
     );
 
     /**
+     * @notice The actual amount out is below the minimum limit specified for the operation.
+     * @param tokenOut The outgoing token
+     * @param amountOut The total BPT amount out
+     * @param minAmountOut The amount of the limit that has been exceeded
+     */
+    error AmountOutBelowMin(IERC20 tokenOut, uint256 amountOut, uint256 minAmountOut);
+
+    /// @notice The swap transaction was not validated before the specified deadline timestamp.
+    error SwapDeadline();
+
+    /**
      * @notice Swap an exact amount of `feeToken` for the `targetToken`, and send proceeds to the `recipient`.
      * @dev Assumes the sweeper has transferred the tokens to the burner prior to the call.
      * @param pool The pool the fees came from (only used for documentation in the event)
      * @param feeToken The feeToken collected from the pool
      * @param feeTokenAmount The number of fee tokens collected
      * @param targetToken The desired target token (token out of the swap)
+     * @param minTargetTokenAmount The minimum amount out for the swap
      * @param recipient The recipient of the swap proceeds
+     * @param deadline Deadline for the burn operation (i.e., swap), after which it will revert
      */
     function burn(
         address pool,
         IERC20 feeToken,
         uint256 feeTokenAmount,
         IERC20 targetToken,
-        address recipient
+        uint256 minTargetTokenAmount,
+        address recipient,
+        uint256 deadline
     ) external;
 }
--- pkg/standalone-utils/contracts/test/ProtocolFeeBurnerMock.sol
+++ pkg/standalone-utils/contracts/test/ProtocolFeeBurnerMock.sol
@@ -7,20 +7,40 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IProtocolFeeBurner } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IProtocolFeeBurner.sol";
 
 import { ERC20TestToken } from "@balancer-labs/v3-solidity-utils/contracts/test/ERC20TestToken.sol";
+import { FixedPoint } from "@balancer-labs/v3-solidity-utils/contracts/math/FixedPoint.sol";
 
 contract ProtocolFeeBurnerMock is IProtocolFeeBurner {
+    using FixedPoint for uint256;
+
+    uint256 private _tokenRatio = FixedPoint.ONE;
+
     /// @inheritdoc IProtocolFeeBurner
     function burn(
         address pool,
         IERC20 feeToken,
         uint256 feeTokenAmount,
         IERC20 targetToken,
-        address recipient
+        uint256 minTargetTokenAmount,
+        address recipient,
+        uint256 deadline
     ) external {
+        if (block.timestamp > deadline) {
+            revert SwapDeadline();
+        }
+
         // Simulate the swap by minting the same amount of target to the recipient.
         ERC20TestToken(address(targetToken)).mint(recipient, feeTokenAmount);
 
+        uint256 targetTokenAmount = feeTokenAmount.mulDown(_tokenRatio);
+        if (targetTokenAmount < minTargetTokenAmount) {
+            revert AmountOutBelowMin(targetToken, targetTokenAmount, minTargetTokenAmount);
+        }
+
         // Just emit the event, simulating the tokens being exchanged at 1-to-1.
-        emit ProtocolFeeBurned(pool, feeToken, feeTokenAmount, targetToken, feeTokenAmount, recipient);
+        emit ProtocolFeeBurned(pool, feeToken, feeTokenAmount, targetToken, targetTokenAmount, recipient);
+    }
+
+    function setTokenRatio(uint256 ratio) external {
+        _tokenRatio = ratio;
     }
 }
