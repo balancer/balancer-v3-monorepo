d0eb0bd50f362b355521320884c050fa80f71713
João Bruno <joaobrunoah@gmail.com>
2025-02-04 20:42:27 -0300

    Test getters

--- pkg/pool-weighted/test/foundry/LBPool.t.sol
+++ pkg/pool-weighted/test/foundry/LBPool.t.sol
@@ -120,6 +120,19 @@ contract LBPoolTest is BaseLBPTest {
         );
     }
 
+    function testCreatePoolTimeTravel() public {
+        vm.expectRevert(Create2.Create2FailedDeployment.selector);
+        _deployAndInitializeWithCustomWeights(
+            startWeights[projectIdx],
+            startWeights[reserveIdx],
+            endWeights[projectIdx],
+            endWeights[reserveIdx],
+            uint32(block.timestamp + 200),
+            uint32(block.timestamp + 100), // EndTime after StartTime, it should revert.
+            DEFAULT_PROJECT_TOKENS_SWAP_IN
+        );
+    }
+
     function testCreatePoolEvent() public {
         uint32 startTime = uint32(block.timestamp + DEFAULT_START_OFFSET);
         uint32 endTime = uint32(block.timestamp + DEFAULT_END_OFFSET);
@@ -141,8 +154,59 @@ contract LBPoolTest is BaseLBPTest {
                             Getters
     ********************************************************/
 
+    function testGetTrustedRouter() public view {
+        assertEq(LBPool(pool).getTrustedRouter(), address(router), "Wrong trusted router");
+    }
+
     function testGetTrustedFactory() public view {
         assertEq(LBPool(pool).getTrustedFactory(), address(lbPoolFactory), "Wrong trusted factory");
+    }
+
+    function testGradualWeightUpdateParams() public {
+        uint32 customStartTime = uint32(block.timestamp + 1);
+        uint32 customEndTime = uint32(block.timestamp + 300);
+        uint256[] memory customStartWeights = [uint256(22e16), uint256(78e16)].toMemoryArray();
+        uint256[] memory customEndWeights = [uint256(65e16), uint256(35e16)].toMemoryArray();
+
+        (address newPool, ) = _deployAndInitializeWithCustomWeights(
+            customStartWeights[projectIdx],
+            customStartWeights[reserveIdx],
+            customEndWeights[projectIdx],
+            customEndWeights[reserveIdx],
+            customStartTime,
+            customEndTime,
+            DEFAULT_PROJECT_TOKENS_SWAP_IN
+        );
+
+        (
+            uint256 poolStartTime,
+            uint256 poolEndTime,
+            uint256[] memory poolStartWeights,
+            uint256[] memory poolEndWeights
+        ) = LBPool(newPool).getGradualWeightUpdateParams();
+        assertEq(poolStartTime, customStartTime, "Start time mismatch");
+        assertEq(poolEndTime, customEndTime, "End time mismatch");
+
+        assertEq(poolStartWeights.length, customStartWeights.length, "Start Weights length mismatch");
+        assertEq(poolStartWeights[projectIdx], customStartWeights[projectIdx], "Project Start Weight mismatch");
+        assertEq(poolStartWeights[reserveIdx], customStartWeights[reserveIdx], "Reserve Start Weight mismatch");
+
+        assertEq(poolEndWeights.length, customEndWeights.length, "End Weights length mismatch");
+        assertEq(poolEndWeights[projectIdx], customEndWeights[projectIdx], "Project End Weight mismatch");
+        assertEq(poolEndWeights[reserveIdx], customEndWeights[reserveIdx], "Reserve End Weight mismatch");
+    }
+
+    function testIsSwapEnabled() public {
+        uint32 startTime = uint32(block.timestamp + DEFAULT_START_OFFSET);
+        uint32 endTime = uint32(block.timestamp + DEFAULT_END_OFFSET);
+
+        assertFalse(LBPool(pool).isSwapEnabled(), "Swap should be disabled before start time");
+
+        vm.warp(startTime + 1);
+        assertTrue(LBPool(pool).isSwapEnabled(), "Swap should be enabled after start time");
+
+        vm.warp(endTime + 1);
+        assertFalse(LBPool(pool).isSwapEnabled(), "Swap should be disabled after end time");
     }
 
     function testAddingLiquidityNotAllowed() public {
