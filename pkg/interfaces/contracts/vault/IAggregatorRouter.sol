39c08e1503f09e77f8363a70d4d2af9ce2e94db4
Juan Ignacio Ubeira <juani@balancer.finance>
2025-02-19 11:55:27 -0300

    Fix.

--- pkg/interfaces/contracts/vault/IAggregatorRouter.sol
+++ pkg/interfaces/contracts/vault/IAggregatorRouter.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
-
+import { IVault } from "./IVault.sol";
 import { SwapKind } from "./VaultTypes.sol";
 
 interface IAggregatorRouter {
