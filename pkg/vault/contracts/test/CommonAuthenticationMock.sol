40c8c44abc5280c52fe41bdb703339588560489e
Juan Ignacio Ubeira <juani@balancer.finance>
2025-02-21 10:33:35 -0300

    Lint.

--- pkg/vault/contracts/test/CommonAuthenticationMock.sol
+++ pkg/vault/contracts/test/CommonAuthenticationMock.sol
@@ -7,7 +7,6 @@ import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol"
 import { CommonAuthentication } from "../CommonAuthentication.sol";
 
 contract CommonAuthenticationMock is CommonAuthentication {
-
     constructor(IVault vault, bytes32 actionIdDisambiguator) CommonAuthentication(vault, actionIdDisambiguator) {
         // solhint-disable-previous-line no-empty-blocks
     }
