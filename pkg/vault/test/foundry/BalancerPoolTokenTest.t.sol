d0a1f7a373bf88184f6f6c2fa04f30750f3396a9
Jeff Bennett <endymionjkb@gmail.com>
2025-02-25 11:35:22 -0500

    fix: switch this to the match other imports for this file

--- pkg/vault/test/foundry/BalancerPoolTokenTest.t.sol
+++ pkg/vault/test/foundry/BalancerPoolTokenTest.t.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import "forge-std/Test.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import { IERC165 } from "@openzeppelin/contracts/interfaces/IERC165.sol";
 import { ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import { IEIP712 } from "permit2/src/interfaces/IEIP712.sol";
 
