4df25be3bab3f1a0e8247d47dd5aaa3bc95788f2
EndymionJkb <EndymionJkb@gmail.com>
2025-02-27 08:43:46 -0500

    Refactor ERC165 import (#1324)
    Co-authored-by: elshan-eth <22689890+elshan-eth@users.noreply.github.com>

--- pkg/standalone-utils/test/foundry/CowSwapFeeBurner.t.sol
+++ pkg/standalone-utils/test/foundry/CowSwapFeeBurner.t.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.24;
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import { IERC1271 } from "@openzeppelin/contracts/interfaces/IERC1271.sol";
-import { IERC165 } from "@openzeppelin/contracts/interfaces/IERC165.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { IProtocolFeeSweeper } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IProtocolFeeSweeper.sol";
