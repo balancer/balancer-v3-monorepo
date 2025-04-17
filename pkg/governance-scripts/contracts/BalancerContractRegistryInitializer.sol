7f868098a68c5500244133cb2f185e18de2a11f8
Jeff Bennett <endymionjkb@gmail.com>
2025-04-16 20:38:12 -0400

    refactor: version not requiring admin permission.

--- pkg/governance-scripts/contracts/BalancerContractRegistryInitializer.sol
+++ pkg/governance-scripts/contracts/BalancerContractRegistryInitializer.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.24;
 
-import { IAuthentication } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/IAuthentication.sol";
-import { IBasicAuthorizer } from "@balancer-labs/v3-interfaces/contracts/governance-scripts/IBasicAuthorizer.sol";
 import {
     IBalancerContractRegistry,
     ContractType
@@ -13,12 +11,9 @@ import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol"
 import { SingletonAuthentication } from "@balancer-labs/v3-vault/contracts/SingletonAuthentication.sol";
 import { InputHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/InputHelpers.sol";
 
-// Associated with `20250221-balancer-registry-initializer`.
+// Associated with `20250411-balancer-registry-initializer-v2`.
 contract BalancerContractRegistryInitializer {
     IBalancerContractRegistry public immutable balancerContractRegistry;
-
-    // IAuthorizer with interface for granting/revoking roles.
-    IBasicAuthorizer internal immutable _authorizer;
 
     // Set to true when operation is complete.
     bool private _initialized;
@@ -66,10 +61,14 @@ contract BalancerContractRegistryInitializer {
         _poolFactoryAddresses = poolFactoryAddresses;
         _aliasNames = aliasNames;
         _aliasAddresses = aliasAddresses;
-
-        _authorizer = IBasicAuthorizer(address(vault.getAuthorizer()));
     }
 
+    /**
+     * @notice The function that initializes the Balancer contract registry, based on the data supplied on deployment.
+     * @dev This function can only be called once. This contract must be granted permission to call two functions on
+     * the `BalancerContractRegistry` being initialized: `registerBalancerContract` and
+     * `addOrUpdateBalancerContractAlias`.
+     */
     function initializeBalancerContractRegistry() external {
         // Explicitly ensure this can only be called once.
         if (_initialized) {
@@ -77,17 +76,6 @@ contract BalancerContractRegistryInitializer {
         }
 
         _initialized = true;
-
-        // Grant permissions to register contracts and add aliases.
-        bytes32 registerContractRole = IAuthentication(address(balancerContractRegistry)).getActionId(
-            IBalancerContractRegistry.registerBalancerContract.selector
-        );
-        bytes32 addAliasRole = IAuthentication(address(balancerContractRegistry)).getActionId(
-            IBalancerContractRegistry.addOrUpdateBalancerContractAlias.selector
-        );
-
-        _authorizer.grantRole(registerContractRole, address(this));
-        _authorizer.grantRole(addAliasRole, address(this));
 
         // Add Routers.
         for (uint256 i = 0; i < _routerNames.length; ++i) {
@@ -111,11 +99,5 @@ contract BalancerContractRegistryInitializer {
         for (uint256 i = 0; i < _aliasNames.length; ++i) {
             balancerContractRegistry.addOrUpdateBalancerContractAlias(_aliasNames[i], _aliasAddresses[i]);
         }
-
-        // Renounce all roles.
-        _authorizer.renounceRole(registerContractRole, address(this));
-        _authorizer.renounceRole(addAliasRole, address(this));
-
-        _authorizer.renounceRole(_authorizer.DEFAULT_ADMIN_ROLE(), address(this));
     }
 }
