eb74b204461625a4db575495895159731061c288
Jeff Bennett <endymionjkb@gmail.com>
2025-04-24 21:14:03 -0400

    refactor: simplify swap fee helper

--- pkg/interfaces/contracts/standalone-utils/IPoolSwapFeeHelper.sol
+++ pkg/interfaces/contracts/standalone-utils/IPoolSwapFeeHelper.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.24;
 
+import "./IPoolHelperCommon.sol";
+
 /**
  * @notice Maintain a set of pools whose static swap fee percentages can be changed from here, vs. from the Vault.
  * @dev Governance can add a set of pools to this contract, then grant swap fee setting permission to accounts on this
@@ -10,19 +12,7 @@ pragma solidity ^0.8.24;
  * Note that governance must grant this contract permission to set swap fees from the Vault, and only pools that
  * allow governance to set fees can be added (i.e., they must not have swap managers).
  */
-interface IPoolSwapFeeHelper {
-    /**
-     * @notice Cannot add a pool that is already there.
-     * @param pool Address of the pool being added
-     */
-    error PoolAlreadyInSwapFeeSet(address pool);
-
-    /**
-     * @notice Cannot remove a pool that was not added.
-     * @param pool Address of the pool being removed
-     */
-    error PoolNotInSwapFeeSet(address pool);
-
+interface IPoolSwapFeeHelper is IPoolHelperCommon {
     /**
      * @notice Cannot add a pool that has a swap manager.
      * @dev The swap manager is an exclusive role. If it is set to a non-zero value during pool registration,
@@ -33,42 +23,9 @@ interface IPoolSwapFeeHelper {
      */
     error PoolHasSwapManager(address pool);
 
-    /// @notice An index is beyond the current bounds of the set.
-    error IndexOutOfBounds();
-
-    /**
-     * @notice Emitted when a pool is added to the list of pools whose swap fee can be changed.
-     * @param pool Address of the pool that was added
-     */
-    event PoolAddedToSwapFeeSet(address pool);
-
-    /**
-     * @notice Emitted when a pool is removed from the list of pools whose swap fee can be changed.
-     * @param pool Address of the pool that was removed
-     */
-    event PoolRemovedFromSwapFeeSet(address pool);
-
     /***************************************************************************
                                     Manage Pools
     ***************************************************************************/
-
-    /**
-     * @notice Add pools to the list of pools whose swap fee can be changed.
-     * @dev This is a permissioned function. Only authorized accounts (e.g., dynamic fee service providers) may add
-     * pools to the set.
-     *
-     * @param newPools List of pools to add
-     */
-    function addPools(address[] calldata newPools) external;
-
-    /**
-     * @notice Remove pools from the list of pools whose swap fee can be changed.
-     * @dev This is a permissioned function. Only authorized accounts (e.g., dynamic fee service providers) may remove
-     * pools from the set.
-     *
-     * @param pools List of pools to remove
-     */
-    function removePools(address[] memory pools) external;
 
     /**
      * @notice Set the static swap fee percentage on a given pool.
@@ -80,31 +37,4 @@ interface IPoolSwapFeeHelper {
      * @param swapFeePercentage The new swap fee percentage
      */
     function setStaticSwapFeePercentage(address pool, uint256 swapFeePercentage) external;
-
-    /***************************************************************************
-                                    Getters                                
-    ***************************************************************************/
-
-    /**
-     * @notice Get the number of pools.
-     * @dev Needed to support pagination in case the list is too long to process in a single transaction.
-     * @return poolCount The current number of pools whose swap fee can be changed from this contract
-     */
-    function getPoolCount() external view returns (uint256);
-
-    /**
-     * @notice Check whether a pool is in the set of pools whose swap fee can be changed.
-     * @param pool Address of the pool
-     * @return isPausable True if the pool is in the set
-     */
-    function hasPool(address pool) external view returns (bool);
-
-    /**
-     * @notice Get a range of pools.
-     * @dev Indexes are 0-based and [start, end) (i.e., inclusive of `start`; exclusive of `end`).
-     * @param from Start index
-     * @param to End index
-     * @return pools List of pools
-     */
-    function getPools(uint256 from, uint256 to) external view returns (address[] memory pools);
 }
--- pkg/standalone-utils/contracts/PoolSwapFeeHelper.sol
+++ pkg/standalone-utils/contracts/PoolSwapFeeHelper.sol
@@ -5,16 +5,15 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 import { IPoolSwapFeeHelper } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IPoolSwapFeeHelper.sol";
-import { SingletonAuthentication } from "@balancer-labs/v3-vault/contracts/SingletonAuthentication.sol";
 import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
-contract PoolSwapFeeHelper is IPoolSwapFeeHelper, SingletonAuthentication {
+import { PoolHelperCommon } from "./PoolHelperCommon.sol";
+
+contract PoolSwapFeeHelper is IPoolSwapFeeHelper, PoolHelperCommon {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    EnumerableSet.AddressSet private _pools;
-
-    constructor(IVault vault) SingletonAuthentication(vault) {
+    constructor(IVault vault) PoolHelperCommon(vault) {
         // solhint-disable-previous-line no-empty-blocks
     }
 
@@ -23,84 +22,21 @@ contract PoolSwapFeeHelper is IPoolSwapFeeHelper, SingletonAuthentication {
     ***************************************************************************/
 
     /// @inheritdoc IPoolSwapFeeHelper
-    function addPools(address[] calldata newPools) external authenticate {
-        uint256 length = newPools.length;
-        IVault vault = getVault();
-
-        for (uint256 i = 0; i < length; i++) {
-            address pool = newPools[i];
-
-            // Ensure the address is a valid pool.
-            if (vault.isPoolRegistered(pool) == false) {
-                revert IVaultErrors.PoolNotRegistered(pool);
-            }
-
-            // Pools cannot have a swap fee manager.
-            if (vault.getPoolRoleAccounts(pool).swapFeeManager != address(0)) {
-                revert PoolHasSwapManager(pool);
-            }
-
-            if (_pools.add(pool) == false) {
-                revert PoolAlreadyInSwapFeeSet(pool);
-            }
-
-            emit PoolAddedToSwapFeeSet(pool);
-        }
-    }
-
-    /// @inheritdoc IPoolSwapFeeHelper
-    function removePools(address[] memory pools) public authenticate {
-        uint256 length = pools.length;
-        for (uint256 i = 0; i < length; i++) {
-            address pool = pools[i];
-            _ensureKnownPool(pool);
-            _pools.remove(pool);
-
-            emit PoolRemovedFromSwapFeeSet(pool);
-        }
-    }
-
-    /// @inheritdoc IPoolSwapFeeHelper
     function setStaticSwapFeePercentage(address pool, uint256 swapFeePercentage) public authenticate {
-        _ensureKnownPool(pool);
+        _ensurePoolAdded(pool);
 
         getVault().setStaticSwapFeePercentage(pool, swapFeePercentage);
-    }
-
-    /***************************************************************************
-                                    Getters                                
-    ***************************************************************************/
-
-    /// @inheritdoc IPoolSwapFeeHelper
-    function getPoolCount() external view returns (uint256) {
-        return _pools.length();
-    }
-
-    /// @inheritdoc IPoolSwapFeeHelper
-    function hasPool(address pool) external view returns (bool) {
-        return _pools.contains(pool);
-    }
-
-    /// @inheritdoc IPoolSwapFeeHelper
-    function getPools(uint256 from, uint256 to) public view returns (address[] memory pools) {
-        uint256 poolLength = _pools.length();
-        if (from > to || to > poolLength || from >= poolLength) {
-            revert IndexOutOfBounds();
-        }
-
-        pools = new address[](to - from);
-        for (uint256 i = from; i < to; i++) {
-            pools[i - from] = _pools.at(i);
-        }
     }
 
     /***************************************************************************
                                 Internal functions                                
     ***************************************************************************/
 
-    function _ensureKnownPool(address pool) internal view {
-        if (_pools.contains(pool) == false) {
-            revert PoolNotInSwapFeeSet(pool);
+    /// @inheritdoc PoolHelperCommon
+    function _validatePool(address pool) internal view override {
+        // Pools cannot have a swap fee manager.
+        if (getVault().getPoolRoleAccounts(pool).swapFeeManager != address(0)) {
+            revert PoolHasSwapManager(pool);
         }
     }
 }
