f7586be70bdaad31d18929ea8aa26dd0020eea2a
Jeff Bennett <endymionjkb@gmail.com>
2025-04-24 21:13:40 -0400

    refactor: simplify protocol fee helper

--- pkg/interfaces/contracts/standalone-utils/IProtocolFeeHelper.sol
+++ pkg/interfaces/contracts/standalone-utils/IProtocolFeeHelper.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.24;
 
+import "./IPoolHelperCommon.sol";
+
 /**
  * @notice Maintain a set of pools whose protocol fees can be set from this helper contract, vs. the fee controller.
  * @dev Governance can add a set of pools to this contract, then grant permission to call protocol swap- or yield-
@@ -10,53 +12,10 @@ pragma solidity ^0.8.24;
  * Note that governance must grant this contract permission to call the pool protocol fee setting functions on the
  * controller.
  */
-interface IProtocolFeeHelper {
-    /**
-     * @notice Cannot add a pool that is already there.
-     * @param pool Address of the pool being added
-     */
-    error PoolAlreadyInProtocolFeeSet(address pool);
-
-    /**
-     * @notice Cannot remove a pool that was not added.
-     * @param pool Address of the pool being removed
-     */
-    error PoolNotInProtocolFeeSet(address pool);
-
-    /// @notice An index is beyond the current bounds of the set.
-    error IndexOutOfBounds();
-
-    /**
-     * @notice Emitted when a pool is added to the list of pools whose protocol fees can be set.
-     * @param pool Address of the pool that was added
-     */
-    event PoolAddedToProtocolFeeSet(address pool);
-
-    /**
-     * @notice Emitted when a pool is removed from the list of pools whose protocol fees can be set.
-     * @param pool Address of the pool that was removed
-     */
-    event PoolRemovedFromProtocolFeeSet(address pool);
-
+interface IProtocolFeeHelper is IPoolHelperCommon {
     /***************************************************************************
                                     Manage Pools
     ***************************************************************************/
-
-    /**
-     * @notice Add pools to the list of pools whose protocol fees can be set.
-     * @dev This is a permissioned function. Only authorized accounts may add pools to the set.
-     *
-     * @param newPools List of pools to add
-     */
-    function addPools(address[] calldata newPools) external;
-
-    /**
-     * @notice Remove pools from the list of pools whose protocol fees can be set.
-     * @dev This is a permissioned function. Only authorized accounts may remove pools from the set.
-     *
-     * @param pools List of pools to remove
-     */
-    function removePools(address[] memory pools) external;
 
     /**
      * @notice Set the protocol swap fee for a pool.
@@ -77,31 +36,4 @@ interface IProtocolFeeHelper {
      * @param newProtocolYieldFeePercentage The new protocol yield fee percentage
      */
     function setProtocolYieldFeePercentage(address pool, uint256 newProtocolYieldFeePercentage) external;
-
-    /***************************************************************************
-                                    Getters                                
-    ***************************************************************************/
-
-    /**
-     * @notice Get the number of pools.
-     * @dev Needed to support pagination in case the list is too long to process in a single transaction.
-     * @return poolCount The current number of pools in the set
-     */
-    function getPoolCount() external view returns (uint256);
-
-    /**
-     * @notice Check whether a pool is in the set of pools.
-     * @param pool Pool to check
-     * @return isProtocolFee True if the pool is in the list, false otherwise
-     */
-    function hasPool(address pool) external view returns (bool);
-
-    /**
-     * @notice Get a range of pools.
-     * @dev Indexes are 0-based and [start, end) (i.e., inclusive of `start`; exclusive of `end`).
-     * @param from Start index
-     * @param to End index
-     * @return pools List of pools
-     */
-    function getPools(uint256 from, uint256 to) external view returns (address[] memory pools);
 }
--- pkg/standalone-utils/contracts/ProtocolFeeHelper.sol
+++ pkg/standalone-utils/contracts/ProtocolFeeHelper.sol
@@ -9,59 +9,23 @@ import { IProtocolFeeController } from "@balancer-labs/v3-interfaces/contracts/v
 import { IVaultErrors } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultErrors.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
-import { SingletonAuthentication } from "@balancer-labs/v3-vault/contracts/SingletonAuthentication.sol";
+import { PoolHelperCommon } from "./PoolHelperCommon.sol";
 
-contract ProtocolFeeHelper is IProtocolFeeHelper, SingletonAuthentication {
+contract ProtocolFeeHelper is IProtocolFeeHelper, PoolHelperCommon {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    EnumerableSet.AddressSet private _pools;
-
     modifier withKnownPool(address pool) {
-        _ensureKnownPool(pool);
+        _ensurePoolAdded(pool);
         _;
     }
 
-    constructor(IVault vault) SingletonAuthentication(vault) {
+    constructor(IVault vault) PoolHelperCommon(vault) {
         // solhint-disable-previous-line no-empty-blocks
     }
 
     /***************************************************************************
                                     Manage Pools
     ***************************************************************************/
-
-    /// @inheritdoc IProtocolFeeHelper
-    function addPools(address[] calldata newPools) external authenticate {
-        uint256 length = newPools.length;
-        IVault vault = getVault();
-
-        for (uint256 i = 0; i < length; i++) {
-            address pool = newPools[i];
-
-            // Ensure the address is a valid pool.
-            if (vault.isPoolRegistered(pool) == false) {
-                revert IVaultErrors.PoolNotRegistered(pool);
-            }
-
-            if (_pools.add(pool) == false) {
-                revert PoolAlreadyInProtocolFeeSet(pool);
-            }
-
-            emit PoolAddedToProtocolFeeSet(pool);
-        }
-    }
-
-    /// @inheritdoc IProtocolFeeHelper
-    function removePools(address[] memory pools) public authenticate {
-        uint256 length = pools.length;
-        for (uint256 i = 0; i < length; i++) {
-            address pool = pools[i];
-            _ensureKnownPool(pool);
-
-            _pools.remove(pool);
-
-            emit PoolRemovedFromProtocolFeeSet(pool);
-        }
-    }
 
     /// @inheritdoc IProtocolFeeHelper
     function setProtocolSwapFeePercentage(
@@ -80,44 +44,11 @@ contract ProtocolFeeHelper is IProtocolFeeHelper, SingletonAuthentication {
     }
 
     /***************************************************************************
-                                    Getters                                
-    ***************************************************************************/
-
-    /// @inheritdoc IProtocolFeeHelper
-    function getPoolCount() external view returns (uint256) {
-        return _pools.length();
-    }
-
-    /// @inheritdoc IProtocolFeeHelper
-    function hasPool(address pool) external view returns (bool) {
-        return _pools.contains(pool);
-    }
-
-    /// @inheritdoc IProtocolFeeHelper
-    function getPools(uint256 from, uint256 to) public view returns (address[] memory pools) {
-        uint256 poolLength = _pools.length();
-        if (from > to || to > poolLength || from >= poolLength) {
-            revert IndexOutOfBounds();
-        }
-
-        pools = new address[](to - from);
-        for (uint256 i = from; i < to; i++) {
-            pools[i - from] = _pools.at(i);
-        }
-    }
-
-    /***************************************************************************
                                 Internal functions                                
     ***************************************************************************/
 
     // The protocol fee controller is upgradeable in the Vault, so we must fetch it every time.
     function _getProtocolFeeController() internal view returns (IProtocolFeeController) {
         return getVault().getProtocolFeeController();
-    }
-
-    function _ensureKnownPool(address pool) internal view {
-        if (_pools.contains(pool) == false) {
-            revert PoolNotInProtocolFeeSet(pool);
-        }
     }
 }
