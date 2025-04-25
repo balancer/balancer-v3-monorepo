36addc7b8e1cf05de18f4b9efec247d5749a6a0e
Jeff Bennett <endymionjkb@gmail.com>
2025-04-24 21:13:56 -0400

    refactor: simplify pool pause helper

--- pkg/standalone-utils/contracts/PoolPauseHelper.sol
+++ pkg/standalone-utils/contracts/PoolPauseHelper.sol
@@ -8,14 +8,12 @@ import { IPoolPauseHelper } from "@balancer-labs/v3-interfaces/contracts/standal
 import { IVaultAdmin } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultAdmin.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 
-import { SingletonAuthentication } from "@balancer-labs/v3-vault/contracts/SingletonAuthentication.sol";
+import { PoolHelperCommon } from "./PoolHelperCommon.sol";
 
-contract PoolPauseHelper is IPoolPauseHelper, SingletonAuthentication {
+contract PoolPauseHelper is IPoolPauseHelper, PoolHelperCommon {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    EnumerableSet.AddressSet private _pausablePools;
-
-    constructor(IVault vault) SingletonAuthentication(vault) {
+    constructor(IVault vault) PoolHelperCommon(vault) {
         // solhint-disable-previous-line no-empty-blocks
     }
 
@@ -24,69 +22,15 @@ contract PoolPauseHelper is IPoolPauseHelper, SingletonAuthentication {
     ***************************************************************************/
 
     /// @inheritdoc IPoolPauseHelper
-    function addPools(address[] calldata newPools) external authenticate {
-        uint256 length = newPools.length;
-
-        for (uint256 i = 0; i < length; i++) {
-            address pool = newPools[i];
-            if (_pausablePools.add(pool) == false) {
-                revert PoolAlreadyInPausableSet(pool);
-            }
-
-            emit PoolAddedToPausableSet(pool);
-        }
-    }
-
-    /// @inheritdoc IPoolPauseHelper
-    function removePools(address[] memory pools) public authenticate {
-        uint256 length = pools.length;
-        for (uint256 i = 0; i < length; i++) {
-            address pool = pools[i];
-            if (_pausablePools.remove(pool) == false) {
-                revert PoolNotInPausableSet(pool);
-            }
-
-            emit PoolRemovedFromPausableSet(pool);
-        }
-    }
-
-    /// @inheritdoc IPoolPauseHelper
     function pausePools(address[] memory pools) public authenticate {
         uint256 length = pools.length;
         for (uint256 i = 0; i < length; i++) {
             address pool = pools[i];
-            if (_pausablePools.contains(pool) == false) {
-                revert PoolNotInPausableSet(pool);
+            if (_pools.contains(pool) == false) {
+                revert PoolNotInSet(pool);
             }
 
             getVault().pausePool(pool);
-        }
-    }
-
-    /***************************************************************************
-                                    Getters                                
-    ***************************************************************************/
-
-    /// @inheritdoc IPoolPauseHelper
-    function getPoolCount() external view returns (uint256) {
-        return _pausablePools.length();
-    }
-
-    /// @inheritdoc IPoolPauseHelper
-    function hasPool(address pool) external view returns (bool) {
-        return _pausablePools.contains(pool);
-    }
-
-    /// @inheritdoc IPoolPauseHelper
-    function getPools(uint256 from, uint256 to) public view returns (address[] memory pools) {
-        uint256 poolLength = _pausablePools.length();
-        if (from > to || to > poolLength || from >= poolLength) {
-            revert IndexOutOfBounds();
-        }
-
-        pools = new address[](to - from);
-        for (uint256 i = from; i < to; i++) {
-            pools[i - from] = _pausablePools.at(i);
         }
     }
 }
