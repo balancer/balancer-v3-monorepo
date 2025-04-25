ab48d59c4a922327a127a2959470a8161409559d
EndymionJkb <EndymionJkb@gmail.com>
2025-04-25 17:09:15 -0400

    Factor common code out of helpers (#1362)

--- pkg/interfaces/contracts/standalone-utils/IPoolSwapFeeHelper.sol
+++ pkg/interfaces/contracts/standalone-utils/IPoolSwapFeeHelper.sol
@@ -12,18 +12,6 @@ pragma solidity ^0.8.24;
  */
 interface IPoolSwapFeeHelper {
     /**
-     * @notice Cannot add a pool that is already there.
-     * @param pool Address of the pool being added
-     */
-    error PoolAlreadyInSwapFeeSet(address pool);
-
-    /**
-     * @notice Cannot remove a pool that was not added.
-     * @param pool Address of the pool being removed
-     */
-    error PoolNotInSwapFeeSet(address pool);
-
-    /**
      * @notice Cannot add a pool that has a swap manager.
      * @dev The swap manager is an exclusive role. If it is set to a non-zero value during pool registration,
      * only the swap manager can change the fee. This helper can only set fees on pools that allow governance
@@ -33,42 +21,9 @@ interface IPoolSwapFeeHelper {
      */
     error PoolHasSwapManager(address pool);
 
-    /// @notice An index is beyond the current bounds of the set.
-    error IndexOutOfBounds();
-
-    /**
-     * @notice Emitted when a pool is added to the list of pools whose swap fee can be changed.
-     * @param pool Address of the pool that was added
-     */
-    event PoolAddedToSwapFeeSet(address pool);
-
-    /**
-     * @notice Emitted when a pool is removed from the list of pools whose swap fee can be changed.
-     * @param pool Address of the pool that was removed
-     */
-    event PoolRemovedFromSwapFeeSet(address pool);
-
     /***************************************************************************
                                     Manage Pools
     ***************************************************************************/
-
-    /**
-     * @notice Add pools to the list of pools whose swap fee can be changed.
-     * @dev This is a permissioned function. Only authorized accounts (e.g., dynamic fee service providers) may add
-     * pools to the set.
-     *
-     * @param newPools List of pools to add
-     */
-    function addPools(address[] calldata newPools) external;
-
-    /**
-     * @notice Remove pools from the list of pools whose swap fee can be changed.
-     * @dev This is a permissioned function. Only authorized accounts (e.g., dynamic fee service providers) may remove
-     * pools from the set.
-     *
-     * @param pools List of pools to remove
-     */
-    function removePools(address[] memory pools) external;
 
     /**
      * @notice Set the static swap fee percentage on a given pool.
@@ -80,31 +35,4 @@ interface IPoolSwapFeeHelper {
      * @param swapFeePercentage The new swap fee percentage
      */
     function setStaticSwapFeePercentage(address pool, uint256 swapFeePercentage) external;
-
-    /***************************************************************************
-                                    Getters                                
-    ***************************************************************************/
-
-    /**
-     * @notice Get the number of pools.
-     * @dev Needed to support pagination in case the list is too long to process in a single transaction.
-     * @return poolCount The current number of pools whose swap fee can be changed from this contract
-     */
-    function getPoolCount() external view returns (uint256);
-
-    /**
-     * @notice Check whether a pool is in the set of pools whose swap fee can be changed.
-     * @param pool Address of the pool
-     * @return isPausable True if the pool is in the set
-     */
-    function hasPool(address pool) external view returns (bool);
-
-    /**
-     * @notice Get a range of pools.
-     * @dev Indexes are 0-based and [start, end) (i.e., inclusive of `start`; exclusive of `end`).
-     * @param from Start index
-     * @param to End index
-     * @return pools List of pools
-     */
-    function getPools(uint256 from, uint256 to) external view returns (address[] memory pools);
 }
