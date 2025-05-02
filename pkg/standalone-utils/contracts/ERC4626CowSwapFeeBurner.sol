410826fd5666bf4cc064339348fea2fa0e6e767e
Juan Ignacio Ubeira <juani@balancer.finance>
2025-05-02 20:35:18 -0300

    Add wrapper cow burner.

---
+++ pkg/standalone-utils/contracts/ERC4626CowSwapFeeBurner.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.24;
+
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { IComposableCow } from "@balancer-labs/v3-interfaces/contracts/standalone-utils/IComposableCow.sol";
+import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+
+import {
+    ReentrancyGuardTransient
+} from "@balancer-labs/v3-solidity-utils/contracts/openzeppelin/ReentrancyGuardTransient.sol";
+
+import { CowSwapFeeBurner } from "./CowSwapFeeBurner.sol";
+
+/**
+ * @title ERC4626CowSwapFeeBurner
+ * @notice A contract that burns ERC4626 protocol fee tokens using CowSwap, previously redeeming underlying assets.
+ * @dev The Cow Watchtower (https://github.com/cowprotocol/watch-tower) must be running for the burner to function.
+ * Only one order per token is allowed at a time.
+ */
+contract ERC4626CowSwapFeeBurner is CowSwapFeeBurner {
+    using SafeERC20 for IERC20;
+
+    constructor(
+        IVault vault,
+        IComposableCow composableCow,
+        address vaultRelayer,
+        bytes32 appData,
+        string memory version
+    ) CowSwapFeeBurner(vault, composableCow, vaultRelayer, appData, version) {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /**
+     * @notice Treats `feeToken` as an ERC4626, redeems `exactFeeTokenAmountIn`, swaps the underlying asset for
+     * `targetToken`, and send proceeds to the `recipient`.
+     * @dev Assumes the sweeper has granted allowance for the fee tokens to the burner prior to the call.
+     * @param pool The pool the fees came from (only used for documentation in the event)
+     * @param feeToken The token collected from the pool
+     * @param exactFeeTokenAmountIn The number of fee tokens collected
+     * @param targetToken The desired target token (token out of the swap)
+     * @param minTargetTokenAmountOut The minimum amount out for the swap
+     * @param recipient The recipient of the swap proceeds
+     * @param deadline Deadline for the burn operation (i.e., swap), after which it will revert
+     */
+    function burn(
+        address pool,
+        IERC20 feeToken,
+        uint256 exactFeeTokenAmountIn,
+        IERC20 targetToken,
+        uint256 minTargetTokenAmountOut,
+        address recipient,
+        uint256 deadline
+    ) external authenticate nonReentrant override {
+        // In this case we first pull the wrapped token, unwrap, and then proceed to burn by creating an order for
+        // the underlying token.
+        feeToken.safeTransferFrom(msg.sender, address(this), exactFeeTokenAmountIn);
+
+        // Redeem and overwrite inputs with new asset and unwrapped amount.
+        IERC4626 erc4626Token = IERC4626(address(feeToken));
+        feeToken = IERC20(erc4626Token.asset());
+        exactFeeTokenAmountIn = erc4626Token.redeem(exactFeeTokenAmountIn, address(this), address(this));
+
+        // This case is not handled by the internal `_burn` function, but it's valid: we can consider that the token
+        // has already been converted to the correct token, so we just forward the result.
+        if (feeToken == targetToken) {
+            // We apply the slippage check, but not deadline as the order settlement is instant in this case.
+            if (exactFeeTokenAmountIn < minTargetTokenAmountOut) {
+                revert AmountOutBelowMin(targetToken, exactFeeTokenAmountIn, minTargetTokenAmountOut);
+            }
+            feeToken.safeTransfer(recipient, exactFeeTokenAmountIn);
+        }
+
+        _burn(
+            pool,
+            feeToken,
+            exactFeeTokenAmountIn,
+            targetToken,
+            minTargetTokenAmountOut,
+            recipient,
+            deadline,
+            false // pullToken
+        );
+    }
+}
