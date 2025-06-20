c53c04a5b53665259cd95d0b735b260deaeafa7a
elshan-eth <22689890+elshan-eth@users.noreply.github.com>
2025-06-20 23:33:11 +0200

    fix lint issue

--- pkg/interfaces/contracts/vault/IRouter.sol
+++ pkg/interfaces/contracts/vault/IRouter.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.24;
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
+import { IVault } from "./IVault.sol";
 
 import { AddLiquidityKind, RemoveLiquidityKind, SwapKind } from "./VaultTypes.sol";
 
