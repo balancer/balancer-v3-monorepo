080153b047bba1858f467db8e87163bbffd3b14c
Juan Ignacio Ubeira <juani@balancer.finance>
2025-06-25 13:00:24 -0300

    Remove unused import.

--- pkg/pool-weighted/test/foundry/LBPMigrationRouter.t.sol
+++ pkg/pool-weighted/test/foundry/LBPMigrationRouter.t.sol
@@ -2,8 +2,6 @@
 
 pragma solidity ^0.8.24;
 
-import "forge-std/Test.sol";
-
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
