0d1aa8cae28e917cab983fd6f97be11282945289
João Bruno <joaobrunoah@gmail.com>
2025-07-02 11:09:16 -0300

    Fix interface of oracle base

--- pkg/interfaces/contracts/standalone-utils/ILPOracleBase.sol
+++ pkg/interfaces/contracts/standalone-utils/ILPOracleBase.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.24;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ILPOracleBase {
     error UnsupportedDecimals();
@@ -11,7 +12,7 @@ interface ILPOracleBase {
      * @notice Calculates the tvl based on the provided prices.
      * @dev Prices are defined the same way as in the oracle feed, and scaled to 18-decimal FP values.
      * @param prices An array of prices for the tokens in the pool
-     * @return tvl TVL calculated from the prices and pool arguments
+     * @return tvl TVL calculated from the prices and current weights
      */
     function calculateTVL(int256[] memory prices) external view returns (uint256 tvl);
 
@@ -24,13 +25,19 @@ interface ILPOracleBase {
 
     /**
      * @notice Gets the list of feeds used by the oracle.
-     * @return An array of AggregatorV3Interface instances representing the feeds.
+     * @return An array of AggregatorV3Interface instances representing the feeds
      */
     function getFeeds() external view returns (AggregatorV3Interface[] memory);
 
     /**
      * @notice Gets the decimal scaling factors for each feed token.
-     * @return An array of scaling factors corresponding to each feed.
+     * @return An array of scaling factors corresponding to each feed
      */
     function getFeedTokenDecimalScalingFactors() external view returns (uint256[] memory);
+
+    /**
+     * @notice Gets the tokens in the pool.
+     * @return An array of token addresses, sorted in token registration order
+     */
+    function getPoolTokens() external view returns (IERC20[] memory);
 }
