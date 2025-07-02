0e6aa760e9a349e501ac34b499154a156202414d
João Bruno <joaobrunoah@gmail.com>
2025-07-02 11:14:02 -0300

    Fix Weighted Oracle interface

--- pkg/interfaces/contracts/standalone-utils/IWeightedLPOracle.sol
+++ pkg/interfaces/contracts/standalone-utils/IWeightedLPOracle.sol
@@ -8,10 +8,4 @@ interface IWeightedLPOracle {
      * @return An array of weights corresponding to each token in the pool
      */
     function getWeights() external view returns (uint256[] memory);
-
-    /**
-     * @notice Gets the tokens in the pool.
-     * @return An array of token addresses, sorted in token registration order
-     */
-    function getPoolTokens() external view returns (IERC20[] memory);
 }
