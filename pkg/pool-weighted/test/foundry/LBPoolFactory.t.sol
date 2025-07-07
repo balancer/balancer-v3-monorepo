f74437e779948a583e786e412328122bfad04261
EndymionJkb <EndymionJkb@gmail.com>
2025-07-07 08:33:23 -0400

    Minimum migration weight fixes (#1419)

--- pkg/pool-weighted/test/foundry/LBPoolFactory.t.sol
+++ pkg/pool-weighted/test/foundry/LBPoolFactory.t.sol
@@ -146,6 +146,8 @@ contract LBPoolFactoryTest is BaseLBPTest {
     function testCreatePoolWithInvalidMigrationWeights() public {
         uint256 initBptLockDuration = 30 days;
         uint256 initBptPercentageToMigrate = 50e16; // 50%
+        uint256 minReserveTokenWeight = 20e16; // 20%
+        uint256 maxProjectTokenWeight = 100e16 - minReserveTokenWeight;
 
         vm.expectRevert(LBPoolFactory.InvalidMigrationWeights.selector);
         _createLBPoolWithMigration(address(0), initBptLockDuration, initBptPercentageToMigrate, 0, 100e16);
@@ -154,7 +156,13 @@ contract LBPoolFactoryTest is BaseLBPTest {
         _createLBPoolWithMigration(address(0), initBptLockDuration, initBptPercentageToMigrate, 100e16, 0);
 
         vm.expectRevert(LBPoolFactory.InvalidMigrationWeights.selector);
-        _createLBPoolWithMigration(address(0), initBptLockDuration, initBptPercentageToMigrate, 80e16 + 1, 20e16 - 1);
+        _createLBPoolWithMigration(
+            address(0),
+            initBptLockDuration,
+            initBptPercentageToMigrate,
+            maxProjectTokenWeight + 1,
+            minReserveTokenWeight - 1
+        );
 
         vm.expectRevert(LBPoolFactory.InvalidMigrationWeights.selector);
         _createLBPoolWithMigration(address(0), initBptLockDuration, initBptPercentageToMigrate, 100e16, 100e16);
