e1ec8bcacf4ea41fa07b00e5fa0c3712e6d94e67
Jeff Bennett <endymionjkb@gmail.com>
2025-07-09 12:12:13 -0400

    refactor: remove unused imports and refactor to allow SwapKind

--- pkg/vault/contracts/AddUnbalancedExtensionRouter.sol
+++ pkg/vault/contracts/AddUnbalancedExtensionRouter.sol
@@ -2,29 +2,27 @@
 
 pragma solidity ^0.8.24;
 
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IPermit2 } from "permit2/src/interfaces/IPermit2.sol";
 
-import { IBatchRouter } from "@balancer-labs/v3-interfaces/contracts/vault/IBatchRouter.sol";
 import { IWETH } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/misc/IWETH.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
-import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
-
-import { EVMCallModeHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/EVMCallModeHelpers.sol";
-import { CastingHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/CastingHelpers.sol";
-import { InputHelpers } from "@balancer-labs/v3-solidity-utils/contracts/helpers/InputHelpers.sol";
 import {
-    TransientEnumerableSet
-} from "@balancer-labs/v3-solidity-utils/contracts/openzeppelin/TransientEnumerableSet.sol";
-import {
-    TransientStorageHelpers
-} from "@balancer-labs/v3-solidity-utils/contracts/helpers/TransientStorageHelpers.sol";
+    SwapKind,
+    AddLiquidityKind,
+    RemoveLiquidityKind
+} from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 
 import { Router } from "./Router.sol";
-import { BatchRouterCommon } from "./BatchRouterCommon.sol";
 
+/**
+ * @notice Enable adding and removing liquidity unbalanced on pools that do not support it natively.
+ * @dev This extends the standard `Router` in order to call shared internal hook implementation functions.
+ * It factors out the unbalanced adds into two operations: a proportional add and a swap, executes them using
+ * the standard router, then checks the limits.
+ * 
+ * It does not support operations on native ETH.
+ */
 contract AddUnbalancedExtensionRouter is Router {
     struct AddLiquidityProportionalParams {
         uint256[] maxAmountsIn;
@@ -32,19 +30,12 @@ contract AddUnbalancedExtensionRouter is Router {
         bytes userData;
     }
 
-    struct SwapExactInParams {
+    struct SwapParams {
         IERC20 tokenIn;
         IERC20 tokenOut;
-        uint256 exactAmountIn;
-        uint256 minAmountOut;
-        bytes userData;
-    }
-
-    struct SwapExactOutParams {
-        IERC20 tokenIn;
-        IERC20 tokenOut;
-        uint256 exactAmountOut;
-        uint256 maxAmountIn;
+        SwapKind kind;
+        uint256 amountGiven;
+        uint256 limit;
         bytes userData;
     }
 
@@ -61,18 +52,18 @@ contract AddUnbalancedExtensionRouter is Router {
         // solhint-disable-previous-line no-empty-blocks
     }
 
-    function addProportionalAndSwapExactIn(
+    function addProportionalAndSwap(
         address pool,
         uint256 deadline,
         AddLiquidityProportionalParams calldata addLiquidityParams,
-        SwapExactInParams calldata swapParams
+        SwapParams calldata swapParams
     )
         external
         saveSender(msg.sender)
         returns (
             uint256[] memory addLiquidityAmountsIn,
             uint256 addLiquidityBptAmountOut,
-            uint256 swapAmountOut,
+            uint256 swapAmountCalculated,
             bytes memory addLiquidityReturnData
         )
     {
@@ -93,12 +84,12 @@ contract AddUnbalancedExtensionRouter is Router {
                             }),
                             swapParams: SwapSingleTokenHookParams({
                                 sender: msg.sender,
-                                kind: SwapKind.EXACT_IN,
+                                kind: swapParams.kind,
                                 pool: pool,
                                 tokenIn: swapParams.tokenIn,
                                 tokenOut: swapParams.tokenOut,
-                                amountGiven: swapParams.exactAmountIn,
-                                limit: swapParams.minAmountOut,
+                                amountGiven: swapParams.amountGiven,
+                                limit: swapParams.limit,
                                 deadline: deadline,
                                 wethIsEth: false,
                                 userData: swapParams.userData
@@ -110,67 +101,18 @@ contract AddUnbalancedExtensionRouter is Router {
             );
     }
 
-    function addProportionalAndSwapExactOut(
-        address pool,
-        uint256 deadline,
-        AddLiquidityProportionalParams calldata addLiquidityParams,
-        SwapExactOutParams calldata swapParams
-    )
-        external
-        saveSender(msg.sender)
-        returns (
-            uint256[] memory addLiquidityAmountsIn,
-            uint256 addLiquidityBptAmountOut,
-            uint256 swapAmountIn,
-            bytes memory addLiquidityReturnData
-        )
-    {
-        return
-            abi.decode(
-                _vault.unlock(
-                    abi.encodeCall(
-                        AddUnbalancedExtensionRouter.addProportionalAndSwapHook,
-                        AddLiquidityAndSwapHookParams({
-                            addLiquidityParams: AddLiquidityHookParams({
-                                sender: msg.sender,
-                                pool: pool,
-                                maxAmountsIn: addLiquidityParams.maxAmountsIn,
-                                minBptAmountOut: addLiquidityParams.exactBptAmountOut,
-                                kind: AddLiquidityKind.PROPORTIONAL,
-                                wethIsEth: false,
-                                userData: addLiquidityParams.userData
-                            }),
-                            swapParams: SwapSingleTokenHookParams({
-                                sender: msg.sender,
-                                kind: SwapKind.EXACT_OUT,
-                                pool: pool,
-                                tokenIn: swapParams.tokenIn,
-                                tokenOut: swapParams.tokenOut,
-                                amountGiven: swapParams.exactAmountOut,
-                                limit: swapParams.maxAmountIn,
-                                deadline: deadline,
-                                wethIsEth: false,
-                                userData: swapParams.userData
-                            })
-                        })
-                    )
-                ),
-                (uint256[], uint256, uint256, bytes)
-            );
-    }
-
-    function queryAddProportionalAndSwapExactIn(
+    function queryAddProportionalAndSwap(
         address pool,
         address sender,
         AddLiquidityProportionalParams calldata addLiquidityParams,
-        SwapExactInParams calldata swapParams
+        SwapParams calldata swapParams
     )
         external
         saveSender(sender)
         returns (
             uint256[] memory addLiquidityAmountsIn,
             uint256 addLiquidityBptAmountOut,
-            uint256 swapAmountOut,
+            uint256 swapAmountCalculated,
             bytes memory addLiquidityReturnData
         )
     {
@@ -191,61 +133,12 @@ contract AddUnbalancedExtensionRouter is Router {
                             }),
                             swapParams: SwapSingleTokenHookParams({
                                 sender: address(this),
-                                kind: SwapKind.EXACT_IN,
+                                kind: swapParams.kind,
                                 pool: pool,
                                 tokenIn: swapParams.tokenIn,
                                 tokenOut: swapParams.tokenOut,
-                                amountGiven: swapParams.exactAmountIn,
-                                limit: swapParams.minAmountOut,
-                                deadline: _MAX_AMOUNT,
-                                wethIsEth: false,
-                                userData: swapParams.userData
-                            })
-                        })
-                    )
-                ),
-                (uint256[], uint256, uint256, bytes)
-            );
-    }
-
-    function queryAddProportionalAndSwapExactOut(
-        address pool,
-        address sender,
-        AddLiquidityProportionalParams calldata addLiquidityParams,
-        SwapExactOutParams calldata swapParams
-    )
-        external
-        saveSender(sender)
-        returns (
-            uint256[] memory addLiquidityAmountsIn,
-            uint256 addLiquidityBptAmountOut,
-            uint256 swapAmountIn,
-            bytes memory addLiquidityReturnData
-        )
-    {
-        return
-            abi.decode(
-                _vault.quote(
-                    abi.encodeCall(
-                        AddUnbalancedExtensionRouter.queryAddProportionalAndSwapHook,
-                        AddLiquidityAndSwapHookParams({
-                            addLiquidityParams: AddLiquidityHookParams({
-                                sender: address(this),
-                                pool: pool,
-                                maxAmountsIn: addLiquidityParams.maxAmountsIn,
-                                minBptAmountOut: addLiquidityParams.exactBptAmountOut,
-                                kind: AddLiquidityKind.PROPORTIONAL,
-                                wethIsEth: false,
-                                userData: addLiquidityParams.userData
-                            }),
-                            swapParams: SwapSingleTokenHookParams({
-                                sender: address(this),
-                                kind: SwapKind.EXACT_OUT,
-                                pool: pool,
-                                tokenIn: swapParams.tokenIn,
-                                tokenOut: swapParams.tokenOut,
-                                amountGiven: swapParams.exactAmountOut,
-                                limit: swapParams.maxAmountIn,
+                                amountGiven: swapParams.amountGiven,
+                                limit: swapParams.limit,
                                 deadline: _MAX_AMOUNT,
                                 wethIsEth: false,
                                 userData: swapParams.userData
