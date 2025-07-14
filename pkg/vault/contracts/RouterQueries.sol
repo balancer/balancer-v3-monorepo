6c89ceb74b442da3dfb442712a9bba89c85c1c15
Elshan Dzhafarov <22689890+elshan-eth@users.noreply.github.com>
2025-07-14 16:41:48 +0200

    Update pkg/vault/contracts/RouterQueries.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/vault/contracts/RouterQueries.sol
+++ pkg/vault/contracts/RouterQueries.sol
@@ -2,19 +2,14 @@
 
 pragma solidity ^0.8.24;
 
-import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
-import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { IPermit2 } from "permit2/src/interfaces/IPermit2.sol";
 
 import { IRouterQueries } from "@balancer-labs/v3-interfaces/contracts/vault/IRouterQueries.sol";
 import { IWETH } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/misc/IWETH.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
-import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 import "@balancer-labs/v3-interfaces/contracts/vault/RouterTypes.sol";
 
-import { RouterWethLib } from "./lib/RouterWethLib.sol";
 import { RouterHooks } from "./RouterHooks.sol";
 
 /**
