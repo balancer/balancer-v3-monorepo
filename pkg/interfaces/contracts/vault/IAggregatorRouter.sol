a74d0e6111621fc10367fa18c04e9d228845d80d
Elshan Dzhafarov <22689890+elshan-eth@users.noreply.github.com>
2025-07-15 10:56:49 +0200

    Update pkg/interfaces/contracts/vault/IAggregatorRouter.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/interfaces/contracts/vault/IAggregatorRouter.sol
+++ pkg/interfaces/contracts/vault/IAggregatorRouter.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IVault } from "./IVault.sol";
 import { IRouterQueries } from "./IRouterQueries.sol";
 
 import { AddLiquidityKind, RemoveLiquidityKind, SwapKind } from "./VaultTypes.sol";
