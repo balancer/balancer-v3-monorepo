f000b23b613d0093fc4c160aa0bf255053bcb9ec
elshan-eth <22689890+elshan-eth@users.noreply.github.com>
2025-07-22 21:13:06 +0200

    split CompositeLiquidityRouter to CompositeLiquidityRouter and CompositeLiquidityRouterQueries

--- pkg/interfaces/contracts/vault/ICompositeLiquidityRouter.sol
+++ pkg/interfaces/contracts/vault/ICompositeLiquidityRouter.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.24;
 
-import {ICompositeLiquidityRouterErrors} from "./ICompositeLiquidityRouterErrors.sol";
+import { ICompositeLiquidityRouterQueries } from "./ICompositeLiquidityRouterQueries.sol";
 
 /**
  * @notice The composite liquidity router supports add/remove liquidity operations on ERC4626 and nested pools.
@@ -17,7 +17,7 @@ import {ICompositeLiquidityRouterErrors} from "./ICompositeLiquidityRouterErrors
  * 1) without having to do any expensive ERC4626 wrapping operations; and
  * 2) without requiring the user to construct a batch operation containing the buffer swap.
  */
-interface ICompositeLiquidityRouter is ICompositeLiquidityRouterErrors{
+interface ICompositeLiquidityRouter is ICompositeLiquidityRouterQueries {
     /***************************************************************************
                                    ERC4626 Pools
     ***************************************************************************/
@@ -88,60 +88,6 @@ interface ICompositeLiquidityRouter is ICompositeLiquidityRouterErrors{
         bytes memory userData
     ) external payable returns (uint256[] memory amountsOut);
 
-    /**
-     * @notice Queries an `addLiquidityUnbalancedToERC4626Pool` operation without actually executing it.
-     * @dev An "ERC4626 pool" contains IERC4626 yield-bearing tokens (e.g., waDAI).
-     * @param pool Address of the liquidity pool
-     * @param wrapUnderlying Flags indicating whether the corresponding token should be wrapped or used as an ERC20
-     * @param exactAmountsIn Exact amounts of underlying/wrapped tokens in, sorted in token registration order
-     * @param sender The sender passed to the operation. It can influence results (e.g., with user-dependent hooks)
-     * @param userData Additional (optional) data required for the query
-     * @return bptAmountOut Expected amount of pool tokens to receive
-     */
-    function queryAddLiquidityUnbalancedToERC4626Pool(
-        address pool,
-        bool[] memory wrapUnderlying,
-        uint256[] memory exactAmountsIn,
-        address sender,
-        bytes memory userData
-    ) external returns (uint256 bptAmountOut);
-
-    /**
-     * @notice Queries an `addLiquidityProportionalToERC4626Pool` operation without actually executing it.
-     * @dev An "ERC4626 pool" contains IERC4626 yield-bearing tokens (e.g., waDAI).
-     * @param pool Address of the liquidity pool
-     * @param wrapUnderlying Flags indicating whether the corresponding token should be wrapped or used as an ERC20
-     * @param exactBptAmountOut Exact amount of pool tokens to be received
-     * @param sender The sender passed to the operation. It can influence results (e.g., with user-dependent hooks)
-     * @param userData Additional (optional) data required for the query
-     * @return amountsIn Expected amounts of tokens added to the pool
-     */
-    function queryAddLiquidityProportionalToERC4626Pool(
-        address pool,
-        bool[] memory wrapUnderlying,
-        uint256 exactBptAmountOut,
-        address sender,
-        bytes memory userData
-    ) external returns (uint256[] memory amountsIn);
-
-    /**
-     * @notice Queries a `removeLiquidityProportionalFromERC4626Pool` operation without actually executing it.
-     * @dev An "ERC4626 pool" contains IERC4626 yield-bearing tokens (e.g., waDAI).
-     * @param pool Address of the liquidity pool
-     * @param unwrapWrapped Flags indicating whether the corresponding token should be unwrapped or used as an ERC20
-     * @param exactBptAmountIn Exact amount of pool tokens provided for the query
-     * @param sender The sender passed to the operation. It can influence results (e.g., with user-dependent hooks)
-     * @param userData Additional (optional) data required for the query
-     * @return amountsOut Expected amounts of tokens to receive
-     */
-    function queryRemoveLiquidityProportionalFromERC4626Pool(
-        address pool,
-        bool[] memory unwrapWrapped,
-        uint256 exactBptAmountIn,
-        address sender,
-        bytes memory userData
-    ) external returns (uint256[] memory amountsOut);
-
     /***************************************************************************
                                    Nested pools
     ***************************************************************************/
@@ -174,25 +120,6 @@ interface ICompositeLiquidityRouter is ICompositeLiquidityRouterErrors{
     ) external payable returns (uint256 bptAmountOut);
 
     /**
-     * @notice Queries an `addLiquidityUnbalancedNestedPool` operation without actually executing it.
-     * @param parentPool The address of the parent pool (which contains BPTs of other pools)
-     * @param tokensIn An array with all tokens from the child pools, and all non-BPT parent tokens, in arbitrary order
-     * @param exactAmountsIn An array with the amountIn of each token, sorted in the same order as tokensIn
-     * @param tokensToWrap A list of ERC4626 tokens which should be wrapped if encountered during pool traversal
-     * @param sender The sender passed to the operation. It can influence results (e.g., with user-dependent hooks)
-     * @param userData Additional (optional) data required for the operation
-     * @return bptAmountOut The actual amount of parent pool tokens received
-     */
-    function queryAddLiquidityUnbalancedNestedPool(
-        address parentPool,
-        address[] memory tokensIn,
-        uint256[] memory exactAmountsIn,
-        address[] memory tokensToWrap,
-        address sender,
-        bytes memory userData
-    ) external returns (uint256 bptAmountOut);
-
-    /**
      * @notice Removes liquidity from a nested pool.
      * @dev A nested pool is one in which one or more tokens are BPTs from another pool (child pool). Since there are
      * multiple pools involved, the token order is not well-defined, and must be specified by the caller. If the parent
@@ -217,23 +144,4 @@ interface ICompositeLiquidityRouter is ICompositeLiquidityRouterErrors{
         bool wethIsEth,
         bytes memory userData
     ) external payable returns (uint256[] memory amountsOut);
-
-    /**
-     * @notice Queries an `removeLiquidityProportionalNestedPool` operation without actually executing it.
-     * @param parentPool The address of the parent pool (which contains BPTs of other pools)
-     * @param exactBptAmountIn The exact amount of `parentPool` tokens provided
-     * @param tokensOut An array with all tokens from the child pools, and all non-BPT parent tokens, in arbitrary order
-     * @param tokensToUnwrap A list of ERC4626 tokens which should be unwrapped if encountered during pool traversal
-     * @param sender The sender passed to the operation. It can influence results (e.g., with user-dependent hooks)
-     * @param userData Additional (optional) data required for the operation
-     * @return amountsOut An array with the expected amountOut of each token, sorted in the same order as tokensOut
-     */
-    function queryRemoveLiquidityProportionalNestedPool(
-        address parentPool,
-        uint256 exactBptAmountIn,
-        address[] memory tokensOut,
-        address[] memory tokensToUnwrap,
-        address sender,
-        bytes memory userData
-    ) external returns (uint256[] memory amountsOut);
 }
