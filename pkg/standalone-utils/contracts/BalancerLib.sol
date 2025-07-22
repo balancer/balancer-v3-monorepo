b8f71b46881b6b85276f3a033548ffef9ad4ac42
elshan-eth <22689890+elshan-eth@users.noreply.github.com>
2025-07-22 18:18:35 +0200

    add rest tests

--- pkg/standalone-utils/contracts/BalancerLib.sol
+++ pkg/standalone-utils/contracts/BalancerLib.sol
@@ -13,17 +13,12 @@ import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol"
 
 import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 
-struct Context {
-    IAggregatorRouter aggregatorRouter;
-    IAggregatorBatchRouter aggregatorBatchRouter;
-    IVault vault;
-}
+import { Context, BalancerInternalLib } from "./BalancerInternalLib.sol";
 
 library BalancerLib {
     using SafeERC20 for IERC20;
     using BalancerInternalLib for Context;
 
-    //TODO: maybe we can use the Registry or specific Registry for library
     function createContext(
         address aggregatorRouter,
         address aggregatorBatchRouter
@@ -50,7 +45,7 @@ library BalancerLib {
         uint256 deadline
     ) internal returns (uint256 amountOut) {
         return
-            BalancerInternalLib._swapSingleTokenExactIn(
+            BalancerInternalLib.swapSingleTokenExactIn(
                 context,
                 pool,
                 tokenIn,
@@ -73,7 +68,7 @@ library BalancerLib {
         bytes memory userData
     ) internal returns (uint256) {
         return
-            BalancerInternalLib._swapSingleTokenExactIn(
+            BalancerInternalLib.swapSingleTokenExactIn(
                 context,
                 pool,
                 tokenIn,
@@ -97,7 +92,7 @@ library BalancerLib {
         IERC20 unwrapedTokenOut
     ) internal returns (uint256 amountOut) {
         return
-            BalancerInternalLib._swapSingleTokenExactIn(
+            BalancerInternalLib.swapSingleTokenExactIn(
                 context,
                 pool,
                 tokenIn,
@@ -124,7 +119,7 @@ library BalancerLib {
         bytes memory userData
     ) internal returns (uint256 amountOut) {
         return
-            BalancerInternalLib._swapSingleTokenExactIn(
+            BalancerInternalLib.swapSingleTokenExactIn(
                 context,
                 pool,
                 tokenIn,
@@ -152,7 +147,7 @@ library BalancerLib {
         uint256 deadline
     ) internal returns (uint256 amountIn) {
         return
-            BalancerInternalLib._swapSingleTokenExactOut(
+            BalancerInternalLib.swapSingleTokenExactOut(
                 context,
                 pool,
                 tokenIn,
@@ -175,7 +170,7 @@ library BalancerLib {
         bytes memory userData
     ) internal returns (uint256 amountIn) {
         return
-            BalancerInternalLib._swapSingleTokenExactOut(
+            BalancerInternalLib.swapSingleTokenExactOut(
                 context,
                 pool,
                 tokenIn,
@@ -199,7 +194,7 @@ library BalancerLib {
         IERC20 unwrapedTokenOut
     ) internal returns (uint256 amountIn) {
         return
-            BalancerInternalLib._swapSingleTokenExactOut(
+            BalancerInternalLib.swapSingleTokenExactOut(
                 context,
                 pool,
                 tokenIn,
@@ -226,7 +221,7 @@ library BalancerLib {
         bytes memory userData
     ) internal returns (uint256 amountIn) {
         return
-            BalancerInternalLib._swapSingleTokenExactOut(
+            BalancerInternalLib.swapSingleTokenExactOut(
                 context,
                 pool,
                 tokenIn,
@@ -238,152 +233,5 @@ library BalancerLib {
                 unwrapedTokenOut,
                 userData
             );
-    }
-}
-
-library BalancerInternalLib {
-    using SafeERC20 for IERC20;
-
-    function _swapSingleTokenExactIn(
-        Context memory context,
-        address pool,
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        uint256 exactAmountIn,
-        uint256 minAmountOut,
-        uint256 deadline,
-        bytes memory userData
-    ) internal returns (uint256 amountOut) {
-        tokenIn.safeTransfer(address(context.vault), exactAmountIn);
-
-        amountOut = context.aggregatorRouter.swapSingleTokenExactIn(
-            pool,
-            tokenIn,
-            tokenOut,
-            exactAmountIn,
-            minAmountOut,
-            deadline,
-            userData
-        );
-    }
-
-    function _swapSingleTokenExactIn(
-        Context memory context,
-        address pool,
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        uint256 exactAmountIn,
-        uint256 minAmountOut,
-        uint256 deadline,
-        IERC20 wrapedTokenIn,
-        IERC20 unwrapedTokenOut,
-        bytes memory userData
-    ) internal returns (uint256 amountOut) {
-        tokenIn.safeTransfer(address(context.vault), exactAmountIn);
-
-        IBatchRouter.SwapPathExactAmountIn[] memory paths = new IBatchRouter.SwapPathExactAmountIn[](1);
-        paths[0] = IBatchRouter.SwapPathExactAmountIn({
-            tokenIn: tokenIn,
-            steps: _makeSwapPathSteps(pool, tokenIn, tokenOut, wrapedTokenIn, unwrapedTokenOut),
-            exactAmountIn: exactAmountIn,
-            minAmountOut: minAmountOut
-        });
-
-        (uint256[] memory pathAmountsOut, , ) = context.aggregatorBatchRouter.swapExactIn(paths, deadline, userData);
-
-        return pathAmountsOut[0];
-    }
-
-    function _swapSingleTokenExactOut(
-        Context memory context,
-        address pool,
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        uint256 exactAmountOut,
-        uint256 maxAmountIn,
-        uint256 deadline,
-        bytes memory userData
-    ) internal returns (uint256 amountIn) {
-        tokenIn.safeTransfer(address(context.vault), maxAmountIn);
-
-        amountIn = context.aggregatorRouter.swapSingleTokenExactOut(
-            pool,
-            tokenIn,
-            tokenOut,
-            exactAmountOut,
-            maxAmountIn,
-            deadline,
-            userData
-        );
-    }
-
-    function _swapSingleTokenExactOut(
-        Context memory context,
-        address pool,
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        uint256 exactAmountOut,
-        uint256 maxAmountIn,
-        uint256 deadline,
-        IERC20 wrapedTokenIn,
-        IERC20 unwrapedTokenOut,
-        bytes memory userData
-    ) internal returns (uint256 amountIn) {
-        tokenIn.safeTransfer(address(context.vault), maxAmountIn);
-
-        IBatchRouter.SwapPathExactAmountOut[] memory paths = new IBatchRouter.SwapPathExactAmountOut[](1);
-        paths[0] = IBatchRouter.SwapPathExactAmountOut({
-            tokenIn: tokenIn,
-            steps: _makeSwapPathSteps(pool, tokenIn, tokenOut, wrapedTokenIn, unwrapedTokenOut),
-            exactAmountOut: exactAmountOut,
-            maxAmountIn: maxAmountIn
-        });
-
-        (uint256[] memory pathAmountsOut, , ) = context.aggregatorBatchRouter.swapExactOut(paths, deadline, userData);
-
-        return pathAmountsOut[0];
-    }
-
-    function _makeSwapPathSteps(
-        address pool,
-        IERC20 tokenIn,
-        IERC20 tokenOut,
-        IERC20 wrapedTokenIn,
-        IERC20 unwrapedTokenOut
-    ) private pure returns (IBatchRouter.SwapPathStep[] memory steps) {
-        IBatchRouter.SwapPathStep memory wrapPath;
-        IBatchRouter.SwapPathStep memory swapPath;
-        IBatchRouter.SwapPathStep memory unwrapPath;
-
-        bool wrapTokenIn = wrapedTokenIn != IERC20(address(0));
-        bool unwrapTokenOut = unwrapedTokenOut != IERC20(address(0));
-
-        IERC20 finalTokenOut = unwrapTokenOut ? unwrapedTokenOut : tokenOut;
-        if (wrapTokenIn) {
-            wrapPath = IBatchRouter.SwapPathStep({ pool: address(wrapedTokenIn), tokenOut: tokenIn, isBuffer: true });
-        }
-
-        if (unwrapTokenOut) {
-            unwrapPath = IBatchRouter.SwapPathStep({
-                pool: address(tokenOut),
-                tokenOut: unwrapedTokenOut,
-                isBuffer: true
-            });
-        }
-
-        swapPath = IBatchRouter.SwapPathStep({ pool: pool, tokenOut: finalTokenOut, isBuffer: false });
-
-        uint256 stepLength = 1 + (wrapTokenIn ? 1 : 0) + (unwrapTokenOut ? 1 : 0);
-        steps = new IBatchRouter.SwapPathStep[](stepLength);
-
-        for (uint256 i = 0; i < stepLength; i++) {
-            if (i == 0 && wrapTokenIn) {
-                steps[i] = wrapPath;
-            } else if (i <= 1) {
-                steps[i] = swapPath;
-            } else {
-                steps[i] = unwrapPath;
-            }
-        }
     }
 }
--- pkg/standalone-utils/test/foundry/BalancerLib.t.sol
+++ pkg/standalone-utils/test/foundry/BalancerLib.t.sol
@@ -7,10 +7,14 @@ import "forge-std/Test.sol";
 import { IHooks } from "@balancer-labs/v3-interfaces/contracts/vault/IHooks.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
 import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+import { TokenConfig, TokenType } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
+import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/IRateProvider.sol";
 
 import { AggregatorRouter } from "@balancer-labs/v3-vault/contracts/AggregatorRouter.sol";
 import { BaseVaultTest } from "@balancer-labs/v3-vault/test/foundry/utils/BaseVaultTest.sol";
 import { AggregatorBatchRouter } from "@balancer-labs/v3-vault/contracts/AggregatorBatchRouter.sol";
+import { PoolFactoryMock } from "@balancer-labs/v3-vault/contracts/test/PoolFactoryMock.sol";
+import { PoolMock } from "@balancer-labs/v3-vault/contracts/test/PoolMock.sol";
 
 import { Context, BalancerLib } from "../../contracts/BalancerLib.sol";
 
@@ -22,9 +26,14 @@ contract BalancerLibTest is BaseVaultTest {
     AggregatorRouter aggregatorRouter;
     AggregatorBatchRouter aggregatorBatchRouter;
 
-    // Track the indices for the standard dai/usdc pool.
     uint256 internal daiIdx;
     uint256 internal usdcIdx;
+
+    address internal wrappedPool;
+    uint256 internal waDaiIdx;
+    uint256 internal waUsdcIdx;
+
+    uint256 constant MAX_DELTA = 10;
 
     function setUp() public override {
         BaseVaultTest.setUp();
@@ -40,6 +49,35 @@ contract BalancerLibTest is BaseVaultTest {
         (daiIdx, usdcIdx) = getSortedIndexes(address(dai), address(usdc));
     }
 
+    function setUpWrappedPool() internal {
+        // 1. TokenConfig for waDAI and waUSDC
+        (waDaiIdx, waUsdcIdx) = getSortedIndexes(address(waDAI), address(waUSDC));
+        TokenConfig[] memory tokenConfig = new TokenConfig[](2);
+        tokenConfig[waDaiIdx].token = IERC20(waDAI);
+        tokenConfig[waUsdcIdx].token = IERC20(waUSDC);
+
+        // 2. Create PoolMock
+        string memory name = "waDAI/waUSDC Pool";
+        string memory symbol = "WAWA";
+        wrappedPool = address(new PoolMock(IVault(address(vault)), name, symbol));
+        vm.label(wrappedPool, name);
+        PoolFactoryMock(poolFactory).registerTestPool(wrappedPool, tokenConfig, poolHooksContract, lp);
+
+        // 3. Initialize buffers for waDAI and waUSDC
+        vm.startPrank(lp);
+        bufferRouter.initializeBuffer(waDAI, poolInitAmount, 0, 0);
+        bufferRouter.initializeBuffer(waUSDC, poolInitAmount, 0, 0);
+        vm.stopPrank();
+
+        // 4. Initialize the pool with liquidity
+        uint256[] memory amountsIn = new uint256[](2);
+        amountsIn[waDaiIdx] = poolInitAmount;
+        amountsIn[waUsdcIdx] = poolInitAmount;
+        vm.startPrank(lp);
+        _initPool(wrappedPool, amountsIn, poolInitAmount);
+        vm.stopPrank();
+    }
+
     function testSwapExactIn() public {
         uint256[] memory poolBalancesBefore = vault.getCurrentLiveBalances(pool);
         uint256 amountIn = poolBalancesBefore[usdcIdx] / 3;
@@ -51,9 +89,10 @@ contract BalancerLibTest is BaseVaultTest {
 
     function testSwapExactInWithUserData() public {
         uint256[] memory poolBalancesBefore = vault.getCurrentLiveBalances(pool);
-        uint256 amountIn = poolBalancesBefore[usdcIdx] / 3;
+        uint256 underlyingAmountIn = poolBalancesBefore[usdcIdx] / 3;
+        uint256 amountIn = _vaultPreviewDeposit(waUSDC, underlyingAmountIn);
 
-        uint256 amountOut = context.swapSingleTokenExactIn(pool, usdc, dai, amountIn, 0, MAX_UINT256, bytes("0x1234"));
+        uint256 amountOut = context.swapSingleTokenExactIn(pool, usdc, dai, amountIn, 0, MAX_UINT256, bytes("test"));
 
         _checkSwapResult(poolBalancesBefore, amountIn, amountOut);
     }
@@ -87,10 +126,126 @@ contract BalancerLibTest is BaseVaultTest {
             amountOut,
             dai.balanceOf(alice),
             MAX_UINT256,
-            bytes("0x1234")
+            bytes("test")
         );
 
         _checkSwapResult(poolBalancesBefore, amountIn, amountOut);
+    }
+
+    function testSwapExactInWithWrappedTokens() public {
+        setUpWrappedPool();
+
+        uint256[] memory poolBalancesBefore = vault.getCurrentLiveBalances(wrappedPool);
+
+        uint256 underlyingAmountIn = poolBalancesBefore[waUsdcIdx] / 3;
+        uint256 poolAmountIn = _vaultPreviewDeposit(waUSDC, underlyingAmountIn);
+
+        uint256 underlyingAmountOut = context.swapSingleTokenExactIn(
+            wrappedPool,
+            usdc,
+            dai,
+            underlyingAmountIn,
+            0,
+            MAX_UINT256,
+            waUSDC,
+            waDAI
+        );
+
+        uint256 poolAmountOut = _vaultPreviewDeposit(waDAI, underlyingAmountOut);
+        _checkWrappedSwapResult(
+            poolBalancesBefore,
+            underlyingAmountIn,
+            underlyingAmountOut,
+            poolAmountIn,
+            poolAmountOut
+        );
+    }
+
+    function testSwapExactInWithWrappedTokensWithUserData() public {
+        setUpWrappedPool();
+
+        uint256[] memory poolBalancesBefore = vault.getCurrentLiveBalances(wrappedPool);
+
+        uint256 underlyingAmountIn = poolBalancesBefore[waUsdcIdx] / 3;
+        uint256 poolAmountIn = _vaultPreviewDeposit(waUSDC, underlyingAmountIn);
+
+        uint256 underlyingAmountOut = context.swapSingleTokenExactIn(
+            wrappedPool,
+            usdc,
+            dai,
+            underlyingAmountIn,
+            0,
+            MAX_UINT256,
+            waUSDC,
+            waDAI,
+            bytes("test")
+        );
+
+        uint256 poolAmountOut = _vaultPreviewDeposit(waDAI, underlyingAmountOut);
+        _checkWrappedSwapResult(
+            poolBalancesBefore,
+            underlyingAmountIn,
+            underlyingAmountOut,
+            poolAmountIn,
+            poolAmountOut
+        );
+    }
+
+    function testSwapExactOutWithWrappedTokens() public {
+        setUpWrappedPool();
+        uint256[] memory poolBalancesBefore = vault.getCurrentLiveBalances(wrappedPool);
+
+        uint256 underlyingAmountOut = poolBalancesBefore[waDaiIdx] / 3;
+        uint256 poolAmountOut = _vaultPreviewDeposit(waDAI, underlyingAmountOut);
+
+        uint256 underlyingAmountIn = context.swapSingleTokenExactOut(
+            wrappedPool,
+            usdc,
+            dai,
+            underlyingAmountOut,
+            usdc.balanceOf(address(this)),
+            MAX_UINT256,
+            waUSDC,
+            waDAI
+        );
+        uint256 poolAmountIn = _vaultPreviewDeposit(waUSDC, underlyingAmountIn);
+
+        _checkWrappedSwapResult(
+            poolBalancesBefore,
+            underlyingAmountIn,
+            underlyingAmountOut,
+            poolAmountIn,
+            poolAmountOut
+        );
+    }
+
+    function testSwapExactOutWithWrappedTokensWithUserData() public {
+        setUpWrappedPool();
+        uint256[] memory poolBalancesBefore = vault.getCurrentLiveBalances(wrappedPool);
+
+        uint256 underlyingAmountOut = poolBalancesBefore[waDaiIdx] / 3;
+        uint256 poolAmountOut = _vaultPreviewDeposit(waDAI, underlyingAmountOut);
+
+        uint256 underlyingAmountIn = context.swapSingleTokenExactOut(
+            wrappedPool,
+            usdc,
+            dai,
+            underlyingAmountOut,
+            usdc.balanceOf(address(this)),
+            MAX_UINT256,
+            waUSDC,
+            waDAI,
+            bytes("test")
+        );
+        uint256 poolAmountIn = _vaultPreviewDeposit(waUSDC, underlyingAmountIn);
+
+        _checkWrappedSwapResult(
+            poolBalancesBefore,
+            underlyingAmountIn,
+            underlyingAmountOut,
+            poolAmountIn,
+            poolAmountOut
+        );
     }
 
     function _checkSwapResult(uint256[] memory poolBalancesBefore, uint256 amountIn, uint256 amountOut) internal view {
@@ -98,7 +253,32 @@ contract BalancerLibTest is BaseVaultTest {
 
         assertEq(usdc.balanceOf(address(this)), defaultAccountBalance() - amountIn, "Wrong USDC balance");
         assertEq(dai.balanceOf(address(this)), defaultAccountBalance() + amountOut, "Wrong DAI balance");
-        assertEq(poolBalancesAfter[daiIdx], poolBalancesBefore[daiIdx] - amountOut, "Wrong DAI pool balance");
+
         assertEq(poolBalancesAfter[usdcIdx], poolBalancesBefore[usdcIdx] + amountIn, "Wrong USDC pool balance");
+        assertEq(poolBalancesAfter[daiIdx], poolBalancesBefore[daiIdx] - amountOut, "Wrong DAI pool balance");
+    }
+
+    function _checkWrappedSwapResult(
+        uint256[] memory poolBalancesBefore,
+        uint256 underlyingAmountIn,
+        uint256 underlyingAmountOut,
+        uint256 poolAmountIn,
+        uint256 poolAmountOut
+    ) internal view {
+        uint256[] memory poolBalancesAfter = vault.getCurrentLiveBalances(wrappedPool);
+
+        assertEq(usdc.balanceOf(address(this)), defaultAccountBalance() - underlyingAmountIn, "Wrong USDC balance");
+        assertEq(dai.balanceOf(address(this)), defaultAccountBalance() + underlyingAmountOut, "Wrong DAI balance");
+        assertEq(
+            poolBalancesAfter[waUsdcIdx],
+            poolBalancesBefore[waUsdcIdx] + poolAmountIn,
+            "Wrong wUSDC pool balance"
+        );
+        assertApproxEqAbs(
+            poolBalancesAfter[waDaiIdx],
+            poolBalancesBefore[waDaiIdx] - poolAmountOut,
+            MAX_DELTA,
+            "Wrong wDAI pool balance"
+        );
     }
 }
