79ccca0a26b3f615377a8905b286c407418a14b1
João Bruno <joaobrunoah@gmail.com>
2025-07-22 15:21:59 -0300

    Document wrapper

--- pkg/standalone-utils/contracts/test/LPOracleWrapper.sol
+++ pkg/standalone-utils/contracts/test/LPOracleWrapper.sol
@@ -4,16 +4,25 @@ pragma solidity ^0.8.24;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+/**
+ * @dev latestRoundData is a view function, so we can't measure the gas cost of calling it on hardhat.
+ * So, we need a non-view function to measure the gas. This wrapper implements this non-view function.
+ */
 contract LPOracleWrapper {
     address private oracle;
-    uint256 public dummy; // dummy state variable
+    // State used only to justify the non-view function.
+    uint256 private dummy;
 
     constructor(address _oracle) {
         oracle = _oracle;
     }
 
     function callLatestRoundData() external returns (uint80, int256, uint256, uint256, uint80) {
-        dummy = block.timestamp; // write to state to justify non-view (solhint-disable does not work)
+        // This function must be a non-view function to measure the gas cost of calling it on hardhat.
+        // Since `solhint-disable func-mutability` does not work when compiling with hardhat, we modify
+        // a dummy state to allow this function to be a non-view.
+        dummy = block.timestamp;
+
         return AggregatorV3Interface(oracle).latestRoundData();
     }
 }
