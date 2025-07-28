a5de3e3af5831b975045a8c1b8e248277dd397f3
Elshan Dzhafarov <22689890+elshan-eth@users.noreply.github.com>
2025-07-28 21:40:38 +0200

    Update pkg/interfaces/contracts/vault/IAggregatorCompositeLiquidityRouter.sol
    Co-authored-by: João Bruno - Balancer Labs <joaobrunoah@gmail.com>

--- pkg/interfaces/contracts/vault/IAggregatorCompositeLiquidityRouter.sol
+++ pkg/interfaces/contracts/vault/IAggregatorCompositeLiquidityRouter.sol
@@ -29,7 +29,7 @@ interface IAggregatorCompositeLiquidityRouter is ICompositeLiquidityRouterQuerie
     /**
      * @notice Add arbitrary amounts of tokens to an ERC4626 pool through the buffer.
      * @dev An "ERC4626 pool" contains at least one IERC4626 yield-bearing token (e.g., waDAI). Ensure that any buffers associated
-     * with the wrapped tokens in the ERC4626 pool have been initialized before initializing or adding liquidity to
+     * with the wrapped tokens in the ERC4626 pool have been initialized before adding liquidity to
      * the "parent" pool, and also make sure limits are set properly.
      *
      * @param pool Address of the liquidity pool
