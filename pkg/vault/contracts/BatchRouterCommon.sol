f25f6f092f4d888cbdf7e4fc44ba7f1c5d78d64a
Jeff Bennett <endymionjkb@gmail.com>
2025-07-31 11:15:50 -0400

    docs: only one helper now

--- pkg/vault/contracts/BatchRouterCommon.sol
+++ pkg/vault/contracts/BatchRouterCommon.sol
@@ -87,8 +87,7 @@ abstract contract BatchRouterCommon is RouterCommon {
         return TransientStorageHelpers.calculateSlot(type(BatchRouterCommon).name, key);
     }
 
-    // Helpers to consolidate updates that always happen together.
-
+    // Helper to consolidate updates that always happen together.
     function _updateSwapTokensOut(address tokenOut, uint256 amountOut) internal {
         _currentSwapTokensOut().add(tokenOut);
         _currentSwapTokenOutAmounts().tAdd(tokenOut, amountOut);
