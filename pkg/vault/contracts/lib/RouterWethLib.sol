293a97d3a3caf8000463417c3b65f2038693f1f1
Jeff Bennett <endymionjkb@gmail.com>
2025-08-08 11:30:59 -0400

    unused imports

--- pkg/vault/contracts/lib/RouterWethLib.sol
+++ pkg/vault/contracts/lib/RouterWethLib.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.24;
 
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 
 import { IWETH } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/misc/IWETH.sol";
