60631880db1444c12e0ca1de75c923151ba72e1b
João Bruno <joaobrunoah@gmail.com>
2025-08-20 10:49:21 -0300

    Fix test

--- pkg/standalone-utils/test/foundry/EclpLPOracle.t.sol
+++ pkg/standalone-utils/test/foundry/EclpLPOracle.t.sol
@@ -217,15 +217,17 @@ contract EclpLPOracleTest is BaseVaultTest, GyroEclpPoolDeployer {
             FeedMock(address(feeds[i])).setLastRoundData(answers[i], updateTimestamps[i]);
         }
 
-        (int256[] memory returnedAnswers, uint256 returnedUpdateTimestamp) = oracle.getFeedData();
+        (int256[] memory returnedAnswers, uint256[] memory returnedTimestamps, uint256 minTimestamp) = oracle
+            .getFeedData();
         for (uint256 i = 0; i < NUM_TOKENS; i++) {
             assertEq(
                 uint256(returnedAnswers[i]),
                 answers[i] * oracle.getFeedTokenDecimalScalingFactors()[i],
                 "Answer does not match"
             );
+            assertEq(returnedTimestamps[i], updateTimestamps[i], "Timestamp does not match");
         }
-        assertEq(returnedUpdateTimestamp, minUpdateTimestamp, "Update timestamp does not match");
+        assertEq(minTimestamp, minUpdateTimestamp, "Update timestamp does not match");
     }
 
     function testCalculateTVL__Fuzz(
