c0bd2733814cf50b137eb7dc309af821c003c1ea
João Bruno - Balancer Labs <joaobrunoah@gmail.com>
2025-08-22 14:44:52 -0300

    Apply suggestions from code review
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/standalone-utils/contracts/HyperEVMRateProviderFactory.sol
+++ pkg/standalone-utils/contracts/HyperEVMRateProviderFactory.sol
@@ -15,11 +15,9 @@ import { Version } from "@balancer-labs/v3-solidity-utils/contracts/helpers/Vers
 
 import { HyperEVMRateProvider } from "./HyperEVMRateProvider.sol";
 
-/**
- * @notice Factory for deploying and managing HyperEVM rate providers.
- */
+/// @notice Factory for deploying and managing HyperEVM rate providers.
 contract HyperEVMRateProviderFactory is IHyperEVMRateProviderFactory, SingletonAuthentication, Version {
-    uint256 internal _rateProviderVersion;
+    uint256 internal immutable _rateProviderVersion;
     bool internal _isDisabled;
 
     mapping(bytes32 rateProviderId => IHyperEVMRateProvider rateProvider) internal _rateProviders;
