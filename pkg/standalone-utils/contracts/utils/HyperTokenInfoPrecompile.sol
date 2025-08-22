81370eac0a227147c76ef88b005c94b8afe8ce92
João Bruno <joaobrunoah@gmail.com>
2025-08-22 15:04:35 -0300

    Documentation

--- pkg/standalone-utils/contracts/utils/HyperTokenInfoPrecompile.sol
+++ pkg/standalone-utils/contracts/utils/HyperTokenInfoPrecompile.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.24;
 
+/**
+ * @notice Library to interact with the Hyperliquid token info precompile.
+ * @dev The precompile is a special type of code, executed in the Hypercore's node. For more information, see
+ * https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/hyperevm/interacting-with-hypercore .
+ */
 library HyperTokenInfoPrecompile {
+    // The following structure is defined by the token info precompile.
     struct HyperTokenInfo {
         string name;
         uint64[] spots;
@@ -14,6 +20,8 @@ library HyperTokenInfoPrecompile {
     }
 
     address public constant TOKEN_INFO_PRECOMPILE_ADDRESS = 0x000000000000000000000000000000000000080C;
+
+    /// @notice The precompile had an error while fetching the token info.
     error TokenInfoPrecompileFailed();
 
     function szDecimals(uint32 tokenIndex) internal view returns (uint8) {
