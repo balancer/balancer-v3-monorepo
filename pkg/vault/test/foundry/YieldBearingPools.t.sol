7d1f76a9095734305b18e4db7454b6664b5559f4
Elshan Dzhafarov <22689890+elshan-eth@users.noreply.github.com>
2025-08-26 00:41:28 +0200

    Update pkg/vault/test/foundry/YieldBearingPools.t.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/vault/test/foundry/YieldBearingPools.t.sol
+++ pkg/vault/test/foundry/YieldBearingPools.t.sol
@@ -7,7 +7,6 @@ import "forge-std/Test.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IRateProvider } from "@balancer-labs/v3-interfaces/contracts/solidity-utils/helpers/IRateProvider.sol";
 import { IVaultEvents } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultEvents.sol";
 import { IVaultAdmin } from "@balancer-labs/v3-interfaces/contracts/vault/IVaultAdmin.sol";
 import { SwapKind } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
