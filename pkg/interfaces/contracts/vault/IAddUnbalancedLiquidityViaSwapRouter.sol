9fb5dd49024621ffc094bf30dd84dfecf5b9d1f6
Elshan Dzhafarov <22689890+elshan-eth@users.noreply.github.com>
2025-08-27 19:04:08 +0200

    Update pkg/interfaces/contracts/vault/IAddUnbalancedLiquidityViaSwapRouter.sol
    Co-authored-by: EndymionJkb <EndymionJkb@gmail.com>

--- pkg/interfaces/contracts/vault/IAddUnbalancedLiquidityViaSwapRouter.sol
+++ pkg/interfaces/contracts/vault/IAddUnbalancedLiquidityViaSwapRouter.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { AddLiquidityKind, RemoveLiquidityKind, SwapKind } from "./VaultTypes.sol";
-import { IVault } from "./IVault.sol";
 import "./RouterTypes.sol";
 
 /// @notice Router interface for adding unbalanced liquidity via a combination of a proportional add and a swap.
