9fef0676a4217c355afa81b06c797153f6db32f6
João Bruno <joaobrunoah@gmail.com>
2025-09-02 11:47:41 -0300

    Revert when rate is zero and improve test coverage

--- pkg/standalone-utils/test/foundry/utils/HypercorePrecompileMock.sol
+++ pkg/standalone-utils/test/foundry/utils/HypercorePrecompileMock.sol
@@ -3,12 +3,27 @@ pragma solidity ^0.8.24;
 
 contract HypercorePrecompileMock {
     bytes internal _data;
+    bool internal _shouldRevert = false;
+    bool internal _shouldReturnZeroBytes = false;
+
+    function setShouldRevert(bool shouldRevert) external {
+        _shouldRevert = shouldRevert;
+    }
+    function setShouldReturnZeroBytes(bool shouldReturnZeroBytes) external {
+        _shouldReturnZeroBytes = shouldReturnZeroBytes;
+    }
 
     function setData(bytes memory data) external {
         _data = data;
     }
 
     fallback(bytes calldata) external returns (bytes memory) {
+        if (_shouldRevert) {
+            revert();
+        }
+        if (_shouldReturnZeroBytes) {
+            return abi.encode(0);
+        }
         return _data;
     }
 }
