bde891d03eaab7494e85236db042401c4ef0a0b8
João Bruno <joaobrunoah@gmail.com>
2025-09-04 10:47:48 -0300

    Remove virtual

--- pkg/vault/contracts/BalancerPoolToken.sol
+++ pkg/vault/contracts/BalancerPoolToken.sol
@@ -71,11 +71,9 @@ contract BalancerPoolToken is IERC20, IERC20Metadata, IERC20Permit, IRateProvide
 
     /**
      * @notice Get the address of the Balancer Vault.
-     * @dev This function is virtual to allow for overriding in derived contracts which also inherits contracts with
-     * `getVault()` definition.
      * @return vault An interface pointer to the Vault
      */
-    function getVault() public view virtual returns (IVault) {
+    function getVault() public view returns (IVault) {
         return _vault;
     }
 
