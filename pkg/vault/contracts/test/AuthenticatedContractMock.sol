9a3823964066a2e49e7eb99a714454e6011df6e8
Jeff Bennett <endymionjkb@gmail.com>
2025-09-04 17:56:45 -0400

    refactor: rename Mock contract to match V3 convention

--- pkg/vault/contracts/test/AuthenticatedContractMock.sol
+++ pkg/vault/contracts/test/AuthenticatedContractMock.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.24;
 
 import "../SingletonAuthentication.sol";
 
-/*
+/**
  * @author Balancer Labs
- * @title MockAuthenticatedContract
+ * @title AuthenticatedContractMock
  * @notice Generic authenticated contract
  * @dev A general purpose contract that can be used for testing permissioned functions in a more abstract way,
  * to test Authorizer functionality independent of specific Vault functions.
  */
-contract MockAuthenticatedContract is SingletonAuthentication {
+contract AuthenticatedContractMock is SingletonAuthentication {
     event ProtectedFunctionCalled(bytes data);
     event SecondProtectedFunctionCalled(bytes data);
 
