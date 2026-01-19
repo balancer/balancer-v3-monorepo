974efb04c5626b3b45cc29c42a31217dbb376f17
Jeff Bennett <endymionjkb@gmail.com>
2026-01-19 15:39:37 -0500

    pick up final OZ release

--- pkg/pool-weighted/contracts/lbp/BPTTimeLocker.sol
+++ pkg/pool-weighted/contracts/lbp/BPTTimeLocker.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.24;
 
-import { ERC6909Metadata } from "@openzeppelin/contracts/token/ERC6909/extensions/draft-ERC6909Metadata.sol";
+import { ERC6909Metadata } from "@openzeppelin/contracts/token/ERC6909/extensions/ERC6909Metadata.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ERC6909 } from "@openzeppelin/contracts/token/ERC6909/draft-ERC6909.sol";
+import { ERC6909 } from "@openzeppelin/contracts/token/ERC6909/ERC6909.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Multicall } from "@openzeppelin/contracts/utils/Multicall.sol";
 
