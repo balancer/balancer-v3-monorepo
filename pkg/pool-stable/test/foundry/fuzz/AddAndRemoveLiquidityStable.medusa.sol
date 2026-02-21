ef9814f5fa98b386dd3105150041d90e1b8fc178
Jeff Bennett <endymionjkb@gmail.com>
2026-02-21 13:51:27 -0500

    docs: fix imports in add/remove test

--- pkg/pool-stable/test/foundry/fuzz/AddAndRemoveLiquidityStable.medusa.sol
+++ pkg/pool-stable/test/foundry/fuzz/AddAndRemoveLiquidityStable.medusa.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.24;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import { PoolRoleAccounts } from "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 import { IVault } from "@balancer-labs/v3-interfaces/contracts/vault/IVault.sol";
-import "@balancer-labs/v3-interfaces/contracts/vault/VaultTypes.sol";
 
 import {
     AddAndRemoveLiquidityMedusaTest
