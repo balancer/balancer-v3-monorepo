626bb89274da33349acd5d1dadaac27739a88b62
Juan Ignacio Ubeira <juani@balancerlabs.dev>
2026-03-02 17:56:29 -0300

    Abbreviated T&C for contract signature registry. (#1634)

--- pkg/standalone-utils/contracts/ClaimSignatureRegistry.sol
+++ pkg/standalone-utils/contracts/ClaimSignatureRegistry.sol
@@ -31,14 +31,14 @@ contract ClaimSignatureRegistry {
 
     constructor() {
         // solhint-disable max-line-length
-        string memory terms = "I confirm and agree to the following:\n\n"
-        "I accept the terms and conditions applicable to this claim, including all provisions of the Balancer Terms of Use and all relevant Balancer governance resolutions.\n\n"
-        "I acknowledge and agree that my acceptance constitutes a full and final settlement and release of any and all past, present, and future claims, liabilities, demands, actions, causes of action, damages, or losses of any kind -whether known or unknown-arising out of or related to the Balancer V2 exploit.\n\n"
-        'This waiver expressly includes claims against the Balancer Foundation, Balancer OpCo Limited, Balancer Onchain Limited and all affiliated entities, as well as their respective officers, directors, contributors, service providers, employees, contractors, advisors, agents, successors, and assigns (collectively, the "Released Parties").\n\n'
-        "I acknowledge the limitation of liability, mandatory arbitration and all risk disclosures set forth in the Balancer Terms of Use.\n"
-        "I waive and relinquish any right to participate in any class or collective action related to the V2 exploit or the Balancer Protocol.\n\n"
-        "I acknowledge and agree to the SEAL Safe Harbor Agreement which was approved by governance resolution and can offer legal protection to whitehats who aid in the recovery of assets during an active exploit.\n\n"
-        "I understand that my claim will not be processed unless I accept these terms in full and without modification.";
+        string
+            memory terms = "I accept the T&Cs applicable to this claim, incl. all provisions of the Balancer ToU & all relevant Balancer governance resolutions.\n\n"
+            "I acknowledge & agree that my acceptance constitutes a full & final settlement & release of any & all past, present, & future claims, liabilities, demands, actions, causes of action, damages, or losses of any kind -whether known or unknown- arising out of or related to the Balancer V2 exploit.\n\n"
+            'This waiver expressly incl. claims against the Balancer Foundation, Balancer OpCo Ltd., Balancer Onchain Ltd. & all affiliated entities, as well as their respective officers, directors, contributors, service providers, employees, contractors, advisors, agents, successors, & assigns (collectively, the "Released Parties").\n\n'
+            "I acknowledge the limitation of liability, mandatory arbitration & all risk disclosures set forth in the Balancer ToU.\n\n"
+            "I waive & relinquish any right to participate in any class or collective action related to the V2 exploit or the Balancer Protocol.\n\n"
+            "I acknowledge & agree to the SEAL Safe Harbor Agreement (approved by governance resolution) which can offer legal protection to whitehats who aid in the recovery of assets during an active exploit.\n\n"
+            "I understand that my claim will not be processed unless I accept these terms in full & without modification.";
 
         TERMS_DIGEST = bytes(terms).toEthSignedMessageHash();
     }
